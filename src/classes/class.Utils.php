17927ec4d3b547a766ff2cde1693f0528b20c9f0
Charles du Jeu <charles@ajaxplorer.info>
2007-12-11 21:14:00 +0000

    function convertBytes

--- src/classes/class.Utils.php
+++ src/classes/class.Utils.php
@@ -269,6 +269,32 @@ function show_hidden_files($fichier)
 	return $retour;
 }
 
+/**
+ * Convert a shorthand byte value from a PHP configuration directive to an integer value
+ * @param    string   $value
+ * @return   int
+ */
+function convertBytes( $value ) {
+    if ( is_numeric( $value ) ) {
+        return $value;
+    } else {
+        $value_length = strlen( $value );
+        $qty = substr( $value, 0, $value_length - 1 );
+        $unit = strtolower( substr( $value, $value_length - 1 ) );
+        switch ( $unit ) {
+            case 'k':
+                $qty *= 1024;
+                break;
+            case 'm':
+                $qty *= 1048576;
+                break;
+            case 'g':
+                $qty *= 1073741824;
+                break;
+        }
+        return $qty;
+    }
+}
 
 
 
