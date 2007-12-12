30f7e985ed623914d9c0c614c1b3346b2b817684
Charles du Jeu <charles@ajaxplorer.info>
2007-12-12 20:03:36 +0000

    Cache origin directories for recycle objects

---
+++ classes/class.RecycleBinManager.php
@@ -0,0 +1,61 @@
+<?php
+class RecycleBinManager
+{
+	function getCacheFileName()
+	{
+		return ".ajxp_recycle_cache.ser";
+	}
+	
+	function fileToRecycle($originalFilePath)
+	{
+		$cache = RecycleBinManager::loadCache();
+		$cache[basename($originalFilePath)] = str_replace("\\", "/", dirname($originalFilePath));
+		RecycleBinManager::saveCache($cache);
+	}
+
+	function deleteFromRecycle($filePath)
+	{
+		$cache = RecycleBinManager::loadCache();
+		if(array_key_exists(basename($filePath), $cache))
+		{
+			unset($cache[basename($filePath)]);
+		}
+		RecycleBinManager::saveCache($cache);		
+	}
+	
+	function getFileOrigin($filePath)
+	{
+		$cache = RecycleBinManager::loadCache();
+		if(array_key_exists(basename($filePath), $cache))
+		{
+			return $cache[basename($filePath)];
+		}
+		return "";
+	}
+	
+	function loadCache(){
+		$result = array();
+		if(ConfService::getRecycleBinDir() == "") return ;
+		$cachePath = ConfService::getRootDir()."/".ConfService::getRecycleBinDir()."/".RecycleBinManager::getCacheFileName();
+		if(is_file($cachePath))
+		{
+			$fileLines = file($cachePath);
+			$result = unserialize($fileLines[0]);
+		}
+		return $result;
+	}
+	
+	function saveCache($value){
+		if(ConfService::getRecycleBinDir() == "") return ;
+		$cachePath = ConfService::getRootDir()."/".ConfService::getRecycleBinDir()."/".RecycleBinManager::getCacheFileName();
+		if(!is_dir(ConfService::getRootDir()."/".ConfService::getRecycleBinDir()))
+		{
+			mkdir(ConfService::getRootDir()."/".ConfService::getRecycleBinDir());
+		}
+		$fp = fopen($cachePath, "w");
+		fwrite($fp, serialize($value));
+		fclose($fp);
+	}
+	
+}
+?>
