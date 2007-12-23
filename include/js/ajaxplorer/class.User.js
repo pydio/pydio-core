9cb38c1a7ae37e842bb90b3d304e59407b1ca5aa
Charles du Jeu <charles@ajaxplorer.info>
2007-12-23 22:56:39 +0000

    Prototypized class definition

--- include/js/ajaxplorer/class.User.js
+++ include/js/ajaxplorer/class.User.js
@@ -1,64 +1,67 @@
-function User(id){
-	this.id = id;
-	this.activeRepository;
-	this.read = false;
-	this.write = false;
-	this.preferences = new Hash();
-	this.repositories = new Hash();
-}
+User = Class.create({
 
-User.prototype.setActiveRepository = function (id, read, write)
-{
-	this.activeRepository = id;
-	this.read = (read=="1"?true:false);
-	this.write = (write=="1"?true:false);
-}
+	id : undefined,
+	activeRepository:undefined,
+	read:false,
+	write:false,
+	preferences:undefined,
+	repositories: undefined,
 
-User.prototype.getActiveRepository = function(){
-	return this.activeRepository;
-}
+	initialize:function(id){
+		this.id = id;
+		this.preferences = new Hash();
+		this.repositories = new Hash();
+	},
 
-User.prototype.canRead = function(){
-	return this.read;
-}
-
-User.prototype.canWrite = function(){
-	return this.write;
-}
-
-User.prototype.getPreference = function(prefName){
-    return this.preferences.get(prefName);	
-}
-
-User.prototype.getRepositoriesList = function(){
-	return this.repositories;
-}
-
-User.prototype.setPreference = function(prefName, prefValue)
-{
-	this.preferences.set(prefName, prefValue);
-}
-
-User.prototype.setRepositoriesList = function(repoHash)
-{
-	this.repositories = repoHash;
-}
-
-User.prototype.savePreferences = function(newPass, onCompleteFunc)
-{
-	var conn = new Connexion();
-	conn.addParameter("get_action", "save_user_pref");
-	var i=0;
-	this.preferences.each(function(pair){
-		conn.addParameter("pref_name_"+i, pair.key);
-		conn.addParameter("pref_value_"+i, pair.value);
-		i++;
-	});
-	if(newPass)
-	{
-		conn.addParameter("pref_name_"+i, "password");
-		conn.addParameter("pref_value_"+i, newPass);
-	}
-	conn.onComplete = onCompleteFunc;
-	conn.sendAsync();
-}
+	setActiveRepository : function (id, read, write){
+		this.activeRepository = id;
+		this.read = (read=="1"?true:false);
+		this.write = (write=="1"?true:false);
+	},
+	
+	getActiveRepository : function(){
+		return this.activeRepository;
+	},
+	
+	canRead : function(){
+		return this.read;
+	},
+	
+	canWrite : function(){
+		return this.write;
+	},
+	
+	getPreference : function(prefName){
+	    return this.preferences.get(prefName);	
+	},
+	
+	getRepositoriesList : function(){
+		return this.repositories;
+	},
+	
+	setPreference : function(prefName, prefValue){
+		this.preferences.set(prefName, prefValue);
+	},
+	
+	setRepositoriesList : function(repoHash){
+		this.repositories = repoHash;
+	},
+	
+	savePreferences : function(newPass, onCompleteFunc){
+		var conn = new Connexion();
+		conn.addParameter("get_action", "save_user_pref");
+		var i=0;
+		this.preferences.each(function(pair){
+			conn.addParameter("pref_name_"+i, pair.key);
+			conn.addParameter("pref_value_"+i, pair.value);
+			i++;
+		});
+		if(newPass)
+		{
+			conn.addParameter("pref_name_"+i, "password");
+			conn.addParameter("pref_value_"+i, newPass);
+		}
+		conn.onComplete = onCompleteFunc;
+		conn.sendAsync();
+	},
+});
