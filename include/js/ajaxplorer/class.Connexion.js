83aed027a01af840de7f8223659cec6485ea0368
Charles du Jeu <charles@ajaxplorer.info>
2007-12-24 17:01:19 +0000

    Prototypized classes

--- include/js/ajaxplorer/class.Connexion.js
+++ include/js/ajaxplorer/class.Connexion.js
@@ -1,136 +1,128 @@
-function Connexion(baseUrl)
-{
-	this._baseUrl = 'content.php';
-	if(baseUrl) this._baseUrl = baseUrl;
-	this._libUrl = 'include/js';
-	this._parameters = new Hash();
-	this._method = 'get';
-	this.addParameter('get_action', 'xml_listing');
-}
+Connexion = Class.create({
 
-Connexion.prototype.addParameter = function (paramName, paramValue)
-{
-	this._parameters.set(paramName, paramValue);	
-}
-
-Connexion.prototype.setParameters = function(hParameters)
-{
-	this._parameters = $H(hParameters);
-}
-
-Connexion.prototype.setMethod = function(method)
-{
-	this._method = 'put';
-}
-
-Connexion.prototype.sendAsync = function()
-{	
-	var oThis = this;
-	// WARNING, FINALLY PAS PAREMETERS AS AN OBJECT, PROTOTYPE 1.6.0 BUG Hash.toQueryString();
-	new Ajax.Request(this._baseUrl, 
+	initialize: function(baseUrl)
 	{
-		method:this._method,
-		onComplete:this.onComplete,
-		parameters:this._parameters.toObject()
-	});
-}
-
-Connexion.prototype.sendSync = function()
-{	
-	var oThis = this;
-	// WARNING, FINALLY PAS PAREMETERS AS AN OBJECT, PROTOTYPE 1.6.0 BUG Hash.toQueryString();
-	new Ajax.Request(this._baseUrl, 
-	{
-		method:this._method,
-		asynchronous: false,
-		onComplete:this.onComplete,
-		parameters:this._parameters.toObject()
-	});
-}
-
-Connexion.prototype.loadLibrary = function(fileName, onLoadedCode)
-{
-	var oThis = this;
-	new Ajax.Request(this._libUrl+'/'+fileName, 
-	{
-		method:'get',
-		asynchronous: false,
-		onComplete:function(transport){
-			if(transport.responseText) 
-			{
-				try
+		this._baseUrl = 'content.php';
+		if(baseUrl) this._baseUrl = baseUrl;
+		this._libUrl = 'include/js';
+		this._parameters = new Hash();
+		this._method = 'get';
+		this.addParameter('get_action', 'xml_listing');
+	},
+	
+	addParameter : function (paramName, paramValue){
+		this._parameters.set(paramName, paramValue);	
+	},
+	
+	setParameters : function(hParameters){
+		this._parameters = $H(hParameters);
+	},
+	
+	setMethod : function(method){
+		this._method = 'put';
+	},
+	
+	sendAsync : function(){	
+		// WARNING, FINALLY PAS PAREMETERS AS AN OBJECT, PROTOTYPE 1.6.0 BUG Hash.toQueryString();
+		new Ajax.Request(this._baseUrl, 
+		{
+			method:this._method,
+			onComplete:this.onComplete,
+			parameters:this._parameters.toObject()
+		});
+	},
+	
+	sendSync : function(){	
+		// WARNING, FINALLY PAS PAREMETERS AS AN OBJECT, PROTOTYPE 1.6.0 BUG Hash.toQueryString();
+		new Ajax.Request(this._baseUrl, 
+		{
+			method:this._method,
+			asynchronous: false,
+			onComplete:this.onComplete,
+			parameters:this._parameters.toObject()
+		});
+	},
+	
+	loadLibrary : function(fileName, onLoadedCode){
+		new Ajax.Request(this._libUrl+'/'+fileName, 
+		{
+			method:'get',
+			asynchronous: false,
+			onComplete:function(transport){
+				if(transport.responseText) 
 				{
-					var script = transport.responseText;					
-				    if (window.execScript){	
-				    	//alert('execScript for'+fileName);
-				        window.execScript( script );
-				    }
-				    else{
-				    	//alert('eval for'+fileName);
-						//window.eval( script );
-						// TO TEST, THIS SEEM TO WORK ON SAFARI
-						window.my_code = script;
-						var script_tag = document.createElement('script');
-						script_tag.type = 'text/javascript';
-						script_tag.innerHTML = 'eval(window.my_code)';
-						document.getElementsByTagName('head')[0].appendChild(script_tag)
-				    }
-					if(onLoadedCode != null) onLoadedCode();
-				}
-				catch(e)
-				{
-					alert('error loading '+fileName+':'+e);
-					//errorWindow=window.open("", "", "height=500, width=600,toolbar=no,scrollbars=yes,menubar=no,resizable=yes");
-					//errorWindow.document.write(transport.responseText);
+					try
+					{
+						var script = transport.responseText;					
+					    if (window.execScript){	
+					    	//alert('execScript for'+fileName);
+					        window.execScript( script );
+					    }
+					    else{
+					    	//alert('eval for'+fileName);
+							//window.eval( script );
+							// TO TEST, THIS SEEM TO WORK ON SAFARI
+							window.my_code = script;
+							var script_tag = document.createElement('script');
+							script_tag.type = 'text/javascript';
+							script_tag.innerHTML = 'eval(window.my_code)';
+							document.getElementsByTagName('head')[0].appendChild(script_tag)
+					    }
+						if(onLoadedCode != null) onLoadedCode();
+					}
+					catch(e)
+					{
+						alert('error loading '+fileName+':'+e);
+						//errorWindow=window.open("", "", "height=500, width=600,toolbar=no,scrollbars=yes,menubar=no,resizable=yes");
+						//errorWindow.document.write(transport.responseText);
+					}
 				}
 			}
-		}
-	});	
-}
-
-Connexion.prototype.loadLibraries = function()
-{
-	if(!dynamicLibLoading) {return;}
-	var toLoad = $A([
-		"lib/webfx/slider/js/timer.js", 
-		"lib/webfx/slider/js/range.js", 
-		"lib/webfx/slider/js/slider.js", 
-		"lib/leightbox/lightbox.js", 
-		"lib/jquery/dimensions.js", 
-		"lib/jquery/splitter.js", 
-		"lib/ufo/ufo.js",
-		"lib/prototype/proto.menu.js",
-		"lib/codepress/codepress.js",
-		"lib/webfx/selectableelements.js", 
-		"lib/webfx/selectabletablerows.js", 
-		"lib/webfx/sortabletable.js", 
-		"lib/webfx/numberksorttype.js", 
-		"lib/webfx/slider/js/timer.js", 
-		"lib/webfx/slider/js/range.js", 
-		"lib/webfx/slider/js/slider.js", 
-		"lib/xloadtree/xtree.js", 
-		"lib/xloadtree/xloadtree.js", 
-		"lib/xloadtree/xmlextras.js",
-		"ajaxplorer/ajxp_multifile.js", 
-		"ajaxplorer/ajxp_utils.js", 
-		"ajaxplorer/class.User.js", 
-		"ajaxplorer/class.AjxpDraggable.js",
-		"ajaxplorer/class.AjxpAutoCompleter.js",
-		"ajaxplorer/class.Diaporama.js",
-		"ajaxplorer/class.Editor.js",
-		"ajaxplorer/class.ActionsManager.js", 
-		"ajaxplorer/class.FilesList.js", 
-		"ajaxplorer/class.FoldersTree.js", 
-		"ajaxplorer/class.SearchEngine.js", 
-		"ajaxplorer/class.InfoPanel.js", 
-		"ajaxplorer/class.ResizeableBar.js", 
-		"ajaxplorer/class.UserSelection.js"]);
-		
-		
-	modal.incrementStepCounts(toLoad.size());
-	toLoad.each(function(fileName){
-		var onLoad = function(){modal.updateLoadingProgress(fileName);};
-		if(fileName == toLoad.last()) onLoad = function(){modal.updateLoadingProgress(fileName);};
-		this.loadLibrary(fileName, onLoad);
-	}.bind(this));
-}
+		});	
+	},
+	
+	loadLibraries : function(){
+		if(!dynamicLibLoading) {return;}
+		var toLoad = $A([
+			"lib/webfx/slider/js/timer.js", 
+			"lib/webfx/slider/js/range.js", 
+			"lib/webfx/slider/js/slider.js", 
+			"lib/leightbox/lightbox.js", 
+			"lib/jquery/dimensions.js", 
+			"lib/jquery/splitter.js", 
+			"lib/ufo/ufo.js",
+			"lib/prototype/proto.menu.js",
+			"lib/codepress/codepress.js",
+			"lib/webfx/selectableelements.js", 
+			"lib/webfx/selectabletablerows.js", 
+			"lib/webfx/sortabletable.js", 
+			"lib/webfx/numberksorttype.js", 
+			"lib/webfx/slider/js/timer.js", 
+			"lib/webfx/slider/js/range.js", 
+			"lib/webfx/slider/js/slider.js", 
+			"lib/xloadtree/xtree.js", 
+			"lib/xloadtree/xloadtree.js", 
+			"lib/xloadtree/xmlextras.js",
+			"ajaxplorer/ajxp_multifile.js", 
+			"ajaxplorer/ajxp_utils.js", 
+			"ajaxplorer/class.User.js", 
+			"ajaxplorer/class.AjxpDraggable.js",
+			"ajaxplorer/class.AjxpAutoCompleter.js",
+			"ajaxplorer/class.Diaporama.js",
+			"ajaxplorer/class.Editor.js",
+			"ajaxplorer/class.ActionsManager.js", 
+			"ajaxplorer/class.FilesList.js", 
+			"ajaxplorer/class.FoldersTree.js", 
+			"ajaxplorer/class.SearchEngine.js", 
+			"ajaxplorer/class.InfoPanel.js", 
+			"ajaxplorer/class.ResizeableBar.js", 
+			"ajaxplorer/class.UserSelection.js"]);
+			
+		modal.incrementStepCounts(toLoad.size());
+		toLoad.each(function(fileName){
+			var onLoad = function(){modal.updateLoadingProgress(fileName);};
+			if(fileName == toLoad.last()) onLoad = function(){modal.updateLoadingProgress(fileName);};
+			this.loadLibrary(fileName, onLoad);
+		}.bind(this));
+	}
+});
--- include/js/ajaxplorer/class.Modal.js
+++ include/js/ajaxplorer/class.Modal.js
@@ -1,289 +1,277 @@
-Modal = function()
-{
-	this.pageLoading = true;
-}
+Modal = Class.create({
 
-Modal.prototype.init = function()
-{
-	this.elementName = 'generic_dialog_box';
-	this.htmlElement = $(this.elementName);
-	this.dialogTitle = this.htmlElement.getElementsBySelector(".dialogTitle")[0];
-	this.dialogContent = this.htmlElement.getElementsBySelector(".dialogContent")[0];
-	this.currentForm;
-	this.cachedForms = new Hash();
-	this.iframeIndex = 0;	
-}
-
-
-Modal.prototype.prepareHeader = function(sTitle, sIconSrc)
-{
-	var hString = "<span class=\"titleString\">";
-	if(sIconSrc != "") hString = "<span class=\"titleString\"><img src=\""+sIconSrc.replace('22', '16')+"\" width=\"16\" height=\"16\" align=\"top\"/>&nbsp;";
-	hString += sTitle + '</span>';
-	this.dialogTitle.innerHTML = hString;
-}
-
-Modal.prototype.showDialogForm = function(sTitle, sFormId, fOnLoad, fOnComplete, fOnCancel, bOkButtonOnly, skipButtons)
-{
-	this.clearContent(this.dialogContent);
-	//this.dialogTitle.innerHTML = sTitle;
-	var newForm;
-	if($(sFormId).tagName == 'FORM') // WE PASSED A PREFETCHED HIDDEN FORM
-	{
-		newForm = $(sFormId);
-		newForm.show();
-	}
-	else
-	{
-		var formDiv = $(sFormId);	
-		var newForm = document.createElement('form');
-		newForm.id = 'action_form';
-		newForm.setAttribute('action', 'cont.php');
-		newForm.appendChild(formDiv.cloneNode(true));
-		var reloadIFrame = null;
-		if($(newForm).getElementsByTagName("iframe")[0])
-		{
-			reloadIFrame = $(newForm).getElementsByTagName("iframe")[0];
-			reloadIFrameSrc = $(newForm).getElementsByTagName("iframe")[0].getAttribute("src");
-		}
-		if(formDiv.getAttribute('action'))
-		{
-			var actionField = document.createElement('input');
-			actionField.setAttribute('type', 'hidden'); 
-			actionField.setAttribute('name', 'get_action'); 
-			actionField.setAttribute('value', formDiv.getAttribute('action'));
-			newForm.appendChild(actionField);
-		}		
-	}
-	if(!this.cachedForms.get(sFormId) && !skipButtons){
-		this.addSubmitCancel(newForm, fOnCancel, bOkButtonOnly);
-	}
-	this.dialogContent.appendChild(newForm);
+	pageLoading: true,
+	initialize: function(){
+	},
 	
-	if(fOnComplete)	{
-		newForm.onsubmit = function(){
-			try{
-				fOnComplete();
-			}catch(e){
-				alert('Unexpected Error : please report!\n'+e);				
+	init: function(){
+		this.elementName = 'generic_dialog_box';
+		this.htmlElement = $(this.elementName);
+		this.dialogTitle = this.htmlElement.getElementsBySelector(".dialogTitle")[0];
+		this.dialogContent = this.htmlElement.getElementsBySelector(".dialogContent")[0];
+		this.currentForm;
+		this.cachedForms = new Hash();
+		this.iframeIndex = 0;	
+	},
+	
+	prepareHeader: function(sTitle, sIconSrc){
+		var hString = "<span class=\"titleString\">";
+		if(sIconSrc != "") hString = "<span class=\"titleString\"><img src=\""+sIconSrc.replace('22', '16')+"\" width=\"16\" height=\"16\" align=\"top\"/>&nbsp;";
+		hString += sTitle + '</span>';
+		this.dialogTitle.innerHTML = hString;
+	},
+	
+	showDialogForm: function(sTitle, sFormId, fOnLoad, fOnComplete, fOnCancel, bOkButtonOnly, skipButtons){
+		this.clearContent(this.dialogContent);
+		//this.dialogTitle.innerHTML = sTitle;
+		var newForm;
+		if($(sFormId).tagName == 'FORM') // WE PASSED A PREFETCHED HIDDEN FORM
+		{
+			newForm = $(sFormId);
+			newForm.show();
+		}
+		else
+		{
+			var formDiv = $(sFormId);	
+			var newForm = document.createElement('form');
+			newForm.id = 'action_form';
+			newForm.setAttribute('action', 'cont.php');
+			newForm.appendChild(formDiv.cloneNode(true));
+			var reloadIFrame = null;
+			if($(newForm).getElementsByTagName("iframe")[0])
+			{
+				reloadIFrame = $(newForm).getElementsByTagName("iframe")[0];
+				reloadIFrameSrc = $(newForm).getElementsByTagName("iframe")[0].getAttribute("src");
 			}
-			return false;
+			if(formDiv.getAttribute('action'))
+			{
+				var actionField = document.createElement('input');
+				actionField.setAttribute('type', 'hidden'); 
+				actionField.setAttribute('name', 'get_action'); 
+				actionField.setAttribute('value', formDiv.getAttribute('action'));
+				newForm.appendChild(actionField);
+			}		
 		}
-	}
-	else {
-		newForm.onsubmit = function(){
-			ajaxplorer.actionBar.submitForm(modal.getForm());
-			hideLightBox();
-			return false;
-		};
-	}
-	this.showContent(this.elementName, $(sFormId).getAttribute("box_width"), $(sFormId).getAttribute("box_height"));
-	if($(newForm).getElementsBySelector(".dialogFocus").length)
-	{
-		objToFocus = $(newForm).getElementsBySelector(".dialogFocus")[0];
-		setTimeout('objToFocus.focus()', 500);
-	}
-	if($(newForm).getElementsBySelector(".replace_rep").length)
-	{
-		repDisplay = $(newForm).getElementsBySelector(".replace_rep")[0];
-		repDisplay.innerHTML = ajaxplorer.filesList.getCurrentRep();
-	}
-	if($(newForm).getElementsBySelector(".replace_file").length)
-	{
-		repDisplay = $(newForm).getElementsBySelector(".replace_file")[0];
-		repDisplay.innerHTML = getBaseName(ajaxplorer.filesList.getUserSelection().getUniqueFileName());
-	}
-	this.currentForm = newForm;
-	if(fOnLoad != null)
-	{
-		fOnLoad(this.currentForm);
-	}
-	// SAFARI => FORCE IFRAME RELOADING
-	if(reloadIFrame) reloadIFrame.src = reloadIFrameSrc;
-}
-
-Modal.prototype.showContent = function(elementName, boxWidth, boxHeight)
-{
-	ajaxplorer.disableShortcuts();
-	ajaxplorer.disableNavigation();
-	ajaxplorer.filesList.blur();
-	//jQuery('#'+elementName).corner("round top 5px");
-	if(Prototype.Browser.IE){
-		jQuery('#'+elementName + ' .dialogTitle').corner("round top 7px");
-	}else{
-		jQuery('#'+elementName).corner("round top 7px");
-	}
-	if(!this.isRounded)
-	{
-		ajxpCorners($(elementName), 'bottom');
-		this.isRounded = true;		
-	}
-	var winWidth = $(document.body).getWidth();
-	var winHeight = $(document.body).getHeight();
-
-	// WIDTH / HEIGHT
-	if(boxWidth != null){
-		if(boxWidth.indexOf('%') > -1){
-			percentWidth = parseInt(boxWidth);
-			boxWidth = parseInt((winWidth * percentWidth) / 100);
+		if(!this.cachedForms.get(sFormId) && !skipButtons){
+			this.addSubmitCancel(newForm, fOnCancel, bOkButtonOnly);
 		}
-		$(elementName).setStyle({width:boxWidth+'px'});
-	}
-	if(boxHeight != null){
-		if(boxHeight.indexOf('%') > -1){
-			percentHeight = parseInt(boxHeight);
-			boxHeight = parseInt((winHeight * percentHeight) / 100);
-		}
-		$(elementName).setStyle({height:boxHeight+'px'});
-	}else{
-		if (Prototype.Browser.IE){	
-			$(elementName).setStyle({height:'1%'});
-		}else{
-			$(elementName).setStyle({height:'auto'});
-		}
-	}
-	
-	// POSITION HORIZONTAL
-	boxWidth = $(elementName).getWidth();	
-	var offsetLeft = (winWidth - parseInt(boxWidth)) / 2;
-	$(elementName).setStyle({left:offsetLeft+'px'});
-	// POSITION VERTICAL
-	var boxHeight = $(elementName).getHeight();
-	var offsetTop = parseInt(((winHeight - boxHeight)/3));
-	$(elementName).setStyle({top:offsetTop+'px'});
-	
-	//if (Prototype.Browser.IE){		
+		this.dialogContent.appendChild(newForm);
 		
-	//}
+		if(fOnComplete)	{
+			newForm.onsubmit = function(){
+				try{
+					fOnComplete();
+				}catch(e){
+					alert('Unexpected Error : please report!\n'+e);				
+				}
+				return false;
+			}
+		}
+		else {
+			newForm.onsubmit = function(){
+				ajaxplorer.actionBar.submitForm(modal.getForm());
+				hideLightBox();
+				return false;
+			};
+		}
+		this.showContent(this.elementName, $(sFormId).getAttribute("box_width"), $(sFormId).getAttribute("box_height"));
+		if($(newForm).getElementsBySelector(".dialogFocus").length)
+		{
+			objToFocus = $(newForm).getElementsBySelector(".dialogFocus")[0];
+			setTimeout('objToFocus.focus()', 500);
+		}
+		if($(newForm).getElementsBySelector(".replace_rep").length)
+		{
+			repDisplay = $(newForm).getElementsBySelector(".replace_rep")[0];
+			repDisplay.innerHTML = ajaxplorer.filesList.getCurrentRep();
+		}
+		if($(newForm).getElementsBySelector(".replace_file").length)
+		{
+			repDisplay = $(newForm).getElementsBySelector(".replace_file")[0];
+			repDisplay.innerHTML = getBaseName(ajaxplorer.filesList.getUserSelection().getUniqueFileName());
+		}
+		this.currentForm = newForm;
+		if(fOnLoad != null)
+		{
+			fOnLoad(this.currentForm);
+		}
+		// SAFARI => FORCE IFRAME RELOADING
+		if(reloadIFrame) reloadIFrame.src = reloadIFrameSrc;
+	},
 	
-	displayLightBoxById(elementName);	
+	showContent: function(elementName, boxWidth, boxHeight){
+		ajaxplorer.disableShortcuts();
+		ajaxplorer.disableNavigation();
+		ajaxplorer.filesList.blur();
+		//jQuery('#'+elementName).corner("round top 5px");
+		if(Prototype.Browser.IE){
+			jQuery('#'+elementName + ' .dialogTitle').corner("round top 7px");
+		}else{
+			jQuery('#'+elementName).corner("round top 7px");
+		}
+		if(!this.isRounded)
+		{
+			ajxpCorners($(elementName), 'bottom');
+			this.isRounded = true;		
+		}
+		var winWidth = $(document.body).getWidth();
+		var winHeight = $(document.body).getHeight();
 	
-	// FORCE ABSOLUTE FOR SAFARI!
-	$(elementName).style.position = 'absolute';
-	// REFRESH PNG IMAGES FOR IE!
-	refreshPNGImages(this.dialogContent);
-}
-
-Modal.prototype.getForm = function()
-{
-	return this.currentForm;
-}
-
-Modal.prototype.clearContent = function(object)
-{
-	// REMOVE CURRENT FORM, IF ANY
-	if(object.getElementsBySelector("form").length)
-	{
-		var oThis = this;
-		object.getElementsBySelector("form").each(function(currentForm){
-			if(currentForm.target == 'hidden_iframe' || currentForm.id=='login_form' || currentForm.id=='user_pref_form'){
-				currentForm.hide();
-				oThis.cachedForms.set(currentForm.id,true);
+		// WIDTH / HEIGHT
+		if(boxWidth != null){
+			if(boxWidth.indexOf('%') > -1){
+				percentWidth = parseInt(boxWidth);
+				boxWidth = parseInt((winWidth * percentWidth) / 100);
+			}
+			$(elementName).setStyle({width:boxWidth+'px'});
+		}
+		if(boxHeight != null){
+			if(boxHeight.indexOf('%') > -1){
+				percentHeight = parseInt(boxHeight);
+				boxHeight = parseInt((winHeight * percentHeight) / 100);
+			}
+			$(elementName).setStyle({height:boxHeight+'px'});
+		}else{
+			if (Prototype.Browser.IE){	
+				$(elementName).setStyle({height:'1%'});
+			}else{
+				$(elementName).setStyle({height:'auto'});
+			}
+		}
+		
+		// POSITION HORIZONTAL
+		boxWidth = $(elementName).getWidth();	
+		var offsetLeft = (winWidth - parseInt(boxWidth)) / 2;
+		$(elementName).setStyle({left:offsetLeft+'px'});
+		// POSITION VERTICAL
+		var boxHeight = $(elementName).getHeight();
+		var offsetTop = parseInt(((winHeight - boxHeight)/3));
+		$(elementName).setStyle({top:offsetTop+'px'});
+		
+		//if (Prototype.Browser.IE){		
+			
+		//}
+		
+		displayLightBoxById(elementName);	
+		
+		// FORCE ABSOLUTE FOR SAFARI!
+		$(elementName).style.position = 'absolute';
+		// REFRESH PNG IMAGES FOR IE!
+		refreshPNGImages(this.dialogContent);
+	},
+	
+	getForm: function()	{
+		return this.currentForm;
+	},
+	
+	clearContent: function(object){
+		// REMOVE CURRENT FORM, IF ANY
+		if(object.getElementsBySelector("form").length)
+		{
+			var oThis = this;
+			object.getElementsBySelector("form").each(function(currentForm){
+				if(currentForm.target == 'hidden_iframe' || currentForm.id=='login_form' || currentForm.id=='user_pref_form'){
+					currentForm.hide();
+					oThis.cachedForms.set(currentForm.id,true);
+				}
+				else{
+					object.removeChild(currentForm);
+				}
+			});		
+		}	
+	},
+	
+	addSubmitCancel: function(oForm, fOnCancel, bOkButtonOnly){
+		var contDiv = document.createElement('div');
+		contDiv.className = 'dialogButtons';
+		var okButton = document.createElement('input');
+		okButton.setAttribute('type', 'submit');
+		okButton.setAttribute('name', 'sub');
+		okButton.setAttribute('value', MessageHash[48]);	
+		$(okButton).addClassName('dialogButton');
+		$(okButton).addClassName('dialogFocus');
+		contDiv.appendChild(okButton);
+		if(!bOkButtonOnly)
+		{
+			var caButton = document.createElement('input');
+			caButton.setAttribute('type', 'button');
+			caButton.setAttribute('name', 'can');
+			caButton.setAttribute('value', MessageHash[49]);
+			$(caButton).addClassName('dialogButton');
+			if(fOnCancel){
+				caButton.onclick = function(){fOnCancel();hideLightBox();return false;};
 			}
 			else{
-				object.removeChild(currentForm);
+				caButton.onclick = function(){hideLightBox();return false;};
 			}
-		});		
-	}	
-}
-
-Modal.prototype.addSubmitCancel = function(oForm, fOnCancel, bOkButtonOnly)
-{
-	var contDiv = document.createElement('div');
-	contDiv.className = 'dialogButtons';
-	var okButton = document.createElement('input');
-	okButton.setAttribute('type', 'submit');
-	okButton.setAttribute('name', 'sub');
-	okButton.setAttribute('value', MessageHash[48]);	
-	$(okButton).addClassName('dialogButton');
-	$(okButton).addClassName('dialogFocus');
-	contDiv.appendChild(okButton);
-	if(!bOkButtonOnly)
-	{
-		var caButton = document.createElement('input');
-		caButton.setAttribute('type', 'button');
-		caButton.setAttribute('name', 'can');
-		caButton.setAttribute('value', MessageHash[49]);
-		$(caButton).addClassName('dialogButton');
-		if(fOnCancel){
-			caButton.onclick = function(){fOnCancel();hideLightBox();return false;};
+			contDiv.appendChild(caButton);
+		}	
+		oForm.appendChild(contDiv);
+		oForm.hasButtons = true;
+	},
+	
+	setLoadingStepCounts: function(count){
+		this.loadingStepsCount = count;
+		this.loadingStep = count;
+	},
+	
+	incrementStepCounts: function(add){
+		this.loadingStepsCount += add;
+		this.loadingStep += add;
+	},
+	
+	updateLoadingProgress: function(state){	
+		this.loadingStep --;
+		var percent = (1 - (this.loadingStep / this.loadingStepsCount));
+		var width = parseInt(parseInt($('progressBarBorder').getWidth()) * percent);
+		/*
+		var command = "if($('progressBar')) $('progressBar').style.width = '"+width+"px';";
+		setTimeout(command, 0);
+		*/
+		if(state){
+			$('progressState').value = state;
 		}
-		else{
-			caButton.onclick = function(){hideLightBox();return false;};
-		}
-		contDiv.appendChild(caButton);
-	}	
-	oForm.appendChild(contDiv);
-	oForm.hasButtons = true;
-}
-
-Modal.prototype.setLoadingStepCounts = function(count){
-	this.loadingStepsCount = count;
-	this.loadingStep = count;
-}
-
-Modal.prototype.incrementStepCounts = function(add){
-	this.loadingStepsCount += add;
-	this.loadingStep += add;
-}
-
-Modal.prototype.updateLoadingProgress = function(state)
-{	
-	this.loadingStep --;
-	var percent = (1 - (this.loadingStep / this.loadingStepsCount));
-	var width = parseInt(parseInt($('progressBarBorder').getWidth()) * percent);
-	/*
-	var command = "if($('progressBar')) $('progressBar').style.width = '"+width+"px';";
-	setTimeout(command, 0);
-	*/
-	if(state){
-		$('progressState').value = state;
-	}
-	if($('progressBar')){
-		 /*
-		 $('progressBar').style.width = width+'px';
-		 */
-		 var afterFinishFunc;
-		if(parseInt(percent)==1){
-			afterFinishFunc = function(effect){
-					new Effect.Opacity('loading_overlay', {
-						from:1.0,
-						to:0,
-						duration:0.3,
-						afterFinish:function(effect){
-							$('loading_overlay').remove();
-							if(ajaxplorer) ajaxplorer.actionBar.update();
-						}
-					});
+		if($('progressBar')){
+			 /*
+			 $('progressBar').style.width = width+'px';
+			 */
+			 var afterFinishFunc;
+			if(parseInt(percent)==1){
+				afterFinishFunc = function(effect){
+						new Effect.Opacity('loading_overlay', {
+							from:1.0,
+							to:0,
+							duration:0.3,
+							afterFinish:function(effect){
+								$('loading_overlay').remove();
+								if(ajaxplorer) ajaxplorer.actionBar.update();
+							}
+						});
+				}
 			}
+			 
+			new Effect.Morph('progressBar',{
+				style:'width:'+width + 'px',
+				duration:0.8,
+				afterFinish:afterFinishFunc
+			});
 		}
-		 
-		new Effect.Morph('progressBar',{
-			style:'width:'+width + 'px',
-			duration:0.8,
-			afterFinish:afterFinishFunc
-		});
+		if(this.loadingStep == 0){
+			//$('loading_overlay').remove();
+			this.pageLoading = false;
+		}
+	},
+	
+	setCloseAction: function(func){
+		this.closeFunction = func;
+	},
+	
+	close: function(){	
+		if(this.closeFunction){
+			 this.closeFunction();
+			 this.closeFunction = null;
+		}
 	}
-	if(this.loadingStep == 0){
-		//$('loading_overlay').remove();
-		this.pageLoading = false;
-	}
-}
-
-Modal.prototype.setCloseAction = function(func){
-	this.closeFunction = func;
-}
-
-Modal.prototype.close = function(){	
-	if(this.closeFunction){
-		 this.closeFunction();
-		 this.closeFunction = null;
-	}
-}
-
-var modal = new Modal();
-/*
-Event.observe(window, 'load', function(){
-	modal.init();
 });
-*/
+	
+var modal = new Modal();
