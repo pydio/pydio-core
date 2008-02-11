f927d1309ad5df14647517c4390d12f48eb52230
Mathieu Baudier <mbaudier@argeo.org>
2008-02-11 18:54:49 +0000

    Introduce file upload

--- jdrivers/org.argeo.ajaxplorer.jdrivers.core/src/main/java/org/argeo/ajaxplorer/jdrivers/SimpleAxpDriver.java
+++ jdrivers/org.argeo.ajaxplorer.jdrivers.core/src/main/java/org/argeo/ajaxplorer/jdrivers/SimpleAxpDriver.java
@@ -11,18 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
 
 public class SimpleAxpDriver implements AxpDriver {
 	protected final Log log = LogFactory.getLog(getClass());
 	private Map<String, AxpAction> actions = new TreeMap<String, AxpAction>();
 
 	public AxpAction getAction(HttpServletRequest request) {
+/*
 		log.debug("Request " + request + ", " + request.getMethod() + ", "
 				+ request.getParameterMap() + ", ");
 		InputStream in = null;
 		try {
 			in = request.getInputStream();
-			log.debug("Request:\n"+IOUtils.toString(in));
+			// log.debug("Request:\n"+IOUtils.toString(in));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -39,6 +42,26 @@ public class SimpleAxpDriver implements AxpDriver {
 			log.debug("Attr: " + name + ": " + request.getAttribute(name));
 		}
 
+		if (request instanceof MultipartHttpServletRequest) {
+			MultipartHttpServletRequest mpr = (MultipartHttpServletRequest) request;
+			log.debug("Multipart req: " + mpr);
+			try {
+				
+				for (Object param : mpr.getParameterMap().keySet()) {
+					log.debug("ParamMPR: " + param + "="
+							+ mpr.getParameterMap().get(param));
+				}
+				for (Object param : mpr.getFileMap().keySet()) {
+					log
+							.debug("ParamFile: " + param + "="
+									+ mpr.getFileMap().get(param));
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+*/
 		String action = request.getParameter("get_action");
 		if (action == null) {
 			action = request.getParameter("action");
--- jdrivers/org.argeo.ajaxplorer.jdrivers.core/src/main/java/org/argeo/ajaxplorer/jdrivers/file/FileUploadAction.java
+++ jdrivers/org.argeo.ajaxplorer.jdrivers.core/src/main/java/org/argeo/ajaxplorer/jdrivers/file/FileUploadAction.java
@@ -1,29 +1,49 @@
 package org.argeo.ajaxplorer.jdrivers.file;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.argeo.ajaxplorer.jdrivers.AxpAnswer;
 import org.argeo.ajaxplorer.jdrivers.AxpDriverException;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public class FileUploadAction extends FileAction {
 
 	public AxpAnswer execute(HttpServletRequest request) {
 		log.debug("Execute upload");
+
+		if (!(request instanceof MultipartHttpServletRequest)) {
+			throw new AxpDriverException(
+					"Cann only deal with MultipartHttpServletRequest");
+		}
+		MultipartHttpServletRequest mpr = (MultipartHttpServletRequest) request;
+		String dir = mpr.getParameter("dir");
+		String fileName = mpr.getParameter("Filename");
+
 		InputStream in = null;
+		OutputStream out = null;
 		try {
-			in = request.getInputStream();
-			String str = IOUtils.toString(in);
-			log.debug("File: " + str);
+			MultipartFile file = mpr.getFile("Filedata");
+			in = file.getInputStream();
+			out = new FileOutputStream(getFileDriverContext()
+					.getBasePath()
+					+ dir + File.separator + fileName);
+			IOUtils.copy(in, out);
 			return new AxpUploadAnswer();
 		} catch (IOException e) {
 			throw new AxpDriverException("Cannot upload file.", e);
 		} finally {
 			IOUtils.closeQuietly(in);
+			IOUtils.closeQuietly(out);
 		}
 	}
 
