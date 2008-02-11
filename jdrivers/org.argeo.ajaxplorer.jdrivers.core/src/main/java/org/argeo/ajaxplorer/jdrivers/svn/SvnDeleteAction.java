2dfa1037cbfe304eb3270a9bb784098fa5c26513
Mathieu Baudier <mbaudier@argeo.org>
2008-02-11 22:57:58 +0000

    Introduce delete action

---
+++ jdrivers/org.argeo.ajaxplorer.jdrivers.core/src/main/java/org/argeo/ajaxplorer/jdrivers/svn/SvnDeleteAction.java
@@ -0,0 +1,39 @@
+package org.argeo.ajaxplorer.jdrivers.svn;
+
+import java.io.File;
+
+import org.argeo.ajaxplorer.jdrivers.AjxpDriverException;
+import org.argeo.ajaxplorer.jdrivers.file.FileDeleteAction;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+
+public class SvnDeleteAction extends FileDeleteAction {
+
+	protected final SVNClientManager manager;
+
+	public SvnDeleteAction() {
+		FSRepositoryFactory.setup();
+		manager = SVNClientManager.newInstance();
+	}
+
+	@Override
+	protected void executeDelete(File file) {
+		File baseDir = new File(getFileDriverContext().getBasePath());
+		try {
+			log.debug("SVN Update: " + baseDir);
+			manager.getUpdateClient().doUpdate(baseDir, SVNRevision.HEAD, true);
+			log.debug("SVN Delete: " + file);
+			manager.getWCClient().doDelete(file, true, false);
+			log.debug("SVN Commit: " + baseDir);
+			manager.getCommitClient().doCommit(new File[] { baseDir }, true,
+					"Commit delete of " + file, true, true);
+			log.debug("SVN Update: " + baseDir);
+			manager.getUpdateClient().doUpdate(baseDir, SVNRevision.HEAD, true);
+		} catch (SVNException e) {
+			throw new AjxpDriverException("Cannot delete file " + file, e);
+		}
+	}
+
+}
