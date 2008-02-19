93990258498bf8e56551c324e8d42077bfe89ef8
Charles du Jeu <charles@ajaxplorer.info>
2008-02-19 16:46:53 +0000

    SVN

--- core/src/client/js/ajaxplorer/class.SVNLogger.js
+++ core/src/client/js/ajaxplorer/class.SVNLogger.js
@@ -1,23 +1,26 @@
 SVNLogger = Class.create({
 	initialize:function(form){
 		this.element = form.select('[id="svnlog_box"]')[0];
-		this.template = new Template('<div style="border-bottom:1px solid #79f"><b>Revision :</b> #{revision}<br><b>Author :</b> #{author}<br><b>Date :</b> #{date}<br><b>Message :</b> #{message}</div>');
+		this.template = new Template('<div style="#{cssStyle}"><b>Revision :</b> #{revision} (<a href="content.php?get_action=svndownload&file=#{fileName}&rev=#{revision}">download</a>)<br><b>Author :</b> #{author}<br><b>Date :</b> #{date}<br><b>Message :</b> #{message}</div>');
 	},
 	
 	open: function(fileName){
+		this.fileName = fileName;
 		var connexion = new Connexion();
-		connexion.addParameter('get_action', 'log');
+		connexion.addParameter('get_action', 'svnlog');
 		connexion.addParameter('file', fileName);
 		connexion.onComplete = this.displayResponse.bind(this);
 		connexion.sendAsync();		
 	},
 	
-	addEntry:function(revision,author,date,message){
+	addEntry:function(revision,author,date,message, isLast){
 		this.element.insert(this.template.evaluate({
 			revision:revision,
 			author:author,
 			date:date,
-			message:message
+			message:message,
+			fileName:this.fileName,			
+			cssStyle:(isLast?"":"border-bottom:1px solid #79f")
 		}));
 	},
 	
@@ -42,7 +45,7 @@ SVNLogger = Class.create({
 					message = entry.childNodes[j].firstChild.nodeValue;
 				}
 			}
-			this.addEntry(revision,author,date,message);
+			this.addEntry(revision,author,date,message, (i==log.childNodes.length-1));
 		}
 	}
 });
