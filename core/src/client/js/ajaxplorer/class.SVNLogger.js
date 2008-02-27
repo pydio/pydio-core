1d4a950a87b1566e1b54d1f43c8d52b90fb77bab
Charles du Jeu <charles@ajaxplorer.info>
2008-02-27 18:56:21 +0000

    Use is_file attribute, i18n templates, onLoad state an pane scrolling

--- core/src/client/js/ajaxplorer/class.SVNLogger.js
+++ core/src/client/js/ajaxplorer/class.SVNLogger.js
@@ -1,7 +1,17 @@
 SVNLogger = Class.create({
 	initialize:function(form){
 		this.element = form.select('[id="svnlog_box"]')[0];
-		this.template = new Template('<div style="#{cssStyle}"><b>Revision :</b> #{revision} (<a href="content.php?get_action=svndownload&file=#{fileName}&rev=#{revision}">download</a>)<br><b>Author :</b> #{author}<br><b>Date :</b> #{date}<br><b>Message :</b> #{message}</div>');
+		this.container = new Element('div', {
+			style:"height:250px;overflow:auto;border:1px solid #ddd;"
+		});
+		this.element.insert(this.container);
+		this.template = new Template('<div style="#{cssStyle}"><b>#{revString} :</b> #{revision} #{downloadLink}<br><b>#{authString} :</b> #{author}<br><b>#{dateString} :</b> #{date}<br><b>#{messString} :</b> #{message}</div>');
+		this.downloadTemplate = new Template('(<a href="content.php?get_action=svndownload&file=#{fileName}&rev=#{revision}">#{downloadString}</a>)');
+		this.revMessage = MessageHash[243];
+		this.authorMessage = MessageHash[244];
+		this.dateMessage = MessageHash[245];
+		this.messMessage = MessageHash[246];
+		this.downMessage = MessageHash[88];
 	},
 	
 	open: function(fileName){
@@ -10,17 +20,27 @@ SVNLogger = Class.create({
 		connexion.addParameter('get_action', 'svnlog');
 		connexion.addParameter('file', fileName);
 		connexion.onComplete = this.displayResponse.bind(this);
-		connexion.sendAsync();		
+		this.setOnLoad();
+		connexion.sendAsync();
 	},
 	
-	addEntry:function(revision,author,date,message, isLast){
-		this.element.insert(this.template.evaluate({
+	addEntry:function(revision,author,date,message, isFile, isLast){
+		var dLink = (isFile?this.downloadTemplate.evaluate({
+			fileName:this.fileName, 
+			revision:revision,
+			downloadString:this.downMessage
+		}):"");		
+		this.container.insert(this.template.evaluate({
 			revision:revision,
 			author:author,
 			date:date,
 			message:message,
-			fileName:this.fileName,			
-			cssStyle:(isLast?"":"border-bottom:1px solid #79f")
+			downloadLink:dLink,			
+			cssStyle:(isLast?"padding:5px":"padding:5px;border-bottom:1px solid #79f"),
+			revString:this.revMessage,
+			authString:this.authorMessage,
+			dateString:this.dateMessage,
+			messString:this.messMessage
 		}));
 	},
 	
@@ -35,6 +55,8 @@ SVNLogger = Class.create({
 		for(var i=0; i<log.childNodes.length;i++){
 			var entry = log.childNodes[i];
 			var revision = entry.getAttribute("revision");
+			var isFile = entry.getAttribute("is_file");
+			isFile = (isFile=='1'?true:false);
 			var author;var date;var message;
 			for(var j=0;j<entry.childNodes.length;j++){
 				if(entry.childNodes[j].nodeName == 'author'){
@@ -45,7 +67,20 @@ SVNLogger = Class.create({
 					message = entry.childNodes[j].firstChild.nodeValue;
 				}
 			}
-			this.addEntry(revision,author,date,message, (i==log.childNodes.length-1));
+			this.addEntry(revision,author,date,message, isFile, (i==log.childNodes.length-1));
 		}
+		this.removeOnLoad();
+	},
+	close:function(){
+		
+	},
+	setOnLoad:function(){
+		addLightboxMarkupToElement(this.container);
+		var img = document.createElement("img");
+		img.src = ajxpResourcesFolder+'/images/loadingImage.gif';
+		this.container.getElementsBySelector("#element_overlay")[0].appendChild(img);		
+	},
+	removeOnLoad:function(){
+		removeLightboxFromElement(this.container);
 	}
 });
