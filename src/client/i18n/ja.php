150115c39922762585e350de9f1f1073d4fce672
Charles du Jeu <charles@ajaxplorer.info>
2009-02-07 17:10:34 +0000

    New strings

--- src/client/i18n/ja.php
+++ src/client/i18n/ja.php
@@ -317,6 +317,8 @@ $mess=array(
 "281" => "上限", 
 "282" => "ファイル毎のサイズ", 
 "283" => "合計サイズ", 
-"284" => "ファイル数" 
+"284" => "ファイル数" ,
+"285" => "Cannot find user, please try again.\\n Make sur your Caps Lock is not engaged!",
+"286" => "Your user is out of date with the current version, please use the upgrade procedure."
 ); 
 ?>
