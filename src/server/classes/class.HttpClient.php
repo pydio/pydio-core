6eff578e0327d62ee725018d53c46e162c75ba09
Charles du Jeu <charles@ajaxplorer.info>
2009-02-23 18:41:54 +0000

    "Legal" header in every code files

--- src/server/classes/class.HttpClient.php
+++ src/server/classes/class.HttpClient.php
@@ -1,8 +1,8 @@
 <?php
-
-/* Version 0.9, 6th April 2003 - Simon Willison ( http://simon.incutio.com/ )
-   Manual: http://scripts.incutio.com/httpclient/
-*/
+/**
+ * Version 0.9, 6th April 2003 - Simon Willison ( http://simon.incutio.com/ )
+ * Manual: http://scripts.incutio.com/httpclient/
+ */
 
 class HttpClient {
     // Request vars
