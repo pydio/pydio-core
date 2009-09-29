a0c178b0e7b670e7a6eb260161b93d191a1b77fa
Charles du Jeu <charles@ajaxplorer.info>
2009-09-29 16:17:42 +0000

    Public Links  : copy some basic images in the PUBLIC folder and apply styles to the password form.

--- server/classes/class.AbstractAccessDriver.php
+++ server/classes/class.AbstractAccessDriver.php
@@ -114,6 +114,10 @@ class AbstractAccessDriver extends AbstractDriver {
     	if(!defined('PUBLIC_DOWNLOAD_FOLDER') || !is_dir(PUBLIC_DOWNLOAD_FOLDER)){
     		return "Public URL folder does not exist!";
     	}
+    	if($data["PASSWORD"] && !is_file(PUBLIC_DOWNLOAD_FOLDER."/GradientBg.gif")){
+    		@copy(INSTALL_PATH."/client/images/GradientBg.gif", PUBLIC_DOWNLOAD_FOLDER."/GradientBg.gif");
+    		@copy(INSTALL_PATH."/client/images/locationBg.gif", PUBLIC_DOWNLOAD_FOLDER."/locationBg.gif");
+    	}
         $data["DRIVER_NAME"] = $this->driverName;
         $data["XML_FILE_PATH"] = $this->xmlFilePath;
         $data["REPOSITORY"] = $this->repository;
@@ -169,7 +173,8 @@ class AbstractAccessDriver extends AbstractDriver {
         {
             if ($_POST['password'] != $data["PASSWORD"])
             {
-                echo "<html><body><form method='post'>This file requires a password<br><input type='password' name='password'><input type='submit' value='download'></form></body></html>";
+                echo "<html><body style=\"background-image:url('GradientBg.gif');background-repeat:repeat-x;background-color: #e0ecff; padding:15px;text-align:center;\" align=\"center\"><form method='post' style=\"color:white;font-family:Trebuchet MS, Sans-serif; font-weight:bold;\"><div style=\"font-size:26px;\">AjaXplorer Public Download</div><div style=\"padding:10px 0px;color:#666;\">A password is required for this download :<br><input type='password' name='password' style=\"width: 200px; height:40px; font-size:30px; border: 1px solid #aaa; background-image:url('locationBg.gif');margin-top: 10px;  background-repeat: no-repeat; background-position: top left;\"><br>
+<input type='submit'  style=\"margin-top: 10px; border: 1px solid #aaa;width: 200px; height:40px; font-size:30px;  background-color:#ddd;color:#666;\"  value='Download'></div></form></body></html>";
                 exit();
             }
         }
