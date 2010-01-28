8f2486956403d4313b0b88395596bdd059ab2772
Charles du Jeu <charles@ajaxplorer.info>
2010-01-28 10:06:07 +0000

    Release date.

--- core/src/server/conf/conf.php
+++ core/src/server/conf/conf.php
@@ -34,7 +34,7 @@
  * Description : configuration file
  */
 define("AJXP_VERSION", "2.5.5");
-define("AJXP_VERSION_DATE", "2009/11/02");
+define("AJXP_VERSION_DATE", "2010/01/30");
 
 define("ENABLE_USERS", 1);
 define("ADMIN_PASSWORD", "admin");
