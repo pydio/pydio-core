bebd37c26ba77afff1caa4fb17ebf07da9fdd565
Charles du Jeu <charles@ajaxplorer.info>
2010-03-10 13:59:51 +0000

    Move conf actions to conf driver.

--- server/classes/class.AbstractConfDriver.php
+++ server/classes/class.AbstractConfDriver.php
@@ -125,8 +125,148 @@ class AbstractConfDriver extends AbstractDriver {
 		return (isSet($this->options[$optionName])?$this->options[$optionName]:"");	
 	}
 	
-	
-	// SAVE / EDIT / CREATE / DELETE CONF ??
+		
+	function switchAction($action, $httpVars, $fileVars)
+	{
+		if(!isSet($this->actions[$action])) return;
+		$xmlBuffer = "";
+		foreach($httpVars as $getName=>$getValue){
+			$$getName = Utils::securePath($getValue);
+		}
+		if(isSet($dir) && $action != "upload") $dir = SystemTextEncoding::fromUTF8($dir);
+		$mess = ConfService::getMessages();
+		
+		switch ($action){			
+			//------------------------------------
+			//	SWITCH THE ROOT REPOSITORY
+			//------------------------------------	
+			case "switch_root_dir":
+			
+				if(!isSet($root_dir_index))
+				{
+					break;
+				}
+				$dirList = ConfService::getRootDirsList();
+				if(!isSet($dirList[$root_dir_index]))
+				{
+					$errorMessage = "Trying to switch to an unkown folder!";
+					break;
+				}
+				ConfService::switchRootDir($root_dir_index);
+				if(AuthService::usersEnabled() && AuthService::getLoggedUser()!=null){
+					$user = AuthService::getLoggedUser();
+					$activeRepId = ConfService::getCurrentRootDirIndex();
+					$user->setArrayPref("history", "last_repository", $activeRepId);
+					$user->save();
+				}
+				$logMessage = "Successfully Switched!";
+				AJXP_Logger::logAction("Switch Repository", array("rep. id"=>$root_dir_index));
+				
+			break;	
+									
+			//------------------------------------
+			//	BOOKMARK BAR
+			//------------------------------------
+			case "get_bookmarks":
+				
+				$bmUser = null;
+				if(AuthService::usersEnabled() && AuthService::getLoggedUser() != null)
+				{
+					$bmUser = AuthService::getLoggedUser();
+				}
+				else if(!AuthService::usersEnabled())
+				{
+					$confStorage = ConfService::getConfStorageImpl();
+					$bmUser = $confStorage->createUserObject("shared");
+				}
+				if($bmUser == null) exit(1);
+				if(isSet($_GET["bm_action"]) && isset($_GET["bm_path"]))
+				{
+					if($_GET["bm_action"] == "add_bookmark")
+					{
+						$title = "";
+						if(isSet($_GET["title"])) $title = $_GET["title"];
+						if($title == "" && $_GET["bm_path"]=="/") $title = ConfService::getCurrentRootDirDisplay();
+						$bmUser->addBookMark($_GET["bm_path"], $title);
+					}
+					else if($_GET["bm_action"] == "delete_bookmark")
+					{
+						$bmUser->removeBookmark($_GET["bm_path"]);
+					}
+					else if($_GET["bm_action"] == "rename_bookmark" && isset($_GET["bm_title"]))
+					{
+						$bmUser->renameBookmark($_GET["bm_path"], $_GET["bm_title"]);
+					}
+				}
+				if(AuthService::usersEnabled() && AuthService::getLoggedUser() != null)
+				{
+					$bmUser->save();
+					AuthService::updateUser($bmUser);
+				}
+				else if(!AuthService::usersEnabled())
+				{
+					$bmUser->save();
+				}		
+				AJXP_XMLWriter::header();
+				AJXP_XMLWriter::writeBookmarks($bmUser->getBookmarks());
+				AJXP_XMLWriter::close();
+				exit(1);
+			
+			break;
+					
+			//------------------------------------
+			//	SAVE USER PREFERENCE
+			//------------------------------------
+			case "save_user_pref":
+				
+				$userObject = AuthService::getLoggedUser();
+				if($userObject == null) exit(1);
+				$i = 0;
+				while(isSet($_GET["pref_name_".$i]) && isSet($_GET["pref_value_".$i]))
+				{
+					$prefName = $_GET["pref_name_".$i];
+					$prefValue = stripslashes($_GET["pref_value_".$i]);
+					if($prefName != "password")
+					{
+						$userObject->setPref($prefName, $prefValue);
+						$userObject->save();
+						AuthService::updateUser($userObject);
+						setcookie("AJXP_$prefName", $prefValue);
+					}
+					else
+					{
+						if(isSet($_GET["crt"]) && AuthService::checkPassword($userObject->getId(), $_GET["crt"], false, $_GET["pass_seed"])){
+							AuthService::updatePassword($userObject->getId(), $prefValue);
+						}else{
+							//$errorMessage = "Wrong password!";
+							header("Content-Type:text/plain");
+							print "PASS_ERROR";
+							exit(1);
+						}
+					}
+					$i++;
+				}
+				header("Content-Type:text/plain");
+				print "SUCCESS";
+				exit(1);
+				
+			break;					
+					
+			default;
+			break;
+		}
+		if(isset($logMessage) || isset($errorMessage))
+		{
+			$xmlBuffer .= AJXP_XMLWriter::sendMessage((isSet($logMessage)?$logMessage:null), (isSet($errorMessage)?$errorMessage:null), false);			
+		}
+		
+		if(isset($requireAuth))
+		{
+			$xmlBuffer .= AJXP_XMLWriter::requireAuth(false);
+		}
+				
+		return $xmlBuffer;		
+	}
 	
 
 }
