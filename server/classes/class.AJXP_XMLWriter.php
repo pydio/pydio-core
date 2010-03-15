9c8cd84856ede8742a7eba08d60813720456ebbb
Charles du Jeu <charles@ajaxplorer.info>
2010-03-15 13:24:40 +0000

    xmlEntities if a folder path contains & ...

--- server/classes/class.AJXP_XMLWriter.php
+++ server/classes/class.AJXP_XMLWriter.php
@@ -216,7 +216,7 @@ class AJXP_XMLWriter
 			print("<pref name=\"sidebar_splitter_size\" value=\"".$loggedUser->getPref("sidebar_splitter_size")."\"/>");
 			print("<pref name=\"vertical_splitter_size\" value=\"".$loggedUser->getPref("vertical_splitter_size")."\"/>");
 			print("<pref name=\"history_last_repository\" value=\"".$loggedUser->getArrayPref("history", "last_repository")."\"/>");
-			print("<pref name=\"history_last_listing\" value=\"".stripslashes($loggedUser->getArrayPref("history", ConfService::getCurrentRootDirIndex()))."\"/>");
+			print("<pref name=\"history_last_listing\" value=\"".AJXP_Utils::xmlEntities(stripslashes($loggedUser->getArrayPref("history", ConfService::getCurrentRootDirIndex())))."\"/>");
 			print("<pref name=\"thumb_size\" value=\"".$loggedUser->getPref("thumb_size")."\"/>");
 			print("<pref name=\"columns_size\" value=\"".stripslashes(str_replace("\"", "'", $loggedUser->getPref("columns_size")))."\"/>");
 			print("</preferences>");
