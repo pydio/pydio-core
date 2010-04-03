3722b223dfb8c77481647e32fe5c6708569ae625
Charles du Jeu <charles@ajaxplorer.info>
2010-04-03 13:30:54 +0000

    Move dewplayer inside editor.audio. Move "play_whole_folder" js code from XML file to AudioPreviewer class. New method ajaxplorer.findEditorById(); Implement pre/post processing on server side to transform the "ls" instruction into XSPF playlist format.

--- core/src/plugins/editor.audio/class.AudioPreviewer.js
+++ core/src/plugins/editor.audio/class.AudioPreviewer.js
@@ -40,24 +40,38 @@ Class.create("AudioPreviewer", AbstractEditor, {
 	},
 		
 	getPreview : function(ajxpNode, rich){
-		if(rich){
-			var escapedFilename = escape(encodeURIComponent(ajxpNode.getPath()));
-			
-			var div = new Element('div', {id:"mp3_container", style:"text-align:center; padding:5px; margin-bottom: 5px;"});
+		if(rich){			
+			var escapedFilename = escape(encodeURIComponent(ajxpNode.getPath()));			
+			var player = 'dewplayer-bubble.swf?mp3=content.php?action=audio_proxy%26file='+escapedFilename+'&amp;showtime=1';
+			var playerWidth = '250';
+			var playerHeight = '65';
+			var containerStyle = 'padding:5 0px; margin-bottom: 5px;';
+			if(!rich){
+				player = 'dewplayer.swf?mp3=content.php?action=audio_proxy%26file='+escapedFilename+'&amp;nopointer=1';
+				playerWidth = '40';
+				playerHeight = '20';
+				containerStyle = '';				
+			}
+			var div = new Element('div', {id:"mp3_container", style:"text-align:center;"+containerStyle});
 			var content = '<object type="application/x-shockwave-flash"';
-			content += 'data="'+ajxpResourcesFolder+'/flash/dewplayer-mini.swf?mp3=content.php?action=audio_proxy%26file='+escapedFilename+'&amp;bgcolor=FFFFFF&amp;showtime=1" width="150" height="20">';
+			content += 'data="plugins/editor.audio/'+player+'" width="'+playerWidth+'" height="'+playerHeight+'">';
 			content += '<param name="wmode" value="transparent">';
-			content += '<param name="movie" value="'+ajxpResourcesFolder+'/flash/dewplayer-mini.swf?mp3=content.php?action=audio_proxy%26file='+escapedFilename+'&amp;bgcolor=FFFFFF&amp;showtime=1" />';
+			content += '<param name="movie" value="plugins/editor.audio/'+player+'" />';
 			content += '</object>';
 			div.update(content);
-			div.resizePreviewElement = function(dimensionObject){
-				// do nothing;
-				/*
-				var imgDim = {width:21, height:29};
-				var styleObj = fitRectangleToDimension(imgDim, dimensionObject);
-				img.setStyle(styleObj);
-				*/
-			}
+			if(rich){
+				div.resizePreviewElement = function(dimensionObject){};
+			}else{
+				div.resizePreviewElement = function(dimensionObject){					
+					var imgDim = {width:800, height:400};
+					var styleObj = fitRectangleToDimension(imgDim, dimensionObject);
+					// fix width artificially
+					if(div.getOffsetParent()){
+						styleObj.width = div.getOffsetParent().getWidth()-2 + 'px';
+					}
+					div.setStyle(styleObj);
+				};
+			}			
 			return div;
 		}else{
 			return new Element('img', {src:resolveImageSource(ajxpNode.getIcon(),'/images/crystal/mimes/ICON_SIZE',64)});
@@ -66,6 +80,23 @@ Class.create("AudioPreviewer", AbstractEditor, {
 	
 	getThumbnailSource : function(ajxpNode){
 		return resolveImageSource(ajxpNode.getIcon(),'/images/crystal/mimes/ICON_SIZE',64);
+	},
+	
+	createFolderPlayer : function(ajxpNode){
+		var template = new Template('<head><title>#{window_title}</title></head><body style="margin:0px; padding:10px;"><div style=\"font-family:Trebuchet MS, sans-serif; color:#79f; font-size:15px; font-weight:bold;\">#{window_title}</div><div style="font-family:Trebuchet MS, sans-serif; color:#666; font-size:10px; padding-bottom: 10px;">#{reading_folder}: #{current_folder}</div><object type="application/x-shockwave-flash" data="plugins/editor.audio/dewplayer-playlist.swf?xml=#{playlist_url}&amp;bgcolor=FFFFFF&amp;showtime=true&amp;autoreplay=true&amp;autoplay=true" width="240" height="200"><param name="wmode" value="transparent"><param name="movie" value="plugins/editor.audio/flash/dewplayer-playlist.swf?xml=#{playlist_url}&amp;bgcolor=FFFFFF&amp;showtime=true&amp;autoplay=true&amp;autoplay=true"/></object></body>');
+		
+		newWin = window.open('', 'ajaxplorer_mp3_player_'+Math.random(), 'width=260,height=270,directories=no,location=no,menubar=no,resizeable=yes,scrollbars=no,status=no,toolbar=no');
+		try{
+			var playlist_url = 'content.php?get_action=ls%26skip_history=true%26playlist=true%26dir='+base64_encode(ajxpNode.getPath());
+			newWin.document.write(template.evaluate({
+				window_title : "AjaXplorer MP3 Player",
+				reading_folder : MessageHash[141],
+				playlist_url:playlist_url, 
+				current_folder:ajxpNode.getLabel()}));
+			newWin.document.close();
+		}catch(e){
+			
+		}		
 	}
 	
 });
