8d3f28fa9eaa41089ca545219a588adb6a106eb4
Charles du Jeu <charles@ajaxplorer.info>
2010-04-03 14:33:30 +0000

    Fix various problems. Only problem left is play_whole_folder in IE.

--- core/src/plugins/editor.audio/class.AudioPreviewer.js
+++ core/src/plugins/editor.audio/class.AudioPreviewer.js
@@ -41,21 +41,24 @@ Class.create("AudioPreviewer", AbstractEditor, {
 		
 	getPreview : function(ajxpNode, rich){
 		if(rich){			
-			var escapedFilename = escape(encodeURIComponent(ajxpNode.getPath()));			
-			var player = 'dewplayer-bubble.swf?mp3=content.php?action=audio_proxy%26file='+escapedFilename+'&amp;showtime=1';
+			var escapedFilename = base64_encode(ajxpNode.getPath());
+			var player = 'dewplayer-bubble.swf';
+			var flashVars = 'mp3=content.php?get_action=audio_proxy%26file='+escapedFilename+'&amp;showtime=1';
 			var playerWidth = '250';
 			var playerHeight = '65';
 			var containerStyle = 'padding:5 0px; margin-bottom: 5px;';
 			if(!rich){
-				player = 'dewplayer.swf?mp3=content.php?action=audio_proxy%26file='+escapedFilename+'&amp;nopointer=1';
+				player = 'dewplayer.swf';
+				flashVars = 'mp3=content.php?get_action=audio_proxy%26file='+escapedFilename+'&amp;nopointer=1';
 				playerWidth = '40';
 				playerHeight = '20';
 				containerStyle = '';				
 			}
 			var div = new Element('div', {id:"mp3_container", style:"text-align:center;"+containerStyle});
 			var content = '<object type="application/x-shockwave-flash"';
-			content += 'data="plugins/editor.audio/'+player+'" width="'+playerWidth+'" height="'+playerHeight+'">';
-			content += '<param name="wmode" value="transparent">';
+			content += 'data="plugins/editor.audio/'+player+'" width="'+playerWidth+'" height="'+playerHeight+'" id="dewplayer" name="dewplayer">';
+			content += '<param name="wmode" value="transparent"/>';
+			content += '<param name="flashvars" value="'+flashVars+'"/>';
 			content += '<param name="movie" value="plugins/editor.audio/'+player+'" />';
 			content += '</object>';
 			div.update(content);
@@ -83,19 +86,19 @@ Class.create("AudioPreviewer", AbstractEditor, {
 	},
 	
 	createFolderPlayer : function(ajxpNode){
-		var template = new Template('<head><title>#{window_title}</title></head><body style="margin:0px; padding:10px;"><div style=\"font-family:Trebuchet MS, sans-serif; color:#79f; font-size:15px; font-weight:bold;\">#{window_title}</div><div style="font-family:Trebuchet MS, sans-serif; color:#666; font-size:10px; padding-bottom: 10px;">#{reading_folder}: #{current_folder}</div><object type="application/x-shockwave-flash" data="plugins/editor.audio/dewplayer-playlist.swf?xml=#{playlist_url}&amp;bgcolor=FFFFFF&amp;showtime=true&amp;autoreplay=true&amp;autoplay=true" width="240" height="200"><param name="wmode" value="transparent"><param name="movie" value="plugins/editor.audio/flash/dewplayer-playlist.swf?xml=#{playlist_url}&amp;bgcolor=FFFFFF&amp;showtime=true&amp;autoplay=true&amp;autoplay=true"/></object></body>');
-		
-		newWin = window.open('', 'ajaxplorer_mp3_player_'+Math.random(), 'width=260,height=270,directories=no,location=no,menubar=no,resizeable=yes,scrollbars=no,status=no,toolbar=no');
+		var template = new Template('<head><title>#{window_title}</title></head><body style="margin:0px; padding:10px;"><div style=\"font-family:Trebuchet MS, sans-serif; color:#79f; font-size:15px; font-weight:bold;\">#{window_title}</div><div style="font-family:Trebuchet MS, sans-serif; color:#666; font-size:10px; padding-bottom: 10px;">#{reading_folder}: #{current_folder}</div><object type="application/x-shockwave-flash" data="plugins/editor.audio/dewplayer-playlist.swf" width="240" height="200"><param name="wmode" value="transparent"><param name="movie" value="plugins/editor.audio/flash/dewplayer-playlist.swf"/><param name="flashvars" value="xml=#{playlist_url}&amp;showtime=true&amp;autoreplay=true&amp;autoplay=true"/></object></body>');
+		newWin = window.open('#', '_blank', 'width=260,height=270,directories=no,location=no,menubar=no,resizeable=yes,scrollbars=no,status=no,toolbar=no');
 		try{
 			var playlist_url = 'content.php?get_action=ls%26skip_history=true%26playlist=true%26dir='+base64_encode(ajxpNode.getPath());
 			newWin.document.write(template.evaluate({
 				window_title : "AjaXplorer MP3 Player",
 				reading_folder : MessageHash[141],
 				playlist_url:playlist_url, 
-				current_folder:ajxpNode.getLabel()}));
+				current_folder:ajxpNode.getLabel()
+			}));
 			newWin.document.close();
 		}catch(e){
-			
+			alert(e);
 		}		
 	}
 	
