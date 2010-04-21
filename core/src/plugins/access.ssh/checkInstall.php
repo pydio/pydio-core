cd20ed49414735ad047e92116f6ad8bf70e7f401
Stage Nex <stagenex@users.sourceforge.net>
2010-04-21 06:29:31 +0000

    Fixing issue reported by Tenable Network Security

--- core/src/plugins/access.ssh/checkInstall.php
+++ core/src/plugins/access.ssh/checkInstall.php
@@ -7,6 +7,8 @@
 echo "<html><body><h1>Access restricted</h1>
       Please edit the source code if you want to enable this test</body></html>";
 exit();
+// WARNING: Don't forget to delete this file once the test passed, as it can be 
+// used by for malicious actions 
 /**/
 
 // Testing the SSH installation
