cc941c8b216805c84c21ed27babe01c2a732ce0b
Charles du Jeu <charles@ajaxplorer.info>
2010-05-15 11:54:20 +0000

    Version number

--- src/server/conf/base.conf.php
+++ src/server/conf/base.conf.php
@@ -2,8 +2,8 @@
 /**************************************************/
 /*	ADVANCED : DO NOT CHANGE THESE VARIABLES BELOW
 /**************************************************/
-define("AJXP_VERSION", "2.7.1");
-define("AJXP_VERSION_DATE", "2010/04/04");
+define("AJXP_VERSION", "2.7.2");
+define("AJXP_VERSION_DATE", "2010/05/15");
 
 $installPath = realpath(dirname(__FILE__)."/../..");
 define("INSTALL_PATH", $installPath);
