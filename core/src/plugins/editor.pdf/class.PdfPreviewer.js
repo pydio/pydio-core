c8a61670a390ed1d51bcba951bf5bc212217acbd
Charles du Jeu <charles@ajaxplorer.info>
2010-05-17 20:54:55 +0000

    Implement viewer for PDF files (based on the ImageMagick implementation)

--- core/src/plugins/editor.pdf/class.PdfPreviewer.js
+++ core/src/plugins/editor.pdf/class.PdfPreviewer.js
@@ -32,13 +32,78 @@
  * 
  * Description : The Pdf previewer.
  */
-Class.create("PdfPreviewer", AbstractEditor, {
+Class.create("PdfPreviewer", Diaporama, {
 
 	fullscreenMode: false,
-	
-	initialize: function($super, oFormObject){
-	},
+
+	initialize: function($super, oFormObject)
+	{
+		$super(oFormObject);
+		this.baseUrl = "content.php?get_action=get_extracted_page&file=";
+		// Override onload for the text
+		this.jsImage.onload = function(){
+			this.imgTag.src = this.jsImage.src;
+			this.resizeImage(true);
+			this.downloadButton.removeClassName("disabled");
+			var i = 0;
+			for(i=0;i<this.items.length;i++){
+				if(this.items[i] == this.currentFile){
+					break;
+				}
+			}
+			i++;
+			var text = this.currentPDF + ' ('+MessageHash[331]+' '+i+' '+MessageHash[332]+' '+this.items.length+')';
+			this.updateTitle(text);
+		}.bind(this);
 		
+	},
+	
+	open : function($super, userSelection)
+	{
+		this.downloadButton.onclick = function(){
+			if(!this.currentFile) return;		
+			ajaxplorer.triggerDownload('content.php?action=download&file='+userSelection.getUniqueFileName());
+			return false;
+		}.bind(this);
+				
+		this.currentPDF = getBaseName(userSelection.getUniqueFileName());
+		// Extract the pages and load result!
+		var connexion = new Connexion();
+		connexion.addParameter("get_action", "pdf_data_proxy");
+		connexion.addParameter("all", "true");
+		connexion.addParameter("file", userSelection.getUniqueFileName());
+		connexion.onComplete = function(transport){
+			this.removeOnLoad();
+			var result = transport.responseJSON;
+			this.items = new Array();
+			this.sizes = new Hash();			
+			for(var i=0;i<result.length;i++){
+				this.items.push(result[i].file);
+				this.sizes.set(result[i].file, {height:result[i].height, width:result[i].width});
+			}
+			if(this.items.length){				
+				this.currentFile = this.items[0];
+				this.setZoomValue(100);
+				this.zoomInput.value = '100 %';	
+				this.updateImage();
+				this.updateButtons();			
+				
+				var tItems = this.items;
+				this.element.observe("editor:close", function(){					
+					var connexion = new Connexion();
+					connexion.addParameter("get_action", "delete_pdf_data");
+					var prefix = tItems[0].replace("-0.jpg", "").replace(".jpg", "");
+					connexion.addParameter("file", prefix);
+					connexion.sendAsync();
+				}.bind(this));
+				
+			}
+		}.bind(this);
+		this.setOnLoad();
+		this.updateTitle(MessageHash[330]);
+		connexion.sendAsync();
+	},
+						
 	getPreview : function(ajxpNode){
 		var img = new Element('img', {
 			src:PdfPreviewer.prototype.getThumbnailSource(ajxpNode), 
@@ -57,6 +122,21 @@ Class.create("PdfPreviewer", AbstractEditor, {
 	
 	getThumbnailSource : function(ajxpNode){
 		return ajxpServerAccessPath+"?get_action=pdf_data_proxy&file="+encodeURIComponent(ajxpNode.getPath());
+	},
+	
+	setOnLoad: function()	{
+		if(this.loading) return;
+		addLightboxMarkupToElement(this.imgContainer);
+		var img = document.createElement("img");
+		img.src = ajxpResourcesFolder+'/images/loadingImage.gif';
+		$(this.imgContainer).getElementsBySelector("#element_overlay")[0].appendChild(img);
+		this.loading = true;
+	},
+	
+	removeOnLoad: function(){
+		removeLightboxFromElement(this.imgContainer);
+		this.loading = false;
 	}
+	
 	
 });
