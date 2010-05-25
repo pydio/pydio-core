297c36ca2fbc604dbf5f2f8958f17e3d5af232d1
Charles du Jeu <charles@ajaxplorer.info>
2010-05-25 14:26:17 +0000

    Add "stat" to the API

--- server/conf/compat.php
+++ server/conf/compat.php
@@ -53,4 +53,28 @@ if( !function_exists('json_encode')){
 }
 
 
+if ( !function_exists('json_decode') ){
+	function json_decode($json)
+	{
+		// Author: walidator.info 2009
+		$comment = false;
+		$out = '$x=';
+
+		for ($i=0; $i<strlen($json); $i++)
+		{
+			if (!$comment)
+			{
+				if ($json[$i] == '{')        $out .= ' array(';
+				else if ($json[$i] == '}')    $out .= ')';
+				else if ($json[$i] == ':')    $out .= '=>';
+				else                         $out .= $json[$i];
+			}
+			else $out .= $json[$i];
+			if ($json[$i] == '"')    $comment = !$comment;
+		}
+		eval($out . ';');
+		return $x;
+	}
+}
+
 ?>
