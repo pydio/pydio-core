623c51dd7aadff6a6ec8f8267ff60f83712b6c27
Charles du Jeu <charles@ajaxplorer.info>
2010-06-20 12:19:21 +0000

    Basic implementation of metadata search passed to SearchEngine via ajxpOptions.

--- client/js/ajaxplorer/class.SearchEngine.js
+++ client/js/ajaxplorer/class.SearchEngine.js
@@ -41,12 +41,16 @@ Class.create("SearchEngine", AjxpPane, {
 	state: 'idle',
 	_runningQueries:undefined,
 	_queriesIndex:0,
+	_ajxpOptions:undefined,
 	
 	_queue : undefined,
 
-	initialize: function($super, mainElementName)
+	initialize: function($super, mainElementName, ajxpOptions)
 	{
 		$super($(mainElementName));
+		if(ajxpOptions){
+			this._ajxpOptions = ajxpOptions;
+		}
 		this.initGUI();
 	},
 	
@@ -55,6 +59,18 @@ Class.create("SearchEngine", AjxpPane, {
 		if(!this.htmlElement) return;
 		
 		this.htmlElement.update('<div id="search_form"><input style="float:left;" type="text" id="search_txt" name="search_txt" onfocus="blockEvents=true;" onblur="blockEvents=false;"><a href="" id="search_button" ajxp_message_title_id="184" title="'+MessageHash[184]+'"><img width="16" height="16" align="absmiddle" src="'+ajxpResourcesFolder+'/images/actions/16/search.png" border="0"/></a><a href="" id="stop_search_button" ajxp_message_title_id="185" title="'+MessageHash[185]+'"><img width="16" height="16" align="absmiddle" src="'+ajxpResourcesFolder+'/images/actions/16/fileclose.png" border="0" /></a></div><div id="search_results"></div>');
+		
+		this.metaOptions = [];
+		if(this._ajxpOptions && this._ajxpOptions.metaColumns){
+			var cols = this._ajxpOptions.metaColumns;
+			$('search_form').insert({bottom:'<div id="search_meta">Search on : <span id="search_meta_options"></span></div>'});			
+			this.initMetaOption($('search_meta_options'), 'filename', 'Filename', true);
+			for(var key in cols){
+				this.initMetaOption($('search_meta_options'), key, cols[key]);
+			}
+		}else{
+			$('search_form').insert('<div style="clear:left;height:9px;"></div>');
+		}
 		
 		this._inputBox = $("search_txt");
 		this._resultsBoxId = 'search_results';
@@ -71,7 +87,10 @@ Class.create("SearchEngine", AjxpPane, {
 		
 		this._inputBox.onkeypress = function(e){
 			if (e==null) e = window.event;
-			if(e.keyCode == 13) this.search();
+			if(e.keyCode == 13) {
+				this.search();
+				Event.stop(e);
+			}
 			if(e.keyCode == 9) return false;		
 		}.bind(this);
 		
@@ -117,6 +136,33 @@ Class.create("SearchEngine", AjxpPane, {
 		if(this.htmlElement && this.htmlElement.visible()){
 			this._inputBox.setStyle({width:Math.max((this.htmlElement.getWidth()-70),70) + "px"});
 		}
+	},
+	
+	initMetaOption : function(element, optionValue, optionLabel, checked){
+		var option = new Element('meta_opt', {value:optionValue}).update(optionLabel);
+		if(checked) option.addClassName('checked');
+		if(element.childElements().length) element.insert(', ');
+		element.insert(option);
+		option.observe('click', function(event){
+			option.toggleClassName('checked');
+		});
+		this.metaOptions.push(option);
+	},
+	
+	hasMetaSearch : function(){
+		var found = false;
+		this.metaOptions.each(function(opt){
+			if(opt.getAttribute("value")!="filename" && opt.hasClassName("checked")) found = true;
+		});
+		return found;
+	},
+	
+	getSearchColumns : function(){
+		var cols = $A();
+		this.metaOptions.each(function(opt){
+			if(opt.hasClassName("checked")) cols.push(opt.getAttribute("value"));
+		});
+		return cols;
 	},
 	
 	focus : function(){
@@ -182,7 +228,9 @@ Class.create("SearchEngine", AjxpPane, {
 		}
 	},
 	
-	addResult : function(folderName, fileName, icon){
+	addResult : function(folderName, ajxpNode, metaFound){
+		var fileName = ajxpNode.getLabel();
+		var icon = ajxpNode.getIcon();
 		// Display the result in the results box.
 		if(folderName == "") folderName = "/";
 		var isFolder = false;
@@ -194,7 +242,12 @@ Class.create("SearchEngine", AjxpPane, {
 			folderName += fileName;
 		}	
 		var imageString = '<img align="absmiddle" width="16" height="16" src="'+ajxpResourcesFolder+'/images/mimes/16/'+icon+'"> ';
-		var stringToDisplay = fileName;	
+		var stringToDisplay;
+		if(metaFound){
+			stringToDisplay = fileName + ' (' + this.highlight(metaFound, this.crtText, 20)+ ') ';
+		}else{
+			stringToDisplay = this.highlight(fileName, this.crtText);
+		}
 		
 		var divElement = new Element('div', {title:MessageHash[224]+' '+ folderName}).update(imageString+stringToDisplay);	
 		$(this._resultsBoxId).insert(divElement);
@@ -233,8 +286,8 @@ Class.create("SearchEngine", AjxpPane, {
 			return;
 		}
 		var connexion = new Connexion();
-		connexion.addParameter('get_action', 'ls');
-		connexion.addParameter('options', 'a');
+		connexion.addParameter('get_action', 'ls');				
+		connexion.addParameter('options', 'a' + (this.hasMetaSearch()?'l':''));
 		connexion.addParameter('dir', currentFolder);
 		connexion.onComplete = function(transport){
 			this._parseXmlAndSearchString(transport.responseXML, currentFolder);
@@ -244,39 +297,88 @@ Class.create("SearchEngine", AjxpPane, {
 	},
 	
 	_parseXmlAndSearchString : function(oXmlDoc, currentFolder){
-		if(this._state == 'interrupt')
-		{
+		if(this._state == 'interrupt'){
 			this.updateStateFinished();
 			return;
 		}
-		if( oXmlDoc == null || oXmlDoc.documentElement == null) 
-		{
+		if( oXmlDoc == null || oXmlDoc.documentElement == null){
 			//alert(currentFolder);
-		}
-		else
-		{
-			var root = oXmlDoc.documentElement;
-			// loop through all tree children
-			var cs = root.childNodes;
-			var l = cs.length;
-			for (var i = 0; i < l; i++) 
+		}else{
+			var nodes = XPathSelectNodes(oXmlDoc.documentElement, "tree");
+			for (var i = 0; i < nodes.length; i++) 
 			{
-				if (cs[i].tagName == "tree") 
+				if (nodes[i].tagName == "tree") 
 				{
-					
-					var icon = cs[i].getAttribute('icon');
-					if(cs[i].getAttribute('text').toLowerCase().indexOf(this.crtText) != -1)
+					var node = this.parseAjxpNode(nodes[i]);					
+					this._searchNode(node, currentFolder);
+					if(!node.isLeaf())
 					{
-						this.addResult(currentFolder, cs[i].getAttribute('text'), icon);
-					}
-					if(cs[i].getAttribute('is_file') == "false")
-					{
-						var newPath = cs[i].getAttribute('filename');
-						//this.searchFolderContent(newPath);
+						var newPath = node.getPath();
 						this.appendFolderToQueue(newPath);
 					}
 				}
 			}		
 		}
-	}
+	},
+	
+	_searchNode : function(ajxpNode, currentFolder){
+		var searchFileName = true;
+		var searchCols;
+		if(this.hasMetaSearch()){
+			searchCols = this.getSearchColumns();
+			if(!searchCols.indexOf('filename')){
+				searchFileName = false;
+			}
+		}
+		if(searchFileName && ajxpNode.getLabel().toLowerCase().indexOf(this.crtText) != -1){
+			this.addResult(currentFolder, ajxpNode);
+			return;
+		}
+		if(!searchCols) return;
+		for(var i=0;i<searchCols.length;i++){
+			var meta = ajxpNode.getMetadata().get(searchCols[i]);
+			if(meta && meta.toLowerCase().indexOf(this.crtText) != -1){
+				this.addResult(currentFolder, ajxpNode, meta);
+				return;			
+			}
+		}
+	},
+	
+	parseAjxpNode : function(xmlNode){
+		var node = new AjxpNode(
+			xmlNode.getAttribute('filename'), 
+			(xmlNode.getAttribute('is_file') == "1" || xmlNode.getAttribute('is_file') == "true"), 
+			xmlNode.getAttribute('text'),
+			xmlNode.getAttribute('icon'));
+		var reserved = ['filename', 'is_file', 'text', 'icon'];
+		var metadata = new Hash();
+		for(var i=0;i<xmlNode.attributes.length;i++)
+		{
+			metadata.set(xmlNode.attributes[i].nodeName, xmlNode.attributes[i].nodeValue);
+			if(Prototype.Browser.IE && xmlNode.attributes[i].nodeName == "ID"){
+				metadata.set("ajxp_sql_"+xmlNode.attributes[i].nodeName, xmlNode.attributes[i].nodeValue);
+			}
+		}
+		node.setMetadata(metadata);
+		return node;
+	},
+	
+	highlight : function(haystack, needle, truncate){
+		var start = haystack.toLowerCase().indexOf(needle);
+		var end = start + needle.length;
+		if(truncate && haystack.length > truncate){
+			var midTrunc = Math.round(truncate/2);
+			var newStart = Math.max(Math.round((end + start) / 2 - truncate / 2), 0);
+			var newEnd = Math.min(Math.round((end + start) / 2 + truncate / 2),haystack.length);
+			haystack = haystack.substring(newStart, newEnd);
+			if(newStart > 0) haystack = '...' + haystack;
+			if(newEnd < haystack.length) haystack = haystack + '...';
+			// recompute
+			start = haystack.toLowerCase().indexOf(needle);
+			end = start + needle.length;
+		}
+		var highlight = haystack.substring(0, start)+'<em>'+haystack.substring(start, end)+'</em>'+haystack.substring(end);
+		return highlight;
+	},
+	
 });
