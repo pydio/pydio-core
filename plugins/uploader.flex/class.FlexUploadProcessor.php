5e49cb5f9c12e5a6f5a286028ca2c888ff1bf622
Charles du Jeu <charles@ajaxplorer.info>
2010-07-01 08:58:43 +0000

    Using pre and post processors, no more uploader detection in fsAccess, but dedicated to plugins. Flex resource moved to plugin.

---
+++ plugins/uploader.flex/class.FlexUploadProcessor.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package info.ajaxplorer
+ * 
+ * Copyright 2007-2009 Charles du Jeu
+ * This file is part of AjaXplorer.
+ * The latest code can be found at http://www.ajaxplorer.info/
+ * 
+ * This program is published under the LGPL Gnu Lesser General Public License.
+ * You should have received a copy of the license along with AjaXplorer.
+ * 
+ * The main conditions are as follow : 
+ * You must conspicuously and appropriately publish on each copy distributed 
+ * an appropriate copyright notice and disclaimer of warranty and keep intact 
+ * all the notices that refer to this License and to the absence of any warranty; 
+ * and give any other recipients of the Program a copy of the GNU Lesser General 
+ * Public License along with the Program. 
+ * 
+ * If you modify your copy or copies of the library or any portion of it, you may 
+ * distribute the resulting library provided you do so under the GNU Lesser 
+ * General Public License. However, programs that link to the library may be 
+ * licensed under terms of your choice, so long as the library itself can be changed. 
+ * Any translation of the GNU Lesser General Public License must be accompanied by the 
+ * GNU Lesser General Public License.
+ * 
+ * If you copy or distribute the program, you must accompany it with the complete 
+ * corresponding machine-readable source code or with a written offer, valid for at 
+ * least three years, to furnish the complete corresponding machine-readable source code. 
+ * 
+ * Any of the above conditions can be waived if you get permission from the copyright holder.
+ * AjaXplorer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * Description : Class for handling flex upload
+ */
+class FlexUploadProcessor extends AJXP_Plugin {
+
+	private static $active = false;
+	
+	public function preProcess($action, &$httpVars, &$fileVars){
+		if(isSet($fileVars["Filedata"])){
+			self::$active = true;
+			$httpVars["dir"] = base64_decode($httpVars["dir"]);
+			$fileVars["Filedata"]["name"] = SystemTextEncoding::fromUTF8($fileVars["Filedata"]["name"]);
+			$fileVars["userfile_0"] = $fileVars["Filedata"];
+			unset($fileVars["Filedata"]);
+			AJXP_Logger::debug("Setting FlexProc active");
+		}
+	}	
+	
+	public function postProcess($action, $httpVars, $postProcessData){
+		AJXP_Logger::debug("FlexProc is active=".self::$active, $postProcessData);
+		if(!self::$active){
+			return false;
+		}
+		$result = $postProcessData["processor_result"];
+		if(isSet($result["SUCCESS"]) && $result["SUCCESS"] === true){
+			header('HTTP/1.0 200 OK');
+			//die("200 OK");
+		}else if(isSet($result["ERROR"]) && is_array($result["ERROR"])){
+			$code = $result["ERROR"]["CODE"];
+			$message = $result["ERROR"]["MESSAGE"];
+			
+			//header("HTTP/1.0 $code $message");
+			die("Error $code $message");
+		}
+	}	
+}
+?>
