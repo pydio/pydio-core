488d0fb2ea72ddf205c7a76f98d885a00b8647d3
Charles du Jeu <charles@ajaxplorer.info>
2010-07-02 10:21:10 +0000

    Low-level security (ajxp_exec, index.html everywhere)

--- publicLet.inc.php
+++ publicLet.inc.php
@@ -1,8 +1,9 @@
 <?php
+require_once("server/conf/base.conf.php"); 
+
 require_once("server/classes/class.AJXP_Logger.php"); 
 require_once("server/classes/class.AJXP_Plugin.php"); 
 require_once("server/classes/class.AJXP_PluginsService.php"); 
-require_once("server/conf/base.conf.php"); 
 require_once("server/classes/class.Repository.php"); 
 require_once("server/classes/class.AJXP_Utils.php"); 
 require_once("server/classes/class.SystemTextEncoding.php"); 
@@ -24,6 +25,10 @@ class ConfService
 	public function getConf($str) { if ($str == "USE_HTTPS") return (!empty($_SERVER["HTTPS"])) ? 1 : 0; return NULL; }
 	public function getRepositoryById($id) {return self::$repository;}
 	public function setRepository($repo) {self::$repository = $repo;}
+	public static function getLogDriverImpl(){
+		return null;
+	}
+
 };
 
 /**
--- runTests.php
+++ runTests.php
@@ -37,9 +37,9 @@
  * It is disabled for security purpose
  */
 die("You are not allowed to see this page.");
+require_once("server/conf/base.conf.php");
 
 require_once("server/classes/class.AJXP_Logger.php");
-require_once("server/conf/base.conf.php");
 require_once("server/classes/class.AJXP_Utils.php");
 
 $outputArray = array();
