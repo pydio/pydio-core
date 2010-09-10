1b138a24316b107db4e34e63d09c76b9d8443022
Charles du Jeu <charles@ajaxplorer.info>
2010-09-10 14:46:15 +0000

    Finnish translation - Aleksi Postari

---
+++ plugins/access.mysql/resources/i18n/fi.php
@@ -0,0 +1,21 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2008 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//	Finnish translation by Aleksi Postari
+//	aleksi (at) postari.net
+//	Last update: 07.09.2010
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Luo taulu",
+"2" => "Syötä SQL lauseesi tähän",
+"3"	=> "Hae",
+"4"	=> "Tyhjennä"
+);
+?>
