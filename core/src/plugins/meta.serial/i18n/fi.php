a8d43cce2ebe33ee2721c5778b0eeb2ee96a8e0d
Charles du Jeu <charles@ajaxplorer.info>
2010-09-10 14:46:15 +0000

    Finnish translation - Aleksi Postari

---
+++ core/src/plugins/meta.serial/i18n/fi.php
@@ -0,0 +1,25 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//	Finnish translation by Aleksi Postari
+//	aleksi (at) postari.net
+//	Last update: 07.09.2010
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Tiedoston metatiedot",
+"2" => "Ei nimeä",
+"3" => "Poista luokitus",
+"4" => "Pieni",
+"5" => "Todo",
+"6" => "Henkilökohtainen",
+"7" => "Työ", 
+"8" => "Tärkeä"
+);
+?>
