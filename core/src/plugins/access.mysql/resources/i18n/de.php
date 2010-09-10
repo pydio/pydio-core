f81468c8f2405d51e6bba19366c04d51a0e0f7bd
Charles du Jeu <charles@ajaxplorer.info>
2010-09-10 09:18:59 +0000

    Deutsh translation update.

---
+++ core/src/plugins/access.mysql/resources/i18n/de.php
@@ -0,0 +1,19 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2008 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//	german translation: Axel Otterstätter <axel.otterstaetter@googlemail.com>
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Tabelle erstellen",
+"2" => "SQL Abfrage hier eingeben",
+"3"	=> "Suchen",
+"4"	=> "Löschen"
+);
+?>
