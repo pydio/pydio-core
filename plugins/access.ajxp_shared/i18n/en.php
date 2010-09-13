8460ef07fa6f0ed14571e090f7fc76787ac7ea7c
Charles du Jeu <charles@ajaxplorer.info>
2010-09-13 14:23:25 +0000

    Repository delegation.

---
+++ plugins/access.ajxp_shared/i18n/en.php
@@ -0,0 +1,42 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  Reference dictionnary for translations
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Shared Users",
+"2" => "Shared Repositories",
+"3" => "Public files",
+"4"	=> "File Path",
+"5"	=> "Repository",
+"6"	=> "Password",
+"7"	=> "Expiration",
+"8" => "Elements",
+"9" => "Associated users",
+"10"=> "Associated repositories",
+"11"=> "Are you sure that you want to delete the selected element(s)?",
+"12"=> "You don't have the permission to delete this element.",
+"13"=> "Successfully deleted public file.",
+"14"=> "Integrity",
+"15"=> "Ok",
+"16"=> "Broken",
+"17"=> "Download URL",
+"18"=> "Copy URL",
+"19"=> "Copy url to send by email.",
+"20"=> "Expired",
+"21"=> "Yes",
+"22"=> "No",
+"23"=> "Successfully deleted %s files.",
+"24"=> "Nothing to delete.",
+"25"=> "Clear Expired",
+"26"=> "Delete expired shared files.",
+"27"=> "Owner",
+); 
+?>
