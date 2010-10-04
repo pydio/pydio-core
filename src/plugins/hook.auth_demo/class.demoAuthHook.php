719fa6b60c5c1177bebd138b9a3ede626d77eb23
Charles du Jeu <charles@ajaxplorer.info>
2010-10-04 07:31:19 +0000

    Fix split() call / Move the password change disable in a hook instead of the demo driver, otherwise people can still change password when switching to Shared Elements (ah ah ah).

---
+++ src/plugins/hook.auth_demo/class.demoAuthHook.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package info.ajaxplorer.plugins
+ * 
+ * Copyright 2007-2009 Charles du Jeu
+ * This file is part of AjaXplorer.
+ * The latest code can be found at http://www.ajaxplorer.info/
+ * 
+ * This program is published under the LGPL Gnu Lesser General Public License.
+ * You should have received a copy of the license along with AjaXplorer.
+ * 
+ * The main conditions are as follow : 
+ * You must conspicuously and appropriately publish on each copy distributed 
+ * an appropriate copyright notice and disclaimer of warranty and keep intact 
+ * all the notices that refer to this License and to the absence of any warranty; 
+ * and give any other recipients of the Program a copy of the GNU Lesser General 
+ * Public License along with the Program. 
+ * 
+ * If you modify your copy or copies of the library or any portion of it, you may 
+ * distribute the resulting library provided you do so under the GNU Lesser 
+ * General Public License. However, programs that link to the library may be 
+ * licensed under terms of your choice, so long as the library itself can be changed. 
+ * Any translation of the GNU Lesser General Public License must be accompanied by the 
+ * GNU Lesser General Public License.
+ * 
+ * If you copy or distribute the program, you must accompany it with the complete 
+ * corresponding machine-readable source code or with a written offer, valid for at 
+ * least three years, to furnish the complete corresponding machine-readable source code. 
+ * 
+ * Any of the above conditions can be waived if you get permission from the copyright holder.
+ * AjaXplorer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * Description : The copy of FS driver but with no "write" access
+ */
+class demoAuthHook extends AJXP_Plugin 
+{
+		
+	function filterUsersPref($action, $httpVars, $fileVars){
+		if($action != "save_user_pref") return ;
+		$loggedUser = AuthService::getLoggedUser()->getId();
+		if($loggedUser != "demo") return ;
+		$i = 0;
+		while(isSet($_GET["pref_name_".$i]) && isSet($_GET["pref_value_".$i]))
+		{
+			$prefName = $_GET["pref_name_".$i];
+			$prefValue = stripslashes($_GET["pref_value_".$i]);
+			if($prefName == "password"){
+				throw new Exception("You are not allowed to change the password");
+			}
+			$i++;			
+		}
+	}
+		    
+}
+
+?>
