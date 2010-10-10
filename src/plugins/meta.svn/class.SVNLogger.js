ca47f61138b314353542236228289947a74e3ee8
Charles du Jeu <charles@ajaxplorer.info>
2010-10-10 20:41:43 +0000

    Remove access.remote_svn and access.svn and replace with meta.svn

--- src/plugins/meta.svn/class.SVNLogger.js
+++ src/plugins/meta.svn/class.SVNLogger.js
@@ -32,7 +32,7 @@
  * 
  * Description : Simple display of SVN logs.
  */
-SVNLogger = Class.create({
+Class.create("SVNLogger", {
 	initialize:function(form){
 		this.element = form.select('[id="svnlog_box"]')[0];
 		this.container = new Element('div', {
@@ -40,30 +40,37 @@ SVNLogger = Class.create({
 		});
 		this.element.insert(this.container);
 		this.template = new Template('<div style="#{cssStyle}"><b>#{revString} :</b> #{revision} #{downloadLink}<br><b>#{authString} :</b> #{author}<br><b>#{dateString} :</b> #{date}<br><b>#{messString} :</b> #{message}</div>');
-		this.downloadTemplate = new Template('(<a href="content.php?get_action=svndownload&file=#{fileName}&rev=#{revision}">#{downloadString}</a>)');
+		this.downloadTemplate = new Template('(<a href="content.php?get_action=svndownload&file=#{fileName}&revision=#{revision}">#{downloadString}</a>)');
+		this.switchTemplate = new Template('(<a ajxp_url="content.php?get_action=svnswitch&revision=#{revision}" href="#">#{switchString}</a>)');
 		this.revMessage = MessageHash[243];
 		this.authorMessage = MessageHash[244];
 		this.dateMessage = MessageHash[245];
 		this.messMessage = MessageHash[246];
 		this.downMessage = MessageHash[88];
+		this.switchMessage = 'Switch...';
 	},
 	
-	open: function(fileName){
-		this.fileName = fileName;
+	open: function(){
+		var ajxpNode = ajaxplorer.getUserSelection().getUniqueNode();
+		this.fileName = ajxpNode.getPath();
+		this.isFile = ajxpNode.isLeaf();
 		var connexion = new Connexion();
 		connexion.addParameter('get_action', 'svnlog');
-		connexion.addParameter('file', fileName);
+		connexion.addParameter('file', this.fileName);
 		connexion.onComplete = this.displayResponse.bind(this);
 		this.setOnLoad();
 		connexion.sendAsync();
 	},
 	
-	addEntry:function(revision,author,date,message, isFile, isLast){
-		var dLink = (isFile?this.downloadTemplate.evaluate({
+	addEntry:function(revision,author,date,message, isLast){
+		var dLink = (this.isFile?this.downloadTemplate.evaluate({
 			fileName:this.fileName, 
 			revision:revision,
 			downloadString:this.downMessage
-		}):"");		
+		}):this.switchTemplate.evaluate({
+			revision:revision,
+			switchString:this.switchMessage
+		}));
 		this.container.insert(this.template.evaluate({
 			revision:revision,
 			author:author,
@@ -89,8 +96,6 @@ SVNLogger = Class.create({
 		for(var i=0; i<log.childNodes.length;i++){
 			var entry = log.childNodes[i];
 			var revision = entry.getAttribute("revision");
-			var isFile = entry.getAttribute("is_file");
-			isFile = (isFile=='1'?true:false);
 			var author;var date;var message;
 			for(var j=0;j<entry.childNodes.length;j++){
 				if(entry.childNodes[j].nodeName == 'author'){
@@ -101,8 +106,13 @@ SVNLogger = Class.create({
 					message = entry.childNodes[j].firstChild.nodeValue;
 				}
 			}
-			this.addEntry(revision,author,date,message, isFile, (i==log.childNodes.length-1));
+			this.addEntry(revision,author,date,message, (i==log.childNodes.length-1));
 		}
+		this.container.select("a[@ajxp_url]").invoke("observe", "click", function(e){
+				var conn = new Connexion(e.findElement().getAttribute("ajxp_url"));
+				conn.onComplete = function(){ajaxplorer.fireContextRefresh()};
+				conn.sendAsync();
+			});		
 		this.removeOnLoad();
 	},
 	close:function(){
