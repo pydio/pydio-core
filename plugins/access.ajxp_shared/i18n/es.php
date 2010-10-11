3302bd7aed75515bda6297da151cffce3f57e05f
Charles du Jeu <charles@ajaxplorer.info>
2010-10-11 16:17:29 +0000

    Spanish translation update

---
+++ plugins/access.ajxp_shared/i18n/es.php
@@ -0,0 +1,48 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  Reference dictionnary for translations
+// spanish translation: Ion Rey Bakaikoa <ionrei@gmail.com>, 2010 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Usuarios compartidos",
+"2" => "Repositorios compartidos",
+"3" => "Archivos publicos",
+"4"	=> "Direccion del archivo",
+"5"	=> "Repositorio",
+"6"	=> "Contraseña",
+"7"	=> "Caduca",
+"8" => "Elementos",
+"9" => "Usuarios asociados",
+"10"=> "Repositorios asociados",
+"11"=> "Estas seguro que quieres borrar el(los) elemento(s)selecionado(s)?",
+"12"=> "No tienes permiso para borrar este elemento.",
+"13"=> "Se ha borrado el archivo publico.",
+"14"=> "Integridad",
+"15"=> "Ok",
+"16"=> "Roto",
+"17"=> "Download URL",
+"18"=> "Copiar URL",
+"19"=> "Copiar url para mandarla email.",
+"20"=> "Downloads",
+"21"=> "Si",
+"22"=> "No",
+"23"=> "Se han borrado %s archivos.",
+"24"=> "Nada para borrar.",
+"25"=> "Limpiar caducados",
+"26"=> "Borra archivos caducados.",
+"27"=> "Dueño",
+"28"=> "Archivo compartidos para download directo, con o sin contraseina.",
+"29"=> "Repositorios delegados creados por usuarios.",
+"30"=> "Usuarios creados para acceder a repositorios delegados.",
+"31"=> "Descripcion",
+"32"=> "Poner a cero",
+"33"=> "Poner a cero contador de downloads",
+); 
+?>
---
+++ plugins/meta.serial/i18n/es.php
@@ -0,0 +1,23 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  Reference dictionnary for translations
+// spanish translation: Ion Rey Bakaikoa <ionrei@gmail.com>, 2010 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Etiquetas",
+"2" => "Sin etiqueta",
+"3" => "Sin valorar",
+"4" => "Poca importancia",
+"5" => "Pendiente",
+"6" => "A revisar",
+"7" => "Importante", 
+"8" => "Urgente"
+);
+?>
