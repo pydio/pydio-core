5164e58fe2ea3aa43ce6c9567bd6c0ac6b06f6b5
Charles du Jeu <charles@ajaxplorer.info>
2010-10-11 16:17:29 +0000

    Spanish translation update

---
+++ core/src/plugins/access.ajxp_conf/i18n/es.php
@@ -0,0 +1,77 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  Reference dictionnary for translations
+// spanish translation: Ion Rey Bakaikoa <ionrei@gmail.com>, 2010 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Datos de configuracion",
+"2" => "Usuarios",
+"3" => "Repositorios",
+"4" => "Logs",
+"5" => "Diagnostico",
+"6" => "Nombre de usuario",
+"7" => "Es Admin", 
+"8" => "Nombre de repositorio",
+"9" => "Modo de acceso",
+"10" => "Meta fuentes",
+"11" => "Nueva fuente",
+"12" => "Meta Plugin",
+"13" => "Estas seguro de borrar esta fuente?",
+"14" => "Cierto",
+"15" => "Falso",
+"16" => "Fle Date",
+"17" => "Fecha",
+"18" => "I.P.", 
+"19" => "Nivel",
+"20" => "Usuario", 
+"21" => "Accion", 
+"22" => "Params",
+"23" => "Test Name",
+"24" => "Test Data",
+"25" => "Repositories Access", 
+"26" => "Modificar Password",
+"27" => "Perms. Admin ",
+"28" => "El usuario tiene permisos de admin?", 
+"29" => "Leer", 
+"30" => "Escribir",
+"32" => "Repository Driver",
+"33" => "Cargando...",
+"34" => "Estas seguro de borrar este usuario? ¡Esta accion no es reversible!",
+"35" => "Estas seguro de borrar este repositorio? ¡Esta accion no es reversible!",
+"36" => "Falta de rellenar campos obligatoris!",
+"37" => "No coinciden los datos",
+"38" => "Escribe el nombre de usuario!",
+"39" => "Escribe la contraseina dos veces!", 
+"40" => "Marca la casilla para confirmar!",
+"41" => "Opciones de driver",
+"42" => "Elige un driver!",
+"43" => "Este usuario ya existe, elige un nuevo login",
+"44" => "Creado el usuario",
+"45" => "Cambiado el permiso admin para el usuario ",
+"46" => "Cambiado el permiso del usuario ",
+"47" => "Guardados los datos del usuario ",
+"48" => "Se ha cambiado la contraseina para el usuario ",
+"49" => "No se puede cambiar la contraseina",
+"50" => "Error: existe un repositorio con el mismo nombre",
+"51" => "La carpeta de configuracion no es escribible",
+"52" => "Se ha creado el repositorio",
+"53" => "Error al intentar editar el repositorio", 
+"54" => "Se ha editado el repositorio", 
+"55" => "Por ahora, solo se puede configurar una sola instancia de meta fuente", 
+"56" => "Se ha guardado la meta fuente", 
+"57" => "Se ha borrado la meta fuente",
+"58" => "Se ha editado la meta fuente",
+"59" => "Se ha borrado el repositorio",
+"60" => "Se ha borrado el usuario",
+"61" => "Argumentos equivocados!",
+"62" => "Resumen de permisos",
+"63" => "Todo",
+); 
+?>
