58579295ea3f46bb763ee379e85224d19bd5137b
Charles du Jeu <charles@ajaxplorer.info>
2010-11-04 16:16:47 +0000

    Hungarian translation

---
+++ core/src/plugins/access.ajxp_shared/i18n/hu.php
@@ -0,0 +1,48 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  hungarian translation: Gyarmati Balázs <gyarmati.balazs@gmail.com>
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Megosztott felhasználók",
+"2" => "Megosztott mappák",
+"3" => "Publikus fájlok",
+"4"	=> "Fájl útvonal",
+"5"	=> "Mappa",
+"6"	=> "Jelszó",
+"7"	=> "Lejárati idő",
+"8" => "Elemek",
+"9" => "Hozzárendelt felhasználók",
+"10"=> "Hozzárendelt mappák",
+"11"=> "Biztos, hogy törölni akarod a kiválasztott eleme(ke)t?",
+"12"=> "Nincs jogod törölni ezt az elemet.",
+"13"=> "Fájl publikus hozzáférése törölve.",
+"14"=> "Épség",
+"15"=> "Ok",
+"16"=> "Hibás",
+"17"=> "Letöltési URL",
+"18"=> "URL másolása",
+"19"=> "URL másolása emailben küldéshez.",
+"20"=> "Letöltve",
+"21"=> "Igen",
+"22"=> "Nem",
+"23"=> "%s fájl sikeresen törölve.",
+"24"=> "Nincs mit törölni.",
+"25"=> "Lejártak törlése",
+"26"=> "Lejárt publikus hozzáférése törlése.",
+"27"=> "Tulajdonos",
+"28"=> "Publikus hozzáférésű fájlok, jelszóval vagy anélkül.",
+"29"=> "Felhasználók által létrehozott mappák felhasználói.",
+"30"=> "Felhasználók hozták létre, hogy el lehessen érni a megosztott mappákat.",
+"31"=> "Leírás",
+"32"=> "Nullázás",
+"33"=> "A letöltésszámláló nullázása",
+); 
+?>
