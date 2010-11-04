ef2c143cd2875915967248282ca16cbc67609c65
Charles du Jeu <charles@ajaxplorer.info>
2010-11-04 16:16:47 +0000

    Hungarian translation

---
+++ src/plugins/access.ajxp_conf/i18n/hu.php
@@ -0,0 +1,77 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  hungarian translation: Gyarmati Balázs <gyarmati.balazs@gmail.com>
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Configuration Data",
+"2" => "Felhasználók",
+"3" => "Hozzáférési mappák",
+"4" => "Naplók",
+"5" => "Diagnosztika",
+"6" => "Felhasználónév",
+"7" => "Rendszergazda?", 
+"8" => "Mappa címke",
+"9" => "Hozzáférés típusa",
+"10" => "Meta források",
+"11" => "Forrás hozzáadása",
+"12" => "Meta Plugin",
+"13" => "Biztos, hogy törlöd ezt a forrást?",
+"14" => "Igen",
+"15" => "Nem",
+"16" => "Fájl dátum",
+"17" => "Dátum",
+"18" => "I.P.", 
+"19" => "Szint",
+"20" => "Felhasználó", 
+"21" => "Action", 
+"22" => "Param.",
+"23" => "Teszt név",
+"24" => "Teszt adat",
+"25" => "Mappa hozzáférés", 
+"26" => "Jelszó módosítása",
+"27" => "Rendszergazdai jog",
+"28" => "Van rendszergazdai joga?", 
+"29" => "Írás", 
+"30" => "Olvasás",
+"32" => "Mappa típusa",
+"33" => "Betöltés...",
+"34" => "Biztos, hogy törlöd ezt a felhasználót? Ezt nem lehet visszavonni!",
+"35" => "Biztos, hogy törlöd ezt a mappát? Ezt nem lehet visszavonni!",
+"36" => "Kötelező mezők kitöltetlenek!",
+"37" => "A megadott jelszó és a megerősítése eltérnek egymástól!",
+"38" => "Töltsd ki a bejelentkezési név mezőt!",
+"39" => "Töltsd ki mind a két jelszó mezőt!", 
+"40" => "Kérlek pipáld be a négyzetet a megerősítéshez!",
+"41" => "Mappa driver beállítások",
+"42" => "Válassz egy mappa drivert!",
+"43" => "Ez a felhasználónév már létezik, válassz másikat!",
+"44" => "Felhasználó sikeresen létrehozva",
+"45" => "Ennek a felhasználónak megváltozott a rendszergazdai joga: ",
+"46" => "Ennek a felhasználónak megváltoztak a hozzáférési jogai: ",
+"47" => "Adat mentve ehhez a felhasználóhoz: ",
+"48" => "Ennek a felhasználónak megváltozott a jelszava: ",
+"49" => "Nem tudom módosítani a jelszót",
+"50" => "Hiba: Létezik már ilyen nevű hozzáférési mappa",
+"51" => "A conf mappa nem írható",
+"52" => "Hozzáférési mappa létrehozva",
+"53" => "Hiba a mappa módosításakor", 
+"54" => "A hozzáférési mappa módosult", 
+"55" => "Figyelem! Jelenleg minden meta pluginből csak egyet adhatsz hozzá.", 
+"56" => "Meta forrás sikeresen hozzáadva", 
+"57" => "Meta forrás sikeresen törölve",
+"58" => "Meta forrás sikeresen szerkesztve",
+"59" => "Mappa sikeresen törölve",
+"60" => "Felhasználó sikeresen törölve",
+"61" => "Hibás paraméterek!",
+"62" => "Jog összesítő",
+"63" => "Minden",
+); 
+?>
