f187c66cc2e19bfdd41f1805b58b3e68853ca1b9
Charles du Jeu <charles@ajaxplorer.info>
2010-12-06 10:27:56 +0000

    Spanish update

---
+++ core/src/plugins/editor.openlayer/i18n/es.php
@@ -0,0 +1,16 @@
+<?php
+
+$mess=array(
+"1" => "Mapa OpenLayers",
+"2" => "Filro",
+"3" => "Posición",
+"4" => "Capas",
+"5" => "Antialias",
+"6" => "Formato",
+"7" => "Estilos",
+"8" => "Filtrar",
+"9" => "Buscar",
+"10" => "Limpiar",
+)
+
+?>
