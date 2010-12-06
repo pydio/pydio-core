731661b25a9a58f266bcecd17b79f3c368f56c86
Charles du Jeu <charles@ajaxplorer.info>
2010-12-06 10:27:56 +0000

    Spanish update

---
+++ plugins/access.wms/i18n/es.php
@@ -0,0 +1,12 @@
+<?php
+
+$mess=array(
+"1" => "Nombre",
+"2" => "Título",
+"3" => "Consultable",
+"4" => "Estilo",
+"5" => "Palábras Clave",
+"6" => "Proyección",
+)
+
+?>
---
+++ plugins/editor.exif/i18n/es.php
@@ -0,0 +1,8 @@
+<?php
+
+$mess = array(
+"1" => "Lector Exif", 
+"2" => "Visualizar datos Exif", 
+);
+
+?>
