0259d68e06070b0632afc1db1f7932085e7d5703
Charles du Jeu <charles@ajaxplorer.info>
2010-12-17 08:27:40 +0000

    Fix svn download : no size retrieven case / download inside an iframe.

--- core/src/plugins/meta.svn/class.SVNLogger.js
+++ core/src/plugins/meta.svn/class.SVNLogger.js
@@ -41,7 +41,7 @@ Class.create("SVNLogger", {
 		this.element.up('div.dialogContent').setStyle({padding:0});
 		this.element.insert(this.container);	
 		this.template = new Template('<div style="padding: 0px; border: 1px solid rgb(204, 204, 204); margin: 5px 8px 8px 4px;"><div style="padding: 3px; background-color: rgb(238, 238, 238);#{cssStyle}"><b>#{dateString} :</b> #{date} &nbsp;&nbsp;&nbsp;&nbsp;<b>#{revString} :</b> #{revision} &nbsp;&nbsp;&nbsp;&nbsp;<b>#{authString} :</b> #{author}<br></div><div style="padding: 3px;color:#333;word-wrap:break-word;">#{message}</div><div style="text-align: right; padding: 3px;">#{downloadLink}</div></div>');
-		this.downloadTemplate = new Template('<a style="color:#79f;font-weight:bold;" href="'+window.ajxpServerAccessPath+'&get_action=svndownload&file=#{fileName}&revision=#{revision}">#{downloadString}</a>');
+		this.downloadTemplate = new Template('<a style="color:#79f;font-weight:bold;" ajxp_download="'+window.ajxpServerAccessPath+'&get_action=svndownload&file=#{fileName}&revision=#{revision}" href="#">#{downloadString}</a>');
 		this.switchTemplate = new Template('<a style="color:#79f;font-weight:bold;" ajxp_url="'+window.ajxpServerAccessPath+'&get_action=svnswitch&revision=#{revision}" href="#">#{switchString}</a>');
 		this.revMessage = MessageHash[243];
 		this.authorMessage = MessageHash[244];
@@ -49,6 +49,9 @@ Class.create("SVNLogger", {
 		this.messMessage = MessageHash[246];
 		this.downMessage = MessageHash[88];
 		this.switchMessage = MessageHash['meta.svn.3'];
+		if(!$('svndownload_iframe')){
+			$('hidden_frames').insert('<iframe id="svndownload_iframe" name="svndownload_iframe" style="display:none"></iframe>');
+		}
 	},
 	
 	open: function(currentRep){
@@ -128,39 +131,52 @@ Class.create("SVNLogger", {
 		var logEntries = XPathSelectNodes(root, "log/logentry");
 		this.currentRev = XPathSelectSingleNode(root, "current_revision");
 		this.revisionRange = XPathSelectSingleNode(root, "revision_range");
-		for(var i=0; i<logEntries.length;i++){
-			var entry = logEntries[i];
-			var revision = entry.getAttribute("revision");
-			var author;var date;var message;
-			for(var j=0;j<entry.childNodes.length;j++){
-				if(entry.childNodes[j].nodeName == 'author'){
-					author = entry.childNodes[j].firstChild.nodeValue;
-				}else if(entry.childNodes[j].nodeName == 'date'){
-					date = entry.childNodes[j].firstChild.nodeValue;
-				}else if(entry.childNodes[j].nodeName == 'msg'){
-					message = entry.childNodes[j].firstChild.nodeValue;
-				}
-			}			
-			this.addEntry(revision,author,date,message);
+		try{
+			for(var i=0; i<logEntries.length;i++){
+				var entry = logEntries[i];
+				var revision = entry.getAttribute("revision");
+				var author;var date;var message;
+				for(var j=0;j<entry.childNodes.length;j++){
+					if(entry.childNodes[j].nodeName == 'author'){
+						author = entry.childNodes[j].firstChild.nodeValue;
+					}else if(entry.childNodes[j].nodeName == 'date'){
+						date = entry.childNodes[j].firstChild.nodeValue;
+					}else if(entry.childNodes[j].nodeName == 'msg'){
+						message = entry.childNodes[j].firstChild.nodeValue;
+					}
+				}			
+				this.addEntry(revision,author,date,message);
+			}
+		}catch(e){
+			ajaxplorer.displayMessage("ERROR", e.description + "(current index : "+i+")");
+		}finally{
+			this.removeOnLoad();
 		}
-		this.container.select("a[@ajxp_url]").invoke("observe", "click", function(e){
-				var conn = new Connexion(e.findElement().getAttribute("ajxp_url"));
+		this.container.select("a").invoke("observe", "click", function(e){
+			var a = e.findElement();
+			if(a.getAttribute("ajxp_url")){
+				var conn = new Connexion(a.getAttribute("ajxp_url"));
 				conn.onComplete = function(){
 					ajaxplorer.fireContextRefresh()
 					hideLightBox();
 				};
 				conn.sendAsync();
-			});		
-		this.removeOnLoad();
+			}else if(a.getAttribute("ajxp_download")){
+				$('svndownload_iframe').src = a.getAttribute("ajxp_download");
+			}
+			Event.stop(e);
+		});		
 	},
 	close:function(){
 		
 	},
 	setOnLoad:function(){
 		addLightboxMarkupToElement(this.container);
-		var img = document.createElement("img");
-		img.src = ajxpResourcesFolder+'/images/loadingImage.gif';
-		this.container.getElementsBySelector("#element_overlay")[0].appendChild(img);		
+		var img = new Element("img", {
+			src:ajxpResourcesFolder+'/images/loadingImage.gif',
+			style:'margin-top:80px;'
+		});
+		this.container.down("#element_overlay").insert(img);		
 	},
 	removeOnLoad:function(){
 		removeLightboxFromElement(this.container);
