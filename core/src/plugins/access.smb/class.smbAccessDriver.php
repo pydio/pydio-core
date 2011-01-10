a2aa1ebe926ce3d481c36c68327816e09f36f1ae
Charles du Jeu <charles@ajaxplorer.info>
2011-01-10 12:05:08 +0000

    Access.smb & auth.smb plugin

---
+++ core/src/plugins/access.smb/class.smbAccessDriver.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @package info.ajaxplorer.plugins
+ * 
+ * Copyright 2007-2009 Charles du Jeu
+ * This file is part of AjaXplorer.
+ * The latest code can be found at http://www.ajaxplorer.info/
+ * 
+ * This program is published under the LGPL Gnu Lesser General Public License.
+ * You should have received a copy of the license along with AjaXplorer.
+ * 
+ * The main conditions are as follow : 
+ * You must conspicuously and appropriately publish on each copy distributed 
+ * an appropriate copyright notice and disclaimer of warranty and keep intact 
+ * all the notices that refer to this License and to the absence of any warranty; 
+ * and give any other recipients of the Program a copy of the GNU Lesser General 
+ * Public License along with the Program. 
+ * 
+ * If you modify your copy or copies of the library or any portion of it, you may 
+ * distribute the resulting library provided you do so under the GNU Lesser 
+ * General Public License. However, programs that link to the library may be 
+ * licensed under terms of your choice, so long as the library itself can be changed. 
+ * Any translation of the GNU Lesser General Public License must be accompanied by the 
+ * GNU Lesser General Public License.
+ * 
+ * If you copy or distribute the program, you must accompany it with the complete 
+ * corresponding machine-readable source code or with a written offer, valid for at 
+ * least three years, to furnish the complete corresponding machine-readable source code. 
+ * 
+ * Any of the above conditions can be waived if you get permission from the copyright holder.
+ * AjaXplorer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * Description : The most used and standard plugin : FileSystem access
+ */
+
+defined('AJXP_EXEC') or die( 'Access not allowed');
+include_once(INSTALL_PATH."/plugins/access.smb/smb.php");
+
+class smbAccessDriver extends fsAccessDriver
+{
+	/**
+	* @var Repository
+	*/
+	public $repository;
+	public $driverConf;
+	protected $wrapperClassName;
+	protected $urlBase;
+		
+	function initRepository(){
+		if(is_array($this->pluginConf)){
+			$this->driverConf = $this->pluginConf;
+		}else{
+			$this->driverConf = array();
+		}
+
+		$create = $this->repository->getOption("CREATE");
+		$recycle = $this->repository->getOption("RECYCLE_BIN");
+		
+		$wrapperData = $this->detectStreamWrapper(true);
+		$this->wrapperClassName = $wrapperData["classname"];
+		$this->urlBase = $wrapperData["protocol"]."://".$this->repository->getId();
+		if(!is_dir($this->urlBase)){
+			//throw new AJXP_Exception("Cannot find base path ($this->urlBase) for your repository! Please check the configuration!");
+		}/*
+		if($recycle != ""){
+			if(!is_dir($this->urlBase."/".$recycle)){
+				@mkdir($this->urlBase."/".$recycle);
+				if(!is_dir($this->urlBase."/".$recycle)){
+					throw new AJXP_Exception("Cannot create recycle bin folder. Please check repository configuration or that your folder is writeable!");
+				}
+			}
+			RecycleBinManager::init($this->urlBase, "/".$recycle);
+		}*/
+	}
+}	
+
+?>
