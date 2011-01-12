747d7a4b0f1b5f247dc7f9cabadc095a56682359
Charles du Jeu <charles@ajaxplorer.info>
2011-01-12 20:33:08 +0000

    CodeMirror editor, should probably replace CodePress that is not maintained anymore.

---
+++ src/plugins/editor.codemirror/i18n/en.php
@@ -0,0 +1,27 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  Reference dictionnary for translations
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1"	=> "CodeMirror",
+"2"	=> "CodeMirror Syntax Highlighter",
+"3" => "Wrapping",
+"3b"=> "Toggle lines wrapping",
+"4" => "Numbers",
+"5" => "Toggle lines numbers",
+"6"	=> "Jump to line",
+"7" => "Undo",
+"8"	=> "Redo",
+"9"	=> "Text Search",
+"10" => "Indent size"
+);
+
+?>
