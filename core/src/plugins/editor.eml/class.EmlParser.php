247632291a6dbcff2936f0aa67136b2285dbb29b
Charles du Jeu <charles@ajaxplorer.info>
2011-02-18 16:24:20 +0000

    Mega-trick for changing columns depending on the current folder. I definitely love this architecture!

--- core/src/plugins/editor.eml/class.EmlParser.php
+++ core/src/plugins/editor.eml/class.EmlParser.php
@@ -3,6 +3,7 @@ defined('AJXP_EXEC') or die( 'Access not allowed');
 
 class EmlParser extends AJXP_Plugin{
 	
+	public static $currentListingOnlyEmails;
 	
 	public function switchAction($action, $httpVars, $filesVars){
 		
@@ -98,6 +99,84 @@ class EmlParser extends AJXP_Plugin{
     	}
     	
     	
+	}
+	
+	public function extractMimeHeaders($currentNode, &$metadata, $wrapperClassName, &$realFile){
+		if(!preg_match("/\.eml$/i",$currentNode)){
+			EmlParser::$currentListingOnlyEmails = FALSE;
+			return;
+		}
+		if(EmlParser::$currentListingOnlyEmails === NULL){
+			EmlParser::$currentListingOnlyEmails = true;
+		}
+		if(!isSet($realFile)){
+			$realFile = call_user_func(array($wrapperClassName, "getRealFSReference"), $currentNode);
+		}
+		
+		require_once("Mail/mimeDecode.php");
+    	$params = array(
+    		'include_bodies' => false,
+    		'decode_bodies' => false,
+    		'decode_headers' => true
+    	);    			
+		$mess = ConfService::getMessages();    	
+    	$content = file_get_contents($realFile);
+    	$decoder = new Mail_mimeDecode($content);
+		$structure = $decoder->decode($params);
+		$allowedHeaders = array("to", "from", "subject", "message-id", "mime-version", "date", "return-path");
+		foreach ($structure->headers as $hKey => $hValue){
+			if(!in_array($hKey, $allowedHeaders)) continue;
+			if(is_array($hValue)){
+				$hValue = implode(", ", $hValue);
+			}
+			if($hKey == "date"){
+				$date = strtotime($hValue);
+				$hValue = date($mess["date_format"], $date);
+			}
+			$metadata["eml_".$hKey] = AJXP_Utils::xmlEntities($hValue, true);
+		}
+	}	
+	
+	public function lsPostProcess($action, $httpVars, $outputVars){
+		if(EmlParser::$currentListingOnlyEmails === true){
+			$config = '<columns template_name="eml.list">
+				<column messageId="editor.eml.1" attributeName="ajxp_label" sortType="String"/>
+				<column messageId="editor.eml.2" attributeName="eml_to" sortType="String"/>
+				<column messageId="editor.eml.4" attributeName="eml_date" sortType="Date"/>
+				<column messageId="editor.eml.3" attributeName="eml_subject" sortType="String" fixedWidth="50%"/>
+				<column messageId="2" attributeName="filesize" sortType="NumberKo"/>
+			</columns>';			
+		}else{
+			// Restore standard columns.. 
+			$xmlRegistry = AJXP_PluginsService::getXmlRegistry();
+			$xPath = new DOMXPath($xmlRegistry);
+			$cols = $xPath->query("client_configs/component_config[@className='FilesList']/columns/*");
+			$config = '<columns switchGridMode="filelist">';
+			foreach($cols as $colNode){
+				$xml = $xmlRegistry->saveXML($colNode);
+				$xml = str_replace("additional_column", "column", $xml);
+				$config.=$xml;
+			}
+			$config.= '</columns>';
+		}			
+		$dom = new DOMDocument("1.0", "UTF-8");
+		$dom->loadXML($outputVars["ob_output"]);
+		if(EmlParser::$currentListingOnlyEmails === true){
+			// Replace all text attributes by the "from" value
+			foreach ($dom->documentElement->childNodes as $child){
+				$child->setAttribute("text", $child->getAttribute("eml_from"));
+			}
+		}
+		
+		// Add the columns template definition
+		$insert = new DOMDocument("1.0", "UTF-8");		
+		$config = "<client_configs><component_config className=\"FilesList\">$config</component_config></client_configs>";			
+		$insert->loadXML($config);
+		$imported = $dom->importNode($insert->documentElement, true);
+		$dom->documentElement->appendChild($imported);
+		header('Content-Type: text/xml; charset=UTF-8');
+		header('Cache-Control: no-cache');			
+		print($dom->saveXML());
 	}
 	
 	protected function _findPartByCType($structure, $primary, $secondary){
