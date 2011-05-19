443e852343ccdb864906252829bd76bbb5a626a7
Charles du Jeu <charles@ajaxplorer.info>
2011-05-19 18:04:46 +0000

    clean url before sending to ajaxplorer

--- core/src/plugins/gui.ios/ajxp-ios.js
+++ core/src/plugins/gui.ios/ajxp-ios.js
@@ -1,8 +1,18 @@
+function cleanURL(url){
+	split = url.split("?");
+	url = split[0];
+	if(url.charAt(url.length-1) == "/") {
+		url = url.substring(0,url.length-1);
+	}
+	return url;
+}
+
 document.observe("ajaxplorer:gui_loaded", function(){
 	document.addEventListener("touchmove", function(event){
 		event.preventDefault();
 	});
 	var currentHref = document.location.href;
-	$("ajxpserver-redir").href = currentHref.replace("http://", "ajxpserver://");
+	
+	$("ajxpserver-redir").href = cleanURL(currentHref).replace("http://", "ajxpserver://");
 	$("skipios-redir").href = currentHref + (currentHref.indexOf("?")>-1?"&":"?") + "skipIOS=true";
 });
