e1b7883febcbbd3f812a17a0521723c19e6cca06
Charles du Jeu <charles@ajaxplorer.info>
2011-06-01 09:21:02 +0000

    editor.other cosmetic

--- plugins/editor.other/resources/i18n/en.php
+++ plugins/editor.other/resources/i18n/en.php
@@ -13,6 +13,7 @@
 $mess=array(
 "1" => "Choose other...",
 "2" => "Choose another editor",
-"3" => "Warning, these editors do not declare to be supporting this file type, their behaviour maybe unpredictable!",
+"3" => "Select another editor to open this file",
+"4" => "Warning, these editors do not declare to be supporting this file type, their behaviour maybe unpredictable!",
 ); 
 ?>
