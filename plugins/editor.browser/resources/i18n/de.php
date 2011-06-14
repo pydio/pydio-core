4943266699e5b092c13605901026ab697e93d205
Axel Otterstätter <mr.o@gmx.de>
2011-06-14 09:41:28 +0000

    Updated german translation

---
+++ plugins/editor.browser/resources/i18n/de.php
@@ -0,0 +1,18 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  Reference dictionnary for translations
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Externes Fenster",
+"2" => "Externes Browser Fenster",
+"3" => "Öffne Datei im externen Fenster, bitte warten, dieses Fenster schließt sich automatisch.",
+); 
+?>
---
+++ plugins/editor.other/resources/i18n/de.php
@@ -0,0 +1,19 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+//							
+//	AjaXplorer
+//	
+//	Copyright 2007-2010 Charles du Jeu - LGPL
+//  www.Ajaxplorer.info
+//
+//  Reference dictionnary for translations
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Anderen wählen...",
+"2" => "Anderen Editor wählen",
+"3" => "Einen anderen Editor zum Öffnen dieser Datei wählen",
+"4" => "Achtung dieser Editor ist nicht geeignet zum Öffnen dieses Dateityps, das Verhalten ist nicht vorhersehbar!",
+); 
+?>
