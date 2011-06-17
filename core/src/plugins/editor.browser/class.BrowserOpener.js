03c5c560b666c3e86247b52eae4c0d081f9af6df
Charles du Jeu <charles@ajaxplorer.info>
2011-06-17 15:42:55 +0000

    open .url files by getting their content first

--- core/src/plugins/editor.browser/class.BrowserOpener.js
+++ core/src/plugins/editor.browser/class.BrowserOpener.js
@@ -38,11 +38,28 @@ Class.create("BrowserOpener", AbstractEditor, {
 	
 	open : function($super, userSelection){
         var fileName =  ajaxplorer.getUserSelection().getUniqueFileName();
+        var node = ajaxplorer.getUserSelection().getUniqueNode();
+        if(node.getAjxpMime() == "url"){
+        	this.openURL(fileName);
+        	return;
+        } 
         var repo = ajaxplorer.user.getActiveRepository();
         var url = document.location.href.substring(0, document.location.href.lastIndexOf('/'));
         var nonSecureAccessPath = ajxpServerAccessPath.substring(0, ajxpServerAccessPath.lastIndexOf('?'));
         var open_file_url = url + "/" + nonSecureAccessPath + "?get_action=open_file&repository_id=" + repo + "&file=" + encodeURIComponent(fileName);
         myRef = window.open(open_file_url);
         hideLightBox();
+	},
+	
+	openURL : function(fileName){
+		var connexion = new Connexion();
+		connexion.addParameter('get_action', 'get_content');
+		connexion.addParameter('file', fileName);	
+		connexion.onComplete = function(transp){
+			var url = transp.responseText;
+	        myRef = window.open(url, "AjaXplorer Bookmark", "location=yes,menubar=yes,resizable=yes,scrollbars=yea,toolbar=yes,status=yes");
+	        hideLightBox();
+		}.bind(this);
+		connexion.sendSync();		
 	}
 });
