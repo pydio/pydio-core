0f7002dc351a134790d1ea34322b4325b9c6598f
Axel Otterstätter <mr.o@gmx.de>
2011-06-21 19:30:42 +0000

    Updated german translation

---
+++ plugins/downloader.http/resources/i18n/de.php
@@ -0,0 +1,11 @@
+<?php
+$mess = array(
+	"1" => "Remote",
+	"2" => "Download der Dateien vom Remote-Server",
+	"3" => "o",
+	"4" => "Bitte mindestens eine URL zu einer Datei eingeben, die direkt von einem externen HTTP-Server in den aktuellen Ordner kopiert werden soll. Diese Aktion wird im Hintergrund durchgeführt, so dass während des Downloads mit dem AjaYplorer weiter gearbeitet werden kann.",
+	"5" => "URL",
+	"6" => "Downloading",
+	"7" => "auf Server",
+	"8" => "Datei heruntergeladen, bitte den Client neuladen!",
+);
