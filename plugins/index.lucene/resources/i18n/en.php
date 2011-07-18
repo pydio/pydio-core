4222418f093602ce21c9add67aeaf863fec193eb
Charles du Jeu <charles@ajaxplorer.info>
2011-07-18 11:51:05 +0000

    lucene resources

---
+++ plugins/index.lucene/resources/i18n/en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Charles
+ * Date: 07/07/11
+ * Time: 10:57
+ * To change this template use File | Settings | File Templates.
+ */
+ 
+$mess = array(
+    "1" => "Index Content",
+    "2" => "Index folder content",
+    "3" => "Running indexation in background",
+    "4" => "The index seems to be locked, an indexation processs may be running. If not, please remove the .ajxp_lock file located in cache/indexes.",
+    "5" => "Indexation finished",
+);
