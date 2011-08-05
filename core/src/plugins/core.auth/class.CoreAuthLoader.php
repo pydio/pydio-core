bc1f58b0cda98e4b143b6c64f660ec3ace644c41
Charles du Jeu <charles@ajaxplorer.info>
2011-08-05 09:23:24 +0000

    guest_browsing test still not fixed

--- core/src/plugins/core.auth/class.CoreAuthLoader.php
+++ core/src/plugins/core.auth/class.CoreAuthLoader.php
@@ -3,7 +3,7 @@ class CoreAuthLoader extends AJXP_Plugin{
 	
 	public function getConfigs(){
 		$configs = parent::getConfigs();		
-		$configs["ALLOW_GUEST_BROWSING"] = !isSet($_SERVER["HTTP_AJXP_FORCE_LOGIN"]) && $configs["ALLOW_GUEST_BROWSING"];
+		$configs["ALLOW_GUEST_BROWSING"] = !isSet($_SERVER["HTTP_AJXP_FORCE_LOGIN"]) && ($configs["ALLOW_GUEST_BROWSING"] === "true" || $configs["ALLOW_GUEST_BROWSING"] === true);
 		return $configs;
 	}
 		
