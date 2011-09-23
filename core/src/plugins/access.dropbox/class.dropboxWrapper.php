37ae88c55dad18fd04db033221a31378c4a4d268
Charles du Jeu <charles@ajaxplorer.info>
2011-09-23 09:28:03 +0000

    CHANGE LICENSE TO AGPL

--- core/src/plugins/access.dropbox/class.dropboxWrapper.php
+++ core/src/plugins/access.dropbox/class.dropboxWrapper.php
@@ -1,8 +1,34 @@
 <?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ *
+ */
+defined('AJXP_EXEC') or die( 'Access not allowed');
+
 
 require_once (AJXP_INSTALL_PATH.'/plugins/access.dropbox/dropbox-php/autoload.php');
 require_once (AJXP_BIN_FOLDER.'/interface.AjxpWrapper.php');
 
+/**
+ * @package info.ajaxplorer.plugins
+ * AjxpWrapper encapsulation the PHP Dropbox client
+ */
 class dropboxWrapper implements AjxpWrapper {
 	
 	/**
