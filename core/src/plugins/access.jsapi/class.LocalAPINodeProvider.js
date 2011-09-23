f6c29f2903ba76a733bff29c1f7797953a0b915e
Charles du Jeu <charles@ajaxplorer.info>
2011-09-23 09:47:28 +0000

    CHANGE LICENSE TO AGPL

--- core/src/plugins/access.jsapi/class.LocalAPINodeProvider.js
+++ core/src/plugins/access.jsapi/class.LocalAPINodeProvider.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
 /**
  * A local implementation that explore currently defined
  * classes
@@ -325,7 +344,7 @@ Class.create("LocalAPINodeProvider", {
 	},
 	
 	parseJavadocs : function(content){
-		var reg = new RegExp(/\/\*\*(([^þ*]|\*(?!\/))*)\*\/([\n\r\s\w]*|[\n\r\s]Class|[\n\r\s]Interface)/gi);
+		var reg = new RegExp(/\/\*\*(([^ï¿½*]|\*(?!\/))*)\*\/([\n\r\s\w]*|[\n\r\s]Class|[\n\r\s]Interface)/gi);
 		var keywords = $A(["param", "returns", "var"]);
 		var res = reg.exec(content);
 		var docs = {};
