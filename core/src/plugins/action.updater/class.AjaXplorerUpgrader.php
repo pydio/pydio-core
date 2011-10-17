aeeffbbf58dbb84ffba23692798f91be832457c2
Charles du Jeu <charles@ajaxplorer.info>
2011-10-17 13:55:05 +0000

    Move upgrade mechanism in its dedicated plugin

--- core/src/plugins/action.updater/class.AjaXplorerUpgrader.php
+++ core/src/plugins/action.updater/class.AjaXplorerUpgrader.php
@@ -1,11 +1,25 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Charles
- * Date: 13/10/11
- * Time: 22:25
- * To change this template use File | Settings | File Templates.
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
  */
+
+defined('AJXP_EXEC') or die( 'Access not allowed');
  
 class AjaXplorerUpgrader {
 
@@ -58,6 +72,12 @@ class AjaXplorerUpgrader {
         );
     }
 
+    static function getUpgradePath($url, $format = "php"){
+        $json = file_get_contents($url."?version=".AJXP_VERSION);
+        if($format == "php") return json_decode($json);
+        else return $json;
+    }
+
     function hasNextStep(){
         if($this->step < count($this->steps) && $this->error == NULL){
             $stepValues = array_values($this->steps);
@@ -76,7 +96,7 @@ class AjaXplorerUpgrader {
                 $this->result = "Skipping step, method not found";
             }
         }catch(Exception $e){
-            $this->error = $e;
+            $this->error = $e->getMessage();
         }
         $this->step ++;
     }
---
+++ core/src/plugins/action.updater/i18n/en.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+
+defined('AJXP_EXEC') or die( 'Access not allowed');
+ 
+$mess = array(
+    "1" => "Upgrade...",
+    "2" => "Automatic version upgrade",
+    "3" => "Click on the button to start automatic upgrade procedure. Please check that the application folder is writeable before launching upgrade.",
+    "4" => "Start Upgrade",
+);
