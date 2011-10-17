498223b9b8b7b8172bb9ae1085339b75b49014de
Charles du Jeu <charles@ajaxplorer.info>
2011-10-17 15:23:51 +0000

    New AJXP_PACKAGING constant to differentiate ZIP / LINUX package

--- base.conf.php
+++ base.conf.php
@@ -21,6 +21,7 @@
  * This is the main configuration file for configuring the core of the application.
  * In a standard usage, you should not have to change any variables.
  */
+define("AJXP_PACKAGING", "zip");
 define("AJXP_INSTALL_PATH", realpath(dirname(__FILE__)));
 define("AJXP_CONF_PATH", AJXP_INSTALL_PATH."/conf");
 require_once(AJXP_CONF_PATH."/bootstrap_context.php");
