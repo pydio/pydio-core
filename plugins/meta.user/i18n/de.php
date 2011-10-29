6edc64d00d2e86a773db48a99d6e12c59e2b3ddc
Charles du Jeu <charles@ajaxplorer.info>
2011-10-29 12:37:24 +0000

    rename meta.serial step 1

---
+++ plugins/meta.user/i18n/de.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+ //	german translation: Axel Otterstätter <axel.otterstaetter@googlemail.com>
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Datei Metadaten",
+"2" => "Kein Label",
+"3" => "Bewertung entfernen",
+"4" => "Niedrig",
+"5" => "Todo",
+"6" => "Persönlich",
+"7" => "Arbeit", 
+"8" => "Wichtig"
+);
+?>
---
+++ plugins/meta.user/i18n/fi.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+ //	Finnish translation by Aleksi Postari
+//	aleksi (at) postari.net
+//	Last update: 07.09.2010
+// 
+//---------------------------------------------------------------------------------------------------
+
+$mess=array(
+"1" => "Tiedoston metatiedot",
+"2" => "Ei nimeä",
+"3" => "Poista luokitus",
+"4" => "Pieni",
+"5" => "Todo",
+"6" => "Henkilökohtainen",
+"7" => "Työ", 
+"8" => "Tärkeä"
+);
+?>
