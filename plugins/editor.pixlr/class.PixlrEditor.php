67a257ea3aeeabfa7387214bd5c78aca13d96592
Charles du Jeu <charles@ajaxplorer.info>
2011-11-24 17:40:53 +0000

    Add option for pixlr editor security token, and back to RewriteRule instead of ErrorDocument...

--- plugins/editor.pixlr/class.PixlrEditor.php
+++ plugins/editor.pixlr/class.PixlrEditor.php
@@ -27,84 +27,89 @@ defined('AJXP_EXEC') or die( 'Access not allowed');
  */
 class PixlrEditor extends AJXP_Plugin {
 
-	public function switchAction($action, $httpVars, $filesVars){
-		
-		if(!isSet($this->actions[$action])) return false;
-    	
-		$repository = ConfService::getRepository();
-		if(!$repository->detectStreamWrapper(true)){
-			return false;
-		}
-		
-		$streamData = $repository->streamData;
-    	$destStreamURL = $streamData["protocol"]."://".$repository->getId();
-		    	
-		if($action == "post_to_server"){	
-					
-			$file = base64_decode($httpVars["file"]);
-			$file = SystemTextEncoding::magicDequote(AJXP_Utils::securePath($file));
-			$target = base64_decode($httpVars["parent_url"])."/plugins/editor.pixlr";
-			$tmp = call_user_func(array($streamData["classname"], "getRealFSReference"), $destStreamURL.$file);			
-			$tmp = SystemTextEncoding::fromUTF8($tmp);
-			$fData = array("tmp_name" => $tmp, "name" => urlencode(basename($file)), "type" => "image/jpg");
-			//var_dump($fData);
-			$httpClient = new HttpClient("pixlr.com");
-			//$httpClient->setDebug(true);
-			$postData = array();							
-			$httpClient->setHandleRedirects(false);
-            $secure = $httpVars["secure_token"];
-			$params = array(
-				"referrer"	=> "AjaXplorer",
-				"method"	=> "get",
-				"loc"		=> ConfService::getLanguage(),
-				"target"	=> $target."/fake_save_pixlr_".md5($secure).".php",
-				"exit"		=> $target."/fake_close_pixlr.php",
-				"title"		=> urlencode(basename($file)),
-				"locktarget"=> "false",
-				"locktitle" => "true",
-				"locktype"	=> "source"
-			);
-			$httpClient->postFile("/editor/", $params, "image", $fData);
-			$loc = $httpClient->getHeader("location");
-			header("Location:$loc");
-			
-		}else if($action == "retrieve_pixlr_image"){
-			$file = AJXP_Utils::decodeSecureMagic($httpVars["original_file"]);
-			$url = $httpVars["new_url"];
-			$urlParts = parse_url($url);
-			$query = $urlParts["query"];
+  public function switchAction($action, $httpVars, $filesVars){
+    
+    if(!isSet($this->actions[$action])) return false;
+      
+    $repository = ConfService::getRepository();
+    if(!$repository->detectStreamWrapper(true)){
+      return false;
+    }
+    
+    $streamData = $repository->streamData;
+      $destStreamURL = $streamData["protocol"]."://".$repository->getId();
+          
+    if($action == "post_to_server"){  
+          
+      $file = base64_decode($httpVars["file"]);
+      $file = SystemTextEncoding::magicDequote(AJXP_Utils::securePath($file));
+      $target = base64_decode($httpVars["parent_url"])."/plugins/editor.pixlr";
+      $tmp = call_user_func(array($streamData["classname"], "getRealFSReference"), $destStreamURL.$file);      
+      $tmp = SystemTextEncoding::fromUTF8($tmp);
+      $fData = array("tmp_name" => $tmp, "name" => urlencode(basename($file)), "type" => "image/jpg");
+      //var_dump($fData);
+      $httpClient = new HttpClient("pixlr.com");
+      //$httpClient->setDebug(true);
+      $postData = array();              
+      $httpClient->setHandleRedirects(false);
+      $saveTarget = $target."/fake_save_pixlr.php";
+      if($this->pluginConf["CHECK_SECURITY_TOKEN"]){
+          $saveTarget = $target."/fake_save_pixlr_".md5($httpVars["secure_token"]).".php";
+      }
+      $params = array(
+        "referrer"  => "AjaXplorer",
+        "method"  => "get",
+        "loc"    => ConfService::getLanguage(),
+        "target"  => $saveTarget,
+        "exit"    => $target."/fake_close_pixlr.php",
+        "title"    => urlencode(basename($file)),
+        "locktarget"=> "false",
+        "locktitle" => "true",
+        "locktype"  => "source"
+      );
+      $httpClient->postFile("/editor/", $params, "image", $fData);
+      $loc = $httpClient->getHeader("location");
+      header("Location:$loc");
+      
+    }else if($action == "retrieve_pixlr_image"){
+      $file = AJXP_Utils::decodeSecureMagic($httpVars["original_file"]);
+      $url = $httpVars["new_url"];
+      $urlParts = parse_url($url);
+      $query = $urlParts["query"];
+        if($this->pluginConf["CHECK_SECURITY_TOKEN"]){
             $scriptName = basename($urlParts["path"]);
             $token = str_replace(array("fake_save_pixlr_", ".php"), "", $scriptName);
             if($token != md5($httpVars["secure_token"])){
                 throw new AJXP_Exception("Invalid Token, this could mean some security problem!");
             }
-			$params = array();
-			$parameters = parse_str($query, $params);
+        }
+      $params = array();
+      parse_str($query, $params);
 
-			$image = $params['image'];
-			$headers = get_headers($image, 1);
-			$content_type = explode("/", $headers['Content-Type']);
-			if ($content_type[0] != "image"){
-				throw new AJXP_Exception("File Type");
-			}
-			
-			$orig = fopen($image, "r");
-			$target = fopen($destStreamURL.$file, "w");
-			while(!feof($orig)){
-				fwrite($target, fread($orig, 4096));
-			}
-			fclose($orig);
-			fclose($target);
-			
-			//header("Content-Type:text/plain");
-			//print($mess[115]);
-			
-		}
-		
-		
-		return ;
-				
-	}
-	
+      $image = $params['image'];      
+      $headers = get_headers($image, 1);
+      $content_type = explode("/", $headers['Content-Type']);
+      if ($content_type[0] != "image"){
+        throw new AJXP_Exception("Invalid File Type");
+      }
+      
+      $orig = fopen($image, "r");
+      $target = fopen($destStreamURL.$file, "w");
+      while(!feof($orig)){
+        fwrite($target, fread($orig, 4096));
+      }
+      fclose($orig);
+      fclose($target);
+      
+      //header("Content-Type:text/plain");
+      //print($mess[115]);
+      
+    }
+    
+    
+    return ;
+        
+  }
+  
 }
 ?>
