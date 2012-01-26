e76730c13b1c57f8289e49ebe310924bb5586751
Charles du Jeu <charles@ajaxplorer.info>
2012-01-26 09:48:20 +0000

    Simple "locking" implementation : manually lock/unlock files to other users.

---
+++ plugins/meta.simple_lock/i18n/conf/fr.php
@@ -0,0 +1,8 @@
+<?php
+
+$mess = array(
+"Simple Lock" => "Blocage simple",
+"Manual Locking mechanism to avoid concurrent edition" => "Blocage manuel des fichiers pour éviter l'édition concurrente",
+);
+
+?>
