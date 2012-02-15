2c6b03a10b99b1ac8531b9f1ba1ba8625f35f145
Charles du Jeu <charles@ajaxplorer.info>
2012-02-15 12:35:51 +0000

    Adapt iOS GUI Plugin for Android as well, ability to set the app url via the configurations.

---
+++ core/src/plugins/gui.ios/i18n/en.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+* This file is part of AjaXplorer.
+*
+* AjaXplorer is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* AjaXplorer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://www.ajaxplorer.info/>.
+*/
+$mess=array(
+"1.ios" => "There is a native application available for accessing %s through iPhone/iPad!",
+"2.ios" => "I have it already, open it and add this server to my remote servers",
+"3.ios" => "I don't have it, send me to the App Store now!",
+"4.ios" => "I don't care, let me use the web version of %s.",
+"1.android" => "There is a native AjaXplorer client available for accessing %s on Android OS.",
+"2.android" => "I have it already, open it and add this server to my preconfigured remote servers",
+"3.android" => "I don't have it, send me to the Android Market now!",
+"4.android" => "I don't care, let me use the web version of %s.",
+);
+?>
