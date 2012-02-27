91d9dc45a0bd19b02eea9ac0f4028c9ce56d0dc1
Charles du Jeu <charles@ajaxplorer.info>
2012-02-27 15:23:31 +0000

    Pass alternate videos versions in the metadata, and set them as HTML5 video source (require an external pre-encoding)

--- plugins/editor.video/class.VideoPreviewer.js
+++ plugins/editor.video/class.VideoPreviewer.js
@@ -26,17 +26,31 @@ Class.create("VideoPreviewer", AbstractEditor, {
 		
 	getPreview : function(ajxpNode, rich){
 		if(rich){
-			var escapedFilename = escape(encodeURIComponent(ajxpNode.getPath()));
 			var url = document.location.href;
 			if(url[(url.length-1)] == '/'){
 				url = url.substr(0, url.length-1);
 			}else if(url.lastIndexOf('/') > -1){
 				url = url.substr(0, url.lastIndexOf('/'));
 			}
-			var fileName = url+'/'+ajxpBootstrap.parameters.get('ajxpServerAccess')+'&action=read_video_data&file='+ajxpNode.getPath();
-			
+
+            var html5proxies = $H({});
 			var mime = ajxpNode.getAjxpMime();
-			if(mime == "mp4" || mime == "webm" || mime == "ogv"){
+            if(mime == "mp4" || mime == "webm" || mime == "ogv"){
+                html5proxies.set(mime, ajxpNode.getPath());
+            }
+            var meta = ajxpNode.getMetadata();
+            if(meta.get("video_altversion_mp4")){
+                html5proxies.set('mp4', meta.get("video_altversion_mp4"));
+            }
+            if(meta.get("video_altversion_webm")){
+                html5proxies.set('webm', meta.get("video_altversion_webm"));
+            }
+            if(meta.get("video_altversion_ogv")){
+                html5proxies.set('ogv', meta.get("video_altversion_ogv"));
+            }
+
+			//if(mime == "mp4" || mime == "webm" || mime == "ogv"){
+            if(html5proxies.keys().length){
 				// Problem : some embedded HTML5 readers do not send the cookies!
 				if(!window.crtAjxpSessid){
 					var connexion = new Connexion();
@@ -47,7 +61,7 @@ Class.create("VideoPreviewer", AbstractEditor, {
 					};
 					connexion.sendSync();
 				}
-				fileName += '&ajxp_sessid='+window.crtAjxpSessid; 
+				var sessidPart = '&ajxp_sessid='+window.crtAjxpSessid;
 
 				var types = {
 					mp4:'video/mp4; codecs="avc1.42E01E, mp4a.40.2"',
@@ -57,16 +71,20 @@ Class.create("VideoPreviewer", AbstractEditor, {
 				var poster = resolveImageSource(ajxpNode.getIcon(),'/images/mimes/ICON_SIZE',64);
 				var div = new Element("div", {className:"video-js-box"});
 				var content = '';
-				content +='<!-- Using the Video for Everybody Embed Code http://camendesign.com/code/video_for_everybody -->';
 				content +='	<video class="video-js" controls preload="auto" height="200">';
-				content +='		<source src="'+fileName+'" type=\''+types[mime]+'\' />';
-				content +='      <!-- Flash Fallback. Use any flash video player here. Make sure to keep the vjs-flash-fallback class. -->';
-				
+                var flashName;
+                html5proxies.each(function(pair){
+                    var fname = url+'/'+ajxpBootstrap.parameters.get('ajxpServerAccess')+'&action=read_video_data'+sessidPart+'&file='+pair.value;
+                    if(!flashName){
+                        flashName = encodeURIComponent(fname);
+                    }
+                    content +='		<source src="'+fname+'" type=\''+types[pair.key]+'\' />';
+                });
 				content += '<object type="application/x-shockwave-flash" class="vjs-flash-fallback" data="plugins/editor.video/player_flv_maxi.swf" width="100%" height="200">';
 				content += '	<param name="movie" value="plugins/editor.video/player_flv_maxi.swf" />';
 				content += '	<param name="quality" value="high">';
 				content += '	<param name="allowFullScreen" value="true" />';
-				content += '	<param name="FlashVars" value="flv='+url+'/'+ajxpBootstrap.parameters.get('ajxpServerAccess')+'%26action=download%26file='+escapedFilename+'&showstop=1&showvolume=1&showtime=1&showfullscreen=1&playercolor=676965&bgcolor1=f1f1ef&bgcolor2=f1f1ef&buttonovercolor=000000&sliderovercolor=000000" />';
+				content += '	<param name="FlashVars" value="flv='+flashName+'&showstop=1&showvolume=1&showtime=1&showfullscreen=1&playercolor=676965&bgcolor1=f1f1ef&bgcolor2=f1f1ef&buttonovercolor=000000&sliderovercolor=000000" />';
 				content += '</object>';
 								
 				content +='	</video>';
@@ -94,12 +112,13 @@ Class.create("VideoPreviewer", AbstractEditor, {
 				}
 				
 			}else{
+                var f = encodeURIComponent(url+'/'+ajxpBootstrap.parameters.get('ajxpServerAccess')+'&action=read_video_data&file='+ajxpNode.getPath());
 				var div = new Element('div', {id:"video_container", style:"text-align:center; margin-bottom: 5px;"});
 				var content = '<object type="application/x-shockwave-flash" data="plugins/editor.video/player_flv_maxi.swf" width="100%" height="200">';
 				content += '	<param name="movie" value="plugins/editor.video/player_flv_maxi.swf" />';
 				content += '	<param name="quality" value="high">';
 				content += '	<param name="allowFullScreen" value="true" />';
-				content += '	<param name="FlashVars" value="flv='+url+'/'+ajxpBootstrap.parameters.get('ajxpServerAccess')+'%26action=download%26file='+escapedFilename+'&showstop=1&showvolume=1&showtime=1&showfullscreen=1&playercolor=676965&bgcolor1=f1f1ef&bgcolor2=f1f1ef&buttonovercolor=000000&sliderovercolor=000000" />';
+				content += '	<param name="FlashVars" value="flv='+f+'&showstop=1&showvolume=1&showtime=1&showfullscreen=1&playercolor=676965&bgcolor1=f1f1ef&bgcolor2=f1f1ef&buttonovercolor=000000&sliderovercolor=000000" />';
 				content += '</object>';
 				div.update(content);
 				div.resizePreviewElement = function(dimensionObject){
