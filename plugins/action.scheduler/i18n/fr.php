c6df8ebf4d2ba3699f853b970528a195a1bceb72
Charles du Jeu <charles@ajaxplorer.info>
2012-06-22 12:00:41 +0000

    [action.scheduler] New scheduler plugin

---
+++ plugins/action.scheduler/i18n/fr.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+$mess = array(
+    "1" => "Action",
+    "1d" => "Action que cette tâche doit déclencher",
+    "2" => "Plannification",
+    "2d" => "Expression de type Crontab au format suivant : minutes heures jours joursDeSemaine mois. Vous pouvez utiliser des wildcards (*), des pas (,) et des intervalles (-), ainsi que le diviseur.",
+    "3" => "Prochaine execution",
+    "4" => "ID Dépôt",
+    "4s" => "Dépôt",
+    "4d" => "Id du dépôt cible",
+    "5" => "Paramètres",
+    "5d" => "Entrez autant de paramêtres que nécessaires pour cette action",
+    "6" => "Clé",
+    "6d" => "Nom du parametre",
+    "7" => "Valeur",
+    "7d" => "Valeur associée",
+    "8" => "Nouvelle tâche",
+    "9" => "Plannifier une tâche qui sera executée régulièrement",
+    "10" => "Editer tâche",
+    "11" => "Editer les parametres et la plannification de la tâche",
+    "12" => "Libellé",
+    "12d"=> "Libellé de la tâche",
+    "13" => "Statut",
+    "14" => "Dernière execution",
+    "15"=> "Démarrer",
+    "15d" => "Démarrer le plannificateur pour faire tourner les tâches",
+    "16" => "Lancer les tâches",
+    "16d" => "Démarrer le plannificateur pour faire tourner les tâches",
+    "17" => "Utilisateur(s)",
+    "17d" => "Entre une liste d'utilisateur, ou un wildcard * pour lancer la tâche sur chaque utilisateur.",
+    "18"  => "Plannificateur",
+    "18d" => "Actions du plannificateur",
+    "19" => "Supprimer",
+    "19d" => "Déplannifier et supprimer la tâche",
+    "20" => "Expression CRON",
+    "20d" => "Generer l'expression CRON",
+    "21" => "Ajoutez la ligne suivante dans votre crontab, en remplaçant votre mot de passe, et éventuellement l'expression de plannification (ici configurées pour déclencher le script toutes les 5 minutes).",
+);
