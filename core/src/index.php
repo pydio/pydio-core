d544c6b27dde5bb474e506bdb34ec2e2b2a46a2f
Charles du Jeu <charles@ajaxplorer.info>
2012-06-24 14:28:30 +0000

    Do not catch STRICT errors

--- core/src/index.php
+++ core/src/index.php
@@ -43,7 +43,7 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-cache, must-revalidate");
 header("Pragma: no-cache");
 require_once(AJXP_BIN_FOLDER."/class.AJXP_Logger.php");
-set_error_handler(array("AJXP_XMLWriter", "catchError"), E_ALL & ~E_NOTICE );
+set_error_handler(array("AJXP_XMLWriter", "catchError"), E_ALL & ~E_NOTICE & ~E_STRICT );
 set_exception_handler(array("AJXP_XMLWriter", "catchException"));
 $pServ = AJXP_PluginsService::getInstance();
 ConfService::init();
