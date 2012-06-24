15eeeaf922c620e7ea03eb2e3cc65fc640f2d5fe
Charles du Jeu <charles@ajaxplorer.info>
2012-06-24 17:04:11 +0000

    Print message

--- dist/php/4.2.0.php
+++ dist/php/4.2.0.php
@@ -4,3 +4,5 @@ if(is_file(AJXP_INSTALL_PATH."/conf/bootstrap_context.php".".new-".date("Ymd")))
     rename(AJXP_INSTALL_PATH."/conf/bootstrap_context.php", AJXP_INSTALL_PATH."/conf/bootstrap_context.php.orig");
     rename(AJXP_INSTALL_PATH."/conf/bootstrap_context.php".".new-".date("Ymd"), AJXP_INSTALL_PATH."/conf/bootstrap_context.php");
 }
+
+echo "The bootstrap_context was replaced by the new version, the .orig version is kept.";
