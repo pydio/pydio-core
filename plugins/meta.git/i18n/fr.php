e5204129ce824ae031b2cb79da324171e5223dbe
Charles du Jeu <charles@ajaxplorer.info>
2012-07-05 21:58:05 +0000

    French

---
+++ plugins/meta.git/i18n/fr.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+
+defined('AJXP_EXEC') or die('Access not allowed');
+
+$mess = array(
+    "1" => "Versions",
+    "2" => "Précédentes versions du document",
+    "3" => "Télécharger",
+    "4" => "Télécharger cette version sur votre ordinateur",
+    "5" => "Voir (navigateur)",
+    "6" => "Ouvrir la version sélectionnée dans le navigateur",
+    "7" => "Revenir à...",
+    "8" => "Revenir à la version sélectionnée",
+    "9" => "#",
+    "10" => "Date",
+    "11" => "Auteur",
+    "12" => "Modification",
+    "13" => "Êtes-vous sûr? Cette opération va créer une nouvelle version du fichier avec le contenu de la révision sélectionnée.",
+    "RENAME"        => "Fichier renommé",
+    "MOVE"          => "Déplacement",
+    "MODIFICATION"  => "Contenu modifié",
+    "DELETION"      => "Fichier supprimé",
+    "CREATION"      => "Fichier créé"
+);
