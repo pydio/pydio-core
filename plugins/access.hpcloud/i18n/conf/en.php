4f8777dffe103a926bdb95e76afd15137118273c
Charles du Jeu <charles@ajaxplorer.info>
2012-07-27 15:26:46 +0000

    [access.hpcloud] First implementation of HPCloud Object Storage access, based on their PHP bindings + patch. See if possible to send back the changes.

---
+++ plugins/access.hpcloud/i18n/conf/en.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+* This file is part of AjaXplorer.
+*
+* AjaXplorer is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* AjaXplorer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://www.ajaxplorer.info/>.
+*/
+$mess=array(
+"S3 (Amazon Web Service)" => "S3 (Amazon Web Service)",
+"Access an AWS server. Requires AWS-SDK for PHP" => "Access an AWS server. Requires AWS-SDK for PHP ",
+"Key" => "Key",
+"S3 Api Key" => "S3 Api Key",
+"Secret Key" => "Secret Key",
+"S3 secret key" => "S3 secret key",
+"Region" => "Region",
+"S3 storage region" => "S3 storage region",
+"Container" => "Bucket",
+"Root container in the S3 storage" => "S3 bucket"
+);
---
+++ plugins/access.hpcloud/i18n/conf/fr.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+* This file is part of AjaXplorer.
+*
+* AjaXplorer is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* AjaXplorer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://www.ajaxplorer.info/>.
+*/
+$mess=array(
+    "S3 (Amazon Web Service)" => "S3 (Amazon Web Service)",
+    "Access an AWS server. Requires AWS-SDK for PHP" => "Accéder à un serveur AWS. Nécessite AWS-SDK for PHP ",
+    "Key" => "Clé",
+    "S3 Api Key" => "Clé d'API S3",
+    "Secret Key" => "Clé secrète",
+    "S3 secret key" => "Clé secrète S3",
+    "Region" => "Région",
+    "S3 storage region" => "Région du stockage S3",
+    "Container" => "Container",
+    "Root container in the S3 storage" => "Container S3 sans lequel sont stockées les données"
+);
