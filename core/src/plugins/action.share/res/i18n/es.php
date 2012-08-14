652063f3d885fffa58b0c0b777026d19f1009902
Charles du Jeu <charles@ajaxplorer.info>
2012-08-14 14:25:05 +0000

    ShareCenter : Spanish translation

---
+++ core/src/plugins/action.share/res/i18n/es.php
@@ -0,0 +1,32 @@
+<?php
+
+$mess = array(
+    // will be replaced by the application title
+    "1" => "%s Descarga Pública",
+    // will be replaced by the filename to download
+    "2" => "Haz clic en la imagen para descargar %s a tu disco duro",
+    "3" => "Contraseña incorrecta",
+    "4" => "Introduce la contraseña correcta y haz clic en la imagen para descargar %s a tu disco duro",
+    "5" => "contraseña",
+    "6" => "Dejar de compartir",
+    "7" => "Dejar de compartir no eliminará el fichero. Si quieres cambiar los parámetros, recomparte el fichero.",
+    "8" => "Fichero compartido",
+    "9" => "Usa este botón para dejar de compartir esta carpeta",
+    "10"=> "Carpeta compartida",
+    "11"=> "Expiración",
+    "12"=> "Contraseña",
+    "13"=> "Sí",
+    "14"=> "No",
+    "15"=> "El fichero fue descargado %s veces",
+    "16"=> "reiniciar",
+    "17"=> "Clic para reiniciar contador de descargas",
+    "18"=> "Carpeta compartida como un nuevo repositorio",
+    "19"=> "Las opciones del repositorio compartido fueron modificadas",
+    "20"=> "Ooops, el fichero requerido no fue encontrado. Tal vez fue eliminado o ya no está compartido.",
+    "21"=> "Caducidad (en días): ",
+    "22"=> "Descargas permitidas: ",
+    "23"=> "Contraseña opcional: ",
+    "24"=> "Establezca límite de descargas y expiración para el acceso al fichero y haga clic en \"Crear\".",
+    "25"=> "Sin límites",
+    "26"=> "Choose users or groups and set their access rights. 'Write' only will create an upload-only repository."
+);
