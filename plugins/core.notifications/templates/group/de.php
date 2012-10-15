a9f5ccc9b8eaaf196767874862b8bbafd79ad5a0
Axel Otterstätter <mr.o@gmx.de>
2012-10-15 11:18:45 +0000

    some german translations added

---
+++ plugins/core.notifications/templates/group/de.php
@@ -0,0 +1,38 @@
+<?php
+    /*
+    * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+    * This file is part of AjaXplorer.
+    *
+    * AjaXplorer is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Affero General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * AjaXplorer is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+    *
+    * You should have received a copy of the GNU Affero General Public License
+    * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+    *
+    * The latest code can be found at <http://www.ajaxplorer.info/>.
+    */
+$mess=array(
+    "file.add" => "Datei AJXP_NODE_LABEL hinzufügen zu AJXP_PARENT_PATH",
+    "file.delete" => "Datei AJXP_NODE_LABEL gelöscht von AJXP_PARENT_PATH",
+    "file.change" => "Datei AJXP_NODE_LABEL geändert von AJXP_USER",
+    "file.view" => "Datei AJXP_NODE_LABEL betrachtet von AJXP_USER",
+    "file.copy_to" => "AJXP_NODE_LABEL kopiert nach AJXP_TARGET_FOLDER",
+    "file.copy_from" => "AJXP_NODE_LABEL kopiert von AJXP_SOURCE_FOLDER",
+    "file.move_to" => "AJXP_NODE_LABEL verschoben nach AJXP_TARGET_FOLDER",
+    "file.move_from" => "AJXP_NODE_LABEL verschoben von AJXP_SOURCE_FOLDER",
+    "folder.add" => "Verzeichnis AJXP_NODE_LABEL erstellt",
+    "folder.delete" => "Verzeichnis AJXP_NODE_LABEL gelöscht",
+    "folder.change" => "Inhalt von AJXP_NODE_LABEL hat sich geändert.",
+    "folder.view" => "Verzeichnis AJXP_NODE_LABEL wurde geöffnet",
+    "folder.copy_to" => "Verzeichnis AJXP_NODE_LABEL kopiert nach AJXP_TARGET_FOLDER",
+    "folder.copy_from" => "Verzeichnis AJXP_NODE_LABEL kopiert von AJXP_SOURCE_FOLDER",
+    "folder.move_to" => "Verzeichnis AJXP_NODE_LABEL verschoben nach AJXP_TARGET_FOLDER",
+    "folder.move_from" => "Verzeichnis AJXP_NODE_LABEL verschoben von AJXP_SOURCE_FOLDER",
+);
