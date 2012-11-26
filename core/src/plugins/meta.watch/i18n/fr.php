b1b30fb8616379cf12b0839d5d959f81d71cde64
Charles du Jeu <charles@ajaxplorer.info>
2012-11-26 09:48:57 +0000

    Meta.watch i18n

---
+++ core/src/plugins/meta.watch/i18n/fr.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: admin
+ * Date: 04/10/12
+ * Time: 17:16
+ * To change this template use File | Settings | File Templates.
+ */
+$mess = array(
+    "1" => "Surveiller...",
+    "2" => "Etre alerté quand il y a une modification",
+    "3" => "Stopper la surveillance",
+    "4" => "Ne plus être alerté en cas de modification",
+    "5" => "Surveiller...",
+    "6" => "Surveiller les modifications de fichier ",
+    "7" => "Ne plus surveiller",
+    "8" => "Ne plus être alerté en cas de modification",
+    "9" => "Consultation",
+    "10"=> "Un utilisateur ouvre le fichier",
+    "10b"=> "Un utilisateur explore ce répertoire",
+    "11"=> "Modification",
+    "12"=> "Un utilisateur modifie le ficher",
+    "12b"=> "Un utilisateur modifie un ficher de ce répertoire",
+    "13"=> "Les deux",
+    "14"=> "Un utilisateur ouvre ou modifie le contenu du fichier",
+    "14b"=> "Un utilisateur liste ou modifie le contenu du répertoire",
+
+);
