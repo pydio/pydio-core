44e2a2ab3a7555bb7c743b2dea8b909ad5f9d033
Charles du Jeu <charles@ajaxplorer.info>
2012-12-20 00:01:20 +0000

    Super simple messaging system, to be ported in a dedicated core.* plugin!

--- core/src/plugins/conf.serial/class.serialConfDriver.php
+++ core/src/plugins/conf.serial/class.serialConfDriver.php
@@ -491,4 +491,142 @@ class serialConfDriver extends AbstractConfDriver {
         }
         return $data;
     }
+
+    /**
+     * @var Array
+     */
+    private $channels;
+
+    function loadChannel($channelName, $create = false){
+        if(isSet($this->channels) && is_array($this->channels[$channelName])) {
+            return;
+        }
+        if(is_file($this->getPluginWorkDir()."/queues/channel-$channelName")){
+            if(!isset($this->channels)) $this->channels = array();
+            $this->channels[$channelName] = AJXP_Utils::loadSerialFile($this->getPluginWorkDir()."/queues/channel-$channelName");
+            return;
+        }
+        if($create){
+            if(!isSet($this->channels)) $this->channels = array();
+            $this->channels[$channelName] = array("CLIENTS" => array(),
+                                                  "MESSAGES" => array());
+        }
+    }
+
+    function __destruct(){
+        if(isSet($this->channels) && is_array($this->channels)){
+            foreach($this->channels as $channelName => $data){
+                AJXP_Utils::saveSerialFile($this->getPluginWorkDir()."/queues/channel-$channelName", $data);
+            }
+        }
+    }
+
+    /**
+     * @param null AJXP_Node $origNode
+     * @param null AJXP_Node $newNode
+     * @param bool bool $copy
+     */
+    public function publishNodeChange($origNode = null, $newNode = null, $copy = false){
+        if($newNode != null) {
+            $repo = $newNode->getRepositoryId();
+            $message = new stdClass();
+            $message->content = AJXP_XMLWriter::writeNodesDiff(array("ADD" => array($newNode)));
+            $this->publishToChannel("nodes:$repo", $message);
+        }
+        if($origNode != null){
+            $repo = $origNode->getRepositoryId();
+            $message = new stdClass();
+            $message->content = AJXP_XMLWriter::writeNodesDiff(array("REMOVE" => array($origNode->getPath())));
+            $this->publishToChannel("nodes:$repo", $message);
+        }
+
+    }
+
+    /**
+     * @param $action
+     * @param $httpVars
+     * @param $fileVars
+     *
+     * JS SAMPLE
+     *
+     new PeriodicalExecuter(function(pe){
+         var conn = new Connexion();
+         conn.setParameters($H({get_action:'client_consume_channel',channel:'nodes:0',client_id:'toto'}));
+         conn.onComplete = function(transport){ajaxplorer.actionBar.parseXmlMessage(transport.responseXML);};
+         conn.sendAsync();
+     }, 5);
+     *
+     */
+
+    public function clientChannelMethod($action, $httpVars, $fileVars){
+        switch($action){
+            case "client_register_channel":
+                $this->suscribeToChannel($httpVars["channel"], $httpVars["client_id"]);
+                break;
+            case "client_unregister_channel":
+                $this->unsuscribeFromChannel($httpVars["channel"], $httpVars["client_id"]);
+                break;
+            case "client_consume_channel":
+                $data = $this->consumeChannel($httpVars["channel"], $httpVars["client_id"]);
+                if(count($data)){
+                    AJXP_XMLWriter::header();
+                    ksort($data);
+                    foreach($data as $messageObject){
+                        echo $messageObject->content;
+                    }
+                    AJXP_XMLWriter::close();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    function suscribeToChannel($channelName, $clientId){
+        $this->loadChannel($channelName, true);
+        $this->channels[$channelName]["CLIENTS"][$clientId] = $clientId;
+        foreach($this->channels[$channelName]["MESSAGES"] as &$object){
+            $object->messageRC[$clientId] = $clientId;
+        }
+    }
+
+    function unsuscribeFromChannel($channelName, $clientId){
+        $this->loadChannel($channelName);
+        if(!isSet($this->channels) || !isSet($this->channels[$channelName])) return;
+        if(!array_key_exists($clientId,  $this->channels[$channelName]["CLIENTS"])) return;
+        unset($this->channels[$channelName]["CLIENTS"][$clientId]);
+        foreach($this->channels[$channelName]["MESSAGES"] as $index => &$object){
+            unset($object->messageRC[$clientId]);
+            if(count($object->messageRC)== 0){
+                unset($this->channels[$channelName]["MESSAGES"][$index]);
+            }
+        }
+    }
+
+    function publishToChannel($channelName, $messageObject){
+        $this->loadChannel($channelName);
+        if(!isSet($this->channels) || !isSet($this->channels[$channelName])) return;
+        if(!count($this->channels[$channelName]["CLIENTS"])) return;
+        $messageObject->messageRC = $this->channels[$channelName]["CLIENTS"];
+        $messageObject->messageTS = microtime();
+        $this->channels[$channelName]["MESSAGES"][] = $messageObject;
+    }
+
+    function consumeChannel($channelName, $clientId){
+        $this->loadChannel($channelName);
+        if(!isSet($this->channels) || !isSet($this->channels[$channelName])) return;
+        if(!array_key_exists($clientId,  $this->channels[$channelName]["CLIENTS"])) return;
+        $result = array();
+        foreach($this->channels[$channelName]["MESSAGES"] as $index => $object){
+            if(!isSet($object->messageRC[$clientId])) continue;
+            $result[] = $object;
+            unset($object->messageRC[$clientId]);
+            if(count($object->messageRC) <= 0){
+                unset($this->channels[$channelName]["MESSAGES"][$index]);
+            }else{
+                $this->channels[$channelName]["MESSAGES"][$index] = $object;
+            }
+        }
+        return $result;
+    }
 }
