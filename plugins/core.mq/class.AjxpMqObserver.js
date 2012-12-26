a4a1bbc7ac84d52f1c50b69e71f08e27f8b20c82
Charles du Jeu <charles@ajaxplorer.info>
2012-12-26 23:07:49 +0000

    Auto switch between websocket and polling

--- plugins/core.mq/class.AjxpMqObserver.js
+++ plugins/core.mq/class.AjxpMqObserver.js
@@ -18,7 +18,9 @@
  * The latest code can be found at <http://www.ajaxplorer.info/>.
  */
 
-
+/**
+ * Use WebSocket or Poller
+ */
 Class.create("AjxpMqObserver", {
 
     pe:null,
@@ -29,54 +31,72 @@ Class.create("AjxpMqObserver", {
     initialize : function(){
         "use strict";
         this.clientId = window.ajxpBootstrap.parameters.get("SECURE_TOKEN");
+        var configs = ajaxplorer.getPluginConfigs("mq");
 
         document.observe("ajaxplorer:repository_list_refreshed", function(event){
 
-            if(this.pe){
-                this.pe.stop();
-            }
-            if(this.currentRepo){
-                var conn = new Connexion();
-                conn.setParameters($H({
-                    get_action:'client_unregister_channel',
-                    channel:'nodes:' + this.currentRepo,
-                    client_id:this.clientId
-                }));
-                conn.discrete = true;
-                conn.sendSync();
-                this.currentRepo = null;
-            }
-
+            console.log("repository_list_refreshed", event.memo);
             var repoId;
             var data = event.memo;
             if(data.active) repoId = data.active;
             else if(ajaxplorer.repositoryId) repoId = ajaxplorer.repositoryId;
-            if(!repoId) {
-                return;
-            }
 
-            this.currentRepo = repoId;
-            var conn = new Connexion();
-            conn.setParameters($H({
-                get_action:'client_register_channel',
-                channel:'nodes:' + repoId,
-                client_id:this.clientId
-            }));
-            conn.discrete = true;
-            conn.sendAsync();
+            if(window.WebSocket && parseInt(configs.get("WS_SERVER_ACTIVE"))){
 
-            if(window.WebSocket){
+                if(this.ws) {
+                    if(!repoId){
+                        this.ws.onclose = function(){
+                            delete this.ws;
+                        }.bind(this);
+                        this.ws.close();
 
-                if(this.ws) this.ws.close();
-                this.ws = new WebSocket("ws://192.168.0.18:8090/echo");
-                this.ws.onmessage = function(event){
-                    var obj = parseXml(event.data);
-                    if(obj){
-                        ajaxplorer.actionBar.parseXmlMessage(obj);
+                    } else {
+                        this.ws.send("register:" + repoId);
                     }
-                };
+                }else{
+                    var url = "ws"+(parseInt(configs.get("WS_SERVER_SECURE"))?"s":"")+"://"+configs.get("WS_SERVER_HOST")+":"+configs.get("WS_SERVER_PORT")+configs.get("WS_SERVER_PATH");
+                    this.ws = new WebSocket(url);
+                    this.ws.onmessage = function(event){
+                        var obj = parseXml(event.data);
+                        if(obj){
+                            ajaxplorer.actionBar.parseXmlMessage(obj);
+                        }
+                    };
+                    //console.log("Sending " + "register:" + repoId);
+                    this.ws.onopen = function(){
+                        this.ws.send("register:" + repoId);
+                    }.bind(this);
+                }
 
             }else{
+
+                if(this.pe){
+                    this.pe.stop();
+                }
+
+                if(this.currentRepo){
+                    var conn = new Connexion();
+                    conn.setParameters($H({
+                        get_action:'client_unregister_channel',
+                        channel:'nodes:' + this.currentRepo,
+                        client_id:this.clientId
+                    }));
+                    conn.discrete = true;
+                    conn.sendSync();
+                    this.currentRepo = null;
+                }
+
+                if(!repoId) return;
+
+                this.currentRepo = repoId;
+                var conn = new Connexion();
+                conn.setParameters($H({
+                    get_action:'client_register_channel',
+                    channel:'nodes:' + repoId,
+                    client_id:this.clientId
+                }));
+                conn.discrete = true;
+                conn.sendAsync();
 
                 this.pe = new PeriodicalExecuter(function(pe){
                     var conn = new Connexion();
