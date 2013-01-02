19c29407aff71f04c9395de204409065ae5cf55c
Charles du Jeu <charles@ajaxplorer.info>
2013-01-02 15:07:12 +0000

    Add a start test for Output Buffering

---
+++ core/tests/test.PHP_OB.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+defined('AJXP_EXEC') or die( 'Access not allowed');
+require_once('../classes/class.AbstractTest.php');
+
+/**
+ * @package info.ajaxplorer.test
+ * Check that DOMXml is enabled
+ */
+class PHP_OB extends AbstractTest
+{
+    function PHP_OB() { parent::AbstractTest("PHP Output Buffer disabled", "You should disable php output_buffering parameter for better performances with AjaXplorer."); }
+    function doTest()
+    {
+        $this->failedLevel = "warning";
+        $v = @ini_get("output_buffering");
+        if (isSet($v) && (is_numeric($v) || strtolower($v) == "on")){
+            $this->testedParams["PHP Output Buffer disabled"] = "No";
+            return FALSE;
+        }else if(!isSet($v)){
+            $this->failedInfo = "Unable to detect the output_buffering value, please make sure that it is disabled (Off) in your php.ini or your virtual host.";
+            return FALSE;
+        }
+        $this->testedParams["PHP Output Buffer disabled"] = "Yes";
+        return TRUE;
+    }
+};
