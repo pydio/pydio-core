fb4809654fa9c0b395d9f27fca2daf8dc5031109
Charles du Jeu <charles@ajaxplorer.info>
2013-02-21 15:26:04 +0000

    New shorten.multi plugin (contributed by Miguelon), should replace shorten.bitly ...

---
+++ plugins/shorten.multi/i18n/conf/en.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* Multi shortener plugin for ajaXplorer by FrenandoAloso
+*         based in bit.ly plugin
+*/
+$mess=array(
+"Multi URL Shortener" => "Multi URL Shortener",
+"Shorten Download Links before sending them back to the user." => "Shorten Download Links before sending them back to the user.",
+"User ID" => "User ID",
+"adF.ly account user ID.\nThe numbers after -= ?id= =- in of your referral program's URL" => "adF.ly account user ID.\nThe numbers after -= ?id= =- in of your referral program's URL",
+"API Key" => "API Key",
+"adF.ly account API Key" => "adF.ly account API Key",
+"Type of ADS" => "Type of ADS",
+"Type of AD you like to show.\n Select Fullscreen AD or Top banner AD" => "Type of AD you like to show.\n Select Fullscreen AD or Top banner AD",
+"Short domain" => "Short domain",
+"adF.ly or q.gs domains, select wich you like" => "adF.ly or q.gs domains, select wich you like",
+"FullScreen" => "FullScreen",
+"Banner" => "Banner",
+"Bit.ly URL Shortener" => "Bit.ly URL Shortener",
+"Shorten Download Links before sending them back to the user. Requires a Bit.ly account." => "Shorten Download Links before sending them back to the user. Requires a Bit.ly account.",
+"User Name" => "User Name",
+"Bit.ly account user name" => "Bit.ly account user name",
+"API Key" => "API Key",
+"Bit.ly account API Key" => "Bit.ly account API Key",
+);
+?>
