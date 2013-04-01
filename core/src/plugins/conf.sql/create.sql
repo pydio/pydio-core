60bef13176c7aaeb46e0655320f8a20d925e1517
cdujeu <charles@ajaxplorer.info>
2013-04-01 10:15:43 +0200

    SQL_DRIVER is mandatory Add ajxp_user table in conf.sql as well

--- core/src/plugins/conf.sql/create.sql
+++ core/src/plugins/conf.sql/create.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS ajxp_users (
+  login varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  groupPath varchar(255) NULL,
+  PRIMARY KEY  (login)
+)
+
 CREATE TABLE IF NOT EXISTS ajxp_user_rights (
 	rid INTEGER PRIMARY KEY AUTO_INCREMENT, 
 	login VARCHAR(255) NOT NULL, 
