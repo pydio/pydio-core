44b0bb9b33a2f074906ff7200e674e6614a46a78
cdujeu <charles@ajaxplorer.info>
2013-04-26 01:07:13 +0200

    PHP Version is now officially 5.3

--- core/src/core/tests/test.PHPVersion.php
+++ core/src/core/tests/test.PHPVersion.php
@@ -28,18 +28,18 @@ require_once('../classes/class.AbstractTest.php');
  */
 class PHPVersion extends AbstractTest
 {
-    function PHPVersion() { parent::AbstractTest("PHP version", "Minimum required version is PHP 5.1.0, PHP 5.2 or higher recommended when using foreign language"); }
+    function PHPVersion() { parent::AbstractTest("PHP version", "Minimum required version is PHP 5.3.0"); }
     function doTest() 
     { 
         $version = phpversion(); 
     	$this->testedParams["PHP Version"] = $version;
     	//return false;
-        if (floatval($version) < 5.1) return FALSE; 
+        if (floatval($version) < 5.3) return FALSE;
         $locale = setlocale(LC_CTYPE, 0);
         $dirSep = DIRECTORY_SEPARATOR;
         $this->testedParams["Locale"] = $locale;
         $this->testedParams["Directory Separator"] = $dirSep;
-        if (floatval($version) < 5.1 && $locale != "C" && $dirSep != '\\') { $this->failedLevel = "warning"; return FALSE; } // PHP4 doesn't work well with foreign encoding
+        if (floatval($version) < 5.3 && $locale != "C" && $dirSep != '\\') { $this->failedLevel = "warning"; return FALSE; } // PHP4 doesn't work well with foreign encoding
         return TRUE;
     }
 };
