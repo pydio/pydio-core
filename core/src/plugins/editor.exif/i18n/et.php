7b602bea3e160b6a309361b64163ec7e5df9e5b9
kainv <kain@zone.ee>
2013-05-27 14:42:53 +0300

    Create et.php
    Estonian translation

---
+++ core/src/plugins/editor.exif/i18n/et.php
@@ -0,0 +1,11 @@
+<?php
+//  Estonian translation by Ardi Jürgens <ardi (at) zone.ee>
+//  + updates/fixes by Kain Väljaots <kain (at) zone.ee>
+//	Last update: 27.05.2013
+
+$mess = array(
+"1" => "EXIF luger", 
+"2" => "Kuva EXIF andmed", 
+);
+
+?>
