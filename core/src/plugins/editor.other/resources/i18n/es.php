722dea1ab72c71de41abf17db12cdd8ecea90397
cdujeu <charles@ajaxplorer.info>
2013-05-28 23:26:03 +0200

    Spanish translation, by tbote

---
+++ core/src/plugins/editor.other/resources/i18n/es.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+$mess=array(
+"1" => "Elegir otro...",
+"2" => "Elegir otro editor",
+"3" => "Elija otro editor para abrir este fichero",
+"4" => "Atención, estos editores no parecen soportar este tipo de archivos, su comportamiento puede ser impredecible!",
+"5" => "Borrar las asociaciones existentes",
+); 
+?>
---
+++ core/src/plugins/index.lucene/resources/i18n/es.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
+ *
+ * AjaXplorer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AjaXplorer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
+ */
+
+$mess = array(
+    "1" => "Indexar contenido",
+    "2" => "Indexar el contenido de la carpeta",
+    "3" => "Ejecutando indexación en segundo plano",
+    "4" => "El índice parece estar bloqueado, puede que un proceso de indexación se esté realizando en este momento. En caso contrario borre el fichero  .ajxp_lock localizado en la carpeta cache/indexes.",
+    "5" => "Indexación finalizada",
+    "6" => "Atención, la carpeta actual está indexándose, inténtelo más tarde.",
+    "7" => "Primera indexación de esta carpeta, por favor espere y reintente más tarde su búsqueda",
+    "8" => "Indexando %s en segundo plano",
+    "9" => "No puedo encontrar el índice de la carpeta actual! Debería ejecutar la indexación de los datos primero!",
+);
