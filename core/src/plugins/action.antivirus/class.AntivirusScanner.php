ca96fa524e9138db97d98a15be14ff162e482674
cdujeu <charles@ajaxplorer.info>
2013-06-28 00:06:12 +0200

    Clean and refactor for naming convention the Antivirus contribution. Still to be more deeply tested.

--- core/src/plugins/action.antivirus/class.AntivirusScanner.php
+++ core/src/plugins/action.antivirus/class.AntivirusScanner.php
@@ -9,51 +9,54 @@ class AntivirusScanner extends AJXP_Plugin {
 	protected $extension_scan;
 	protected $scan_all;
 	protected $scan_diff_folder;
-	protected $scan_max_size;
-	protected $file_size;
+    protected $scan_max_size;
+    protected $file_size;
 
-//main function, it is called by the hook.
+    /**
+     * @param AJXP_Node $oldNode
+     * @param AJXP_Node $newNode
+     * Main function, it is called by the hook.
+     */
+    public function scanFile ($oldNode, $newNode) {
 
-	public function scan_file ($oldNode, $newNode) {
+        $this->callSet($newNode);			//initializes attributes
 
-		$this->CallSet($newNode);			//initializes attributes
+        if($oldNode!=null || $newNode == null){
+            return;
+        }
 
-		if($oldNode==null && $newNode != null){
+        // This block scans or doesn't scan the file. This is based on plugin parameters
+        if($this->file_size < $this->scan_max_size) {
+            if($this->scan_all == true) {
+                if($this->inList()==true) {
+                    if($this->pluginConf["TRACE"] == false) {return;}
+                    $this->scanLater();
+                    return ;
+                }else {
+                    $this->scanNow();
+                    return ;
+                }
+            } else {
+                if($this->inList()==true) {
+                    $this->scanNow();
+                    return ;
+                }else {
+                    if($this->pluginConf["TRACE"] == false) {return;}
+                    $this->scanLater();
+                    return ;
+                }
+            }
+        }else {
+            $this->scanLater();
+            return ;
+        }
+    }
 
-		} else{
-			return;
-		}
+    /**
+     * @return bool true if file_extension is in the list extension_scan
+     */
+    private function inList() {
 
-//this block scans or doesn't scan the file. This is based on plugin parameters
-
-		if($this->file_size < $this->scan_max_size) {
-			if($this->scan_all == true) {
-				if($this->In_list()==true) {
-					if($this->pluginConf["TRACE"] == false) {return;}
-					$this->scan_later();
-					return ;
-				}else {
-					$this->scan_now();
-					return ;
-				}
-			} else {
-				if($this->In_list()==true) {
-					$this->scan_now();
-					return ;
-				}else {
-					if($this->pluginConf["TRACE"] == false) {return;}
-					$this->scan_later();
-					return ;
-				}
-			}
-		}else {
-			$this->scan_later();
-			return ;
-		}
-	}
-
-//return true if file_extension is in the list extension_scan
-	private function In_list() {
 		while(strripos($this->extension_scan, $this->file_extension)) {
 			$start_pos = strripos($this->extension_scan, $this->file_extension);
 			$leng_ext = strlen($this->file_extension);
@@ -68,18 +71,18 @@ class AntivirusScanner extends AJXP_Plugin {
 		return false;
 	}
 
-//this function immediatly scans the file, it calls the antivirus command
-	private function scan_now() {
+    /**
+     * This function immediatly scans the file, it calls the antivirus command
+     */
+	private function scanNow() {
 		$command = $this->pluginConf["COMMAND"];
 		$command = str_replace('$' . 'FILE', '"' . $this->path . '"', $command);
-	
-		
-			ob_start();
-			passthru($command, $int);
-			$output=ob_get_contents();
-			ob_end_clean();
-		
-	
+
+        ob_start();
+        passthru($command, $int);
+        $output=ob_get_contents();
+        ob_end_clean();
+
 		if($int != 0) {
 			if(self::DEBUG_ON == 1) {														
 				echo $output;
@@ -88,15 +91,17 @@ class AntivirusScanner extends AJXP_Plugin {
 				$filename = substr($filename, 1);
 				echo 'Virus has been found in : '  . $filename .  '         File removed';
 			}
-			AJXP_Logger::logAction("Upload Virus File" . $this->path, array("file"=>SystemTextEncoding::fromUTF8($dir).DIRECTORY_SEPARATOR.$realpath));
-			break;
+			//AJXP_Logger::logAction("Upload Virus File" . $this->path, array("file"=>SystemTextEncoding::fromUTF8($dir).DIRECTORY_SEPARATOR.$realpath));
 		}
 		return;
 	}
-//this function generates a trace of the file
-	private function scan_later() {
+
+    /**
+     * This function generates a trace of the file
+     */
+	private function scanLater() {
 		$numero=0;
-		$fichier_scanne=false;
+		$scanned=false;
 		if(is_dir($this->scan_diff_folder) == false) {
 			$create_folder = mkdir($this->scan_diff_folder, 0755);
 			if($create_folder == false) {
@@ -104,46 +109,57 @@ class AntivirusScanner extends AJXP_Plugin {
 				return;
 			}
 		}
-		while($fichier_scanne == false) {
+		while($scanned == false) {
 			if(file_exists( $this->scan_diff_folder .DIRECTORY_SEPARATOR. 'file_' . $numero)) {
 				$numero++;
 			}
 			else {	
 				$command = 'echo "'. '\"' . $this->path . '\"' . '" >' . $this->scan_diff_folder .DIRECTORY_SEPARATOR. 'file_' . $numero;
 				passthru($command);
-				$fichier_scanne=true;
+				$scanned=true;
 				return;
 			}
 		}
 	}
 
-//this function initializes attributes. 
-	private function CallSet($nodeobject) {
-		$this->setPath($nodeobject);
-		$this->setFileExtension($nodeobject);
+    /**
+     * @param AJXP_Node $nodeObject
+     */
+    private function callSet($nodeObject) {
+
+		$this->setPath($nodeObject);
+		$this->setFileExtension($nodeObject);
 		$this->setExtensionScan();
 		$this->setScanDiffFolder ();
 		$this->setScanMaxSize ();
-		$this->setFileSize($nodeobject);
+		$this->setFileSize($nodeObject);
 		$this->setScanAll();
 
-//debug option, put in a file attribute values
+        //debug option, put in a file attribute values
 		if(self::DEBUG_ON == 1) {
 			$debug = 'echo "' . $this->path . "     " . $this->file_extension . "     " . $this->extension_scan . "     " . $this->scan_all . "     " . $this->scan_diff_folder . "     " . $this->scan_max_size . "	" . $this->file_size . '" >> plugins/action.antivirus/debug';
 			passthru($debug);
 		}
 		return ;
 	}
-//this function initializes the file path
-	public function setPath($nodeobject){
-		$realpath = $nodeobject->getRealFile();
+
+    /**
+     * This function initializes the file path
+     * @param $nodeObject
+     */
+    public function setPath($nodeObject){
+		$realpath = $nodeObject->getRealFile();
 		$realpath = realpath($realpath);
 		$this->path= $realpath;
 		return ;
 	}
-//this function initializes the file extension
-	public function setFileExtension ($nodeobject) {
-		$realpath = $nodeobject->getRealFile();
+
+    /**
+     * This function initializes the file extension
+     * @param $nodeObject
+     */
+    public function setFileExtension ($nodeObject) {
+		$realpath = $nodeObject->getRealFile();
 		$realpath = realpath($realpath);
 		$realpath = str_replace(" ", "_",$realpath );
 		$realpath = strrchr($realpath, DIRECTORY_SEPARATOR);
@@ -152,13 +168,18 @@ class AntivirusScanner extends AJXP_Plugin {
 		return ;
 	}
 
-//this function initializes the extenension list
-	public function setExtensionScan() {
+    /**
+     * This function initializes the extension list
+     */
+    public function setExtensionScan() {
 		$this->extension_scan = $this->pluginConf["EXT"];
 		return ;
 	}
-//this function initializes attribute scan_all
-	public function setScanAll(){
+
+    /**
+     * this function initializes attribute scan_all
+     */
+    public function setScanAll(){
 		$extension = $this->pluginConf["EXT"];
 		if(substr($extension, 0, 2) == "*/"){
 			$this->scan_all = true;
@@ -167,23 +188,32 @@ class AntivirusScanner extends AJXP_Plugin {
 		}
 		return ;
 	}
-//this function initializes the trace folder
-	public function setScanDiffFolder () {
+
+    /**
+     * this function initializes the trace folder
+     */
+    public function setScanDiffFolder () {
 		$this->scan_diff_folder = $this->pluginConf["PATH"];	
 		return ;
 	}
-//this function initializes max size of the scanned file
-	public function setScanMaxSize () {
+
+    /**
+     * this function initializes max size of the scanned file
+     */
+    public function setScanMaxSize () {
 		$this->scan_max_size = AJXP_Utils::convertBytes($this->pluginConf["SIZE"]);
 		return ;
 	}
-//this function initializes the size of the file
-	public function setFileSize ($nodeobject) {
-		$realpath = $nodeobject->getRealFile();
+
+    /**
+     * This function initializes the size of the file
+     * @param AJXP_Node $nodeObject
+     */
+    public function setFileSize ($nodeObject) {
+		$realpath = $nodeObject->getRealFile();
 		$realpath = realpath($realpath);
 		$this->file_size = filesize($realpath);
 		return ;
 	}
 
 }
-?>
