3eaf38ca5f578fc626d2e426919207026bcc55df
Etienne CHAMPETIER <etienne.champetier@fiducial.net>
2013-09-06 10:01:32 +0200

    Split codestyle checks in mandatory and optionnal tests visibility fixer is making some false positive
    Signed-off-by: Etienne CHAMPETIER <etienne.champetier@fiducial.net>

--- dist/scripts/tests/codestyle.sh
+++ dist/scripts/tests/codestyle.sh
@@ -12,17 +12,35 @@ else
     exit 1
 fi
 
-PHPCSFIXERARGS="fix -v --fixers=indentation,linefeed,trailing_spaces,visibility,short_tag,braces,php_closing_tag,controls_spaces,eof_ending"
+PHPCSFIXERARGS="fix -v --fixers="
+# Mandatory fix
+FIXERS1="indentation,linefeed,trailing_spaces,short_tag,braces,php_closing_tag,controls_spaces,eof_ending"
+# Optionnal fix & false positive
+FIXERS2="visibility"
 
-echo -e "\e[1;34mChecking Formatting/coding standards\e[00m"
-$PHPCSFIXER $PHPCSFIXERARGS --dry-run .
+EXIT=0
+
+echo -e "\e[1;34mChecking mandatory formatting/coding standards\e[00m"
+$PHPCSFIXER $PHPCSFIXERARGS$FIXERS1 --dry-run .
 rc=$?
 if [[ $rc == 0 ]]
 then
     echo -e "\e[1;32mFormatting is OK\e[00m"
 else
     echo -e "\e[1;31mPlease check code Formatting\e[00m"
-    echo -e "\e[1;31m$PHPCSFIXER $PHPCSFIXERARGS .\e[00m"
-    exit 1
+    echo -e "\e[1;31m$PHPCSFIXER $PHPCSFIXERARGS$FIXERS1 .\e[00m"
+    EXIT=1
 fi
 
+echo -e "\e[1;34mChecking optionnal formatting/coding standards\e[00m"
+$PHPCSFIXER $PHPCSFIXERARGS$FIXERS2 --dry-run .
+rc=$?
+if [[ $rc == 0 ]]
+then
+    echo -e "\e[1;32mOptionnal formatting is OK\e[00m"
+else
+    echo -e "\e[1;33mThere are errors in the formatting (or false positive)\e[00m"
+    echo -e "\e[1;33m$PHPCSFIXER $PHPCSFIXERARGS$FIXERS2 .\e[00m"
+fi
+
+exit $EXIT
