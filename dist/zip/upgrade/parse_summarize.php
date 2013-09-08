60565444f70f01a8a0505d330376479f978b26c2
cdujeu <charles@ajaxplorer.info>
2013-09-08 15:48:30 +0200

    Code Style

--- dist/zip/upgrade/parse_summarize.php
+++ dist/zip/upgrade/parse_summarize.php
@@ -7,57 +7,47 @@ $toDelete = array();
 
 @mkdir ($patchDir);
 @mkdir ($patchDir."/UPGRADE");
-foreach($summarizeLines as $line){
+foreach ($summarizeLines as $line) {
     $line = trim($line);
     $letter = substr($line, 0, 1);
     $end = trim(substr($line, 1));
-    if($letter == "D"){
+    if ($letter == "D") {
         $toDelete[] = $end;
         continue;
     }
-    if(is_dir($srcDir."/".$end)){
+    if (is_dir($srcDir."/".$end)) {
         if(!is_dir($patchDir."/".$end)) mkdir($patchDir."/".$end, 777, true);
-    }else{
+    } else {
         if(!is_dir($patchDir."/".dirname($end))) mkdir($patchDir."/".dirname($end), 777, true);
         echo("\n-- Copy ".$srcDir."/".$end ." to ".$patchDir."/".$end);
         copy($srcDir."/".$end, $patchDir."/".$end);
     }
 }
-if(count($toDelete)){
+if (count($toDelete)) {
     file_put_contents($patchDir."/UPGRADE/CLEAN-FILES", implode("\r\n", str_replace("\\", "/", $toDelete)));
 }
 
 function copy_r( $path, $dest )
 {
-    if( is_dir($path) )
-    {
+    if ( is_dir($path) ) {
         @mkdir( $dest );
         $objects = scandir($path);
-        if( sizeof($objects) > 0 )
-        {
-            foreach( $objects as $file )
-            {
+        if ( sizeof($objects) > 0 ) {
+            foreach ($objects as $file) {
                 if( $file == "." || $file == ".." )
                     continue;
                 // go on
-                if( is_dir( $path.DIRECTORY_SEPARATOR.$file ) )
-                {
+                if ( is_dir( $path.DIRECTORY_SEPARATOR.$file ) ) {
                     self::copy_r( $path.DIRECTORY_SEPARATOR.$file, $dest.DIRECTORY_SEPARATOR.$file );
-                }
-                else
-                {
+                } else {
                     copy( $path.DIRECTORY_SEPARATOR.$file, $dest.DIRECTORY_SEPARATOR.$file );
                 }
             }
         }
         return true;
-    }
-    elseif( is_file($path) )
-    {
+    } elseif ( is_file($path) ) {
         return copy($path, $dest);
-    }
-    else
-    {
+    } else {
         return false;
     }
 }
