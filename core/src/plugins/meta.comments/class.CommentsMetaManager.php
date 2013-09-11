c372e5743fb7601db3680fe38d3bae2db983e8b2
cdujeu <charles@ajaxplorer.info>
2013-09-11 14:19:10 +0200

    Skeleton for comments feed.

--- core/src/plugins/meta.comments/class.CommentsMetaManager.php
+++ core/src/plugins/meta.comments/class.CommentsMetaManager.php
@@ -11,9 +11,11 @@ define("AJXP_META_SPACE_COMMENTS", "AJXP_META_SPACE_COMMENTS");
 
 class CommentsMetaManager extends AJXP_Plugin
 {
+    private $accessDriver;
+
     public function initMeta($accessDriver)
     {
-
+        $this->accessDriver = $accessDriver;
     }
         /**
      * @param AJXP_Node $ajxpNode
@@ -33,6 +35,60 @@ class CommentsMetaManager extends AJXP_Plugin
      */
     public function switchActions($actionName, $httpVars, $fileVars)
     {
+        $userSelection = new UserSelection();
+        $userSelection->initFromHttpVars($httpVars);
+        $uniqNode = $userSelection->getUniqueNode($this->accessDriver);
+        $existingFeed = $uniqNode->retrieveMetadata(AJXP_META_SPACE_COMMENTS, false);
+        if($existingFeed == null){
+            $existingFeed = array();
+        }
+        switch($actionName){
+
+            case "post_comment":
+
+                $uId = AuthService::getLoggedUser()->getId();
+                $com = array(
+                    "date"      => time(),
+                    "author"    => $uId,
+                    "content"   => $httpVars["content"]
+                );
+                $existingFeed[] = $com;
+                $uniqNode->removeMetadata(AJXP_META_SPACE_COMMENTS, false);
+                $uniqNode->setMetadata(AJXP_META_SPACE_COMMENTS, $existingFeed, false);
+                HTMLWriter::charsetHeader("application/json");
+                echo json_encode($com);
+
+                break;
+
+            case "load_comments_feed":
+
+                HTMLWriter::charsetHeader("application/json");
+                echo json_encode($existingFeed);
+
+                break;
+
+            case "delete_comment":
+
+                $data = json_decode($httpVars["comment_data"], true);
+                $reFeed = array();
+                if($data["author"] != AuthService::getLoggedUser()->getId()) break;
+                foreach($existingFeed as $fElement){
+                    if($fElement["date"] == $data["date"] && $fElement["author"] == $data["author"] && $fElement["content"] == $data["content"]){
+                        continue;
+                    }
+                    $reFeed[] = $fElement;
+                }
+                $uniqNode->removeMetadata(AJXP_META_SPACE_COMMENTS, false);
+                $uniqNode->setMetadata(AJXP_META_SPACE_COMMENTS, $reFeed, false);
+                HTMLWriter::charsetHeader("application/json");
+                echo json_encode($reFeed);
+
+                break;
+
+            default:
+                break;
+        }
+
     }
 
 }
--- core/src/plugins/meta.comments/class.CommentsPanel.js
+++ core/src/plugins/meta.comments/class.CommentsPanel.js
@@ -19,16 +19,88 @@
  */
 Class.create("CommentsPanel", {
 
+    // Warning, method is called statically, there is no "this"
     loadInfoPanel : function(container, node){
 
+        console.log(container);
+
         container.down("#comments_container").update("Loading comments...");
+        container.down("textarea").observe("focus", function(){
+            ajaxplorer.disableAllKeyBindings();
+        });
+        container.down("textarea").observe("blur", function(){
+            ajaxplorer.enableAllKeyBindings();
+        });
 
-        if(node.getMetadata().get("ajxp_has_comments")){
+        if(node.getMetadata().get("ajxp_has_comments_feed")){
 
+            var conn = new Connexion();
+            conn.setParameters({
+                file: node.getPath(),
+                get_action: "load_comments_feed"
+            });
+            conn.onComplete = function(transport){
+                container.down("#comments_container").update('');
+                var feed = transport.responseJSON;
+                for(var i=0;i<feed.length;i++){
+                    CommentsPanel.prototype.commentObjectToDOM($H(feed[i]), container, node);
+                }
+            };
 
+            conn.sendAsync();
 
         }
 
+        container.down("#comments_submit").observe("click", function(){
+
+            if(!container.down('textarea').getValue()) {
+                return;
+            }
+            var conn = new Connexion();
+            conn.setParameters({
+                file: node.getPath(),
+                get_action: "post_comment",
+                content: container.down('textarea').getValue()
+            });
+            conn.setMethod('POST');
+            conn.onComplete = function(transport){
+                CommentsPanel.prototype.commentObjectToDOM($H(transport.responseJSON), container, node);
+            };
+
+            conn.sendAsync();
+
+        });
+
+    },
+
+    commentObjectToDOM: function(hash, container, node){
+
+        var el = new Element("div").update("<div class='comment_content'><div class='comment_delete'>X</div>"+hash.get("content")+"</div><div class='comment_legend'>By "+hash.get("author")+" on "+hash.get("date")+"</div>");
+        el.setStyle({opacity:0, display:'block'});
+        container.down("#comments_container").insert(el);
+        new Effect.Appear(el, {duration:0.3});
+        if(hash.get("author") != ajaxplorer.user.id){
+            el.down('.comment_delete').remove();
+            return;
+        }
+        el.down('.comment_delete').observe("click", function(){
+
+            var conn = new Connexion();
+            conn.setParameters({
+                file: node.getPath(),
+                get_action: "delete_comment",
+                comment_data: Object.toJSON(hash)
+            });
+            conn.setMethod('POST');
+            conn.onComplete = function(transport){
+                container.down("#comments_container").update('');
+                var feed = transport.responseJSON;
+                for(var i=0;i<feed.length;i++){
+                    CommentsPanel.prototype.commentObjectToDOM($H(feed[i]), container, node);
+                }
+            };
+            conn.sendAsync();
+        });
 
     }
 
