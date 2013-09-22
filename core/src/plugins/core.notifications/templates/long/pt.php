af67a57ab81ba7b035c22495d3b85f81e97e6df6
cdujeu <charles@ajaxplorer.info>
2013-09-22 20:06:10 +0200

    Massive rebranding

--- core/src/plugins/core.notifications/templates/long/pt.php
+++ core/src/plugins/core.notifications/templates/long/pt.php
@@ -1,23 +1,23 @@
 <?php
-    /*
-    * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
-    * This file is part of AjaXplorer.
-    *
-    * AjaXplorer is free software: you can redistribute it and/or modify
-    * it under the terms of the GNU Affero General Public License as published by
-    * the Free Software Foundation, either version 3 of the License, or
-    * (at your option) any later version.
-    *
-    * AjaXplorer is distributed in the hope that it will be useful,
-    * but WITHOUT ANY WARRANTY; without even the implied warranty of
-    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    * GNU Affero General Public License for more details.
-    *
-    * You should have received a copy of the GNU Affero General Public License
-    * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
-    *
-    * The latest code can be found at <http://www.ajaxplorer.info/>.
-    */
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
 $mess=array(
     "file.add" => "Dentro da Área de Trabalho AJXP_REPOSITORY_LABEL, o ficheiro AJXP_NODE_LABEL foi criado dentro de AJXP_PARENT_PATH em AJXP_DATE por AJXP_USER.",
     "file.delete" => "O ficheiro AJXP_NODE_LABEL foi apagado de AJXP_PARENT_PATH em AJXP_DATE, por AJXP_USER.",
