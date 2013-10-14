394f593ba7b7f790f0db7f4a88cc20e00e62c80a
cdujeu <charles@ajaxplorer.info>
2013-10-14 17:30:48 +0200

    Update sabredav node leaf (cherry picked from commit add21d7)

--- core/src/core/classes/sabredav/ajaxplorer/class.AJXP_Sabre_NodeLeaf.php
+++ core/src/core/classes/sabredav/ajaxplorer/class.AJXP_Sabre_NodeLeaf.php
@@ -52,7 +52,7 @@ class AJXP_Sabre_NodeLeaf extends AJXP_Sabre_Node implements Sabre\DAV\IFile
         // Warning, passed by ref
         $p = $this->path;
 
-        if(!AuthService::getLoggedUser()->canWrite($this->repository->getId())){
+        if (!AuthService::getLoggedUser()->canWrite($this->repository->getId())) {
             throw new \Sabre\DAV\Exception\Forbidden() ;
         }
         $this->getAccessDriver()->nodeWillChange($p, intval($_SERVER["CONTENT_LENGTH"]));
@@ -118,8 +118,7 @@ class AJXP_Sabre_NodeLeaf extends AJXP_Sabre_Node implements Sabre\DAV\IFile
         fclose($fp);
         return $fileMime;
         /*
-        if ( $this->options->useMimeExts && ezcBaseFeatures::hasExtensionSupport( 'fileinfo' ) )
-        {
+        if ( $this->options->useMimeExts && ezcBaseFeatures::hasExtensionSupport( 'fileinfo' ) ) {
             $fInfo = new fInfo( FILEINFO_MIME );
             $mimeType = $fInfo->file( $this->getUrl() );
 
