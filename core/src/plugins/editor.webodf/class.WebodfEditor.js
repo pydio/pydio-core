66b64af77d5a537776f56ace1b42eca67d7ffb84
cdujeu <charles@ajaxplorer.info>
2013-10-31 15:38:09 +0100

    Simple implementation of WebODF viewer. Only as viewer for the moment, we have to get a way to send back the modif in a standard manner, probably by replacing the Runtime implementation.

---
+++ core/src/plugins/editor.webodf/class.WebodfEditor.js
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+Class.create("WebodfEditor", AbstractEditor, {
+
+    padID:null,
+    sessionID: null,
+    node: null,
+
+	initialize: function($super, oFormObject, options)
+	{
+		$super(oFormObject, options);
+		if(!ajaxplorer.user || ajaxplorer.user.canWrite()){
+			this.canWrite = true;
+			this.actions.get("saveButton").observe('click', function(){
+				this.saveFile();
+				return false;
+			}.bind(this));		
+		}else{
+			this.canWrite = false;
+			this.actions.get("saveButton").hide();
+		}
+        // HIDE SAVE FOR THE MOMENT
+        this.actions.get("saveButton").hide();
+
+        if(options.context.__className == "Modal"){
+            this.actions.get("downloadFileButton").observe('click', function(){
+                if(!this.currentFile) return;
+                ajaxplorer.triggerDownload(ajxpBootstrap.parameters.get('ajxpServerAccess')+'&action=download&file='+this.currentFile);
+                return false;
+            }.bind(this));
+        }else{
+            this.actions.get("downloadFileButton").hide();
+        }
+	},
+	
+	open : function($super, nodeOrNodes){
+		$super(nodeOrNodes);
+        this.node = nodeOrNodes;
+		var fileName = nodeOrNodes.getPath();
+        var fileContent = ajxpBootstrap.parameters.get('ajxpServerAccess')+'&get_action=download&file='+fileName;
+
+        this.contentMainContainer = this.element.down('#webodf_container');
+        this.contentMainContainer.src = 'plugins/editor.webodf/frame.php?token='+Connexion.SECURE_TOKEN+'&file='+fileName;
+
+        this.resize();
+        this.element.observe('editor:close', function(){
+            this.contentMainContainer.src = '';
+        }.bind(this));
+        /*
+        var conn = new Connexion();
+        conn.addParameter("get_action", "get_content");
+        conn.addParameter("file", fileName);
+
+        conn.onComplete = function(transport){
+            var data = transport.responseJSON;
+            this.padID = data.padID;
+            this.sessionID = data.sessionID;
+            $("ether_box").down("#ether_box_frame").src = data.url;
+            fitHeightToBottom($("ether_box").down("#ether_box_frame"));
+        }.bind(this);
+        conn.sendAsync();
+        */
+
+		if(window.ajxpMobile){
+			this.setFullScreen();
+			attachMobileScroll(this.textarea, "vertical");
+		}
+
+
+	},
+	
+
+	saveFile : function(){
+        if(!this.padID || !this.node) return;
+        var conn = new Connexion();
+        conn.addParameter("get_action", "etherpad_save");
+        conn.addParameter("file", this.node.getPath());
+        conn.addParameter("pad_id", this.padID);
+        conn.sendAsync();
+	}
+	
+});
---
+++ core/src/plugins/editor.webodf/frame.php
@@ -0,0 +1,32 @@
+<?php
+$AJXP_SECURE_TOKEN = $_GET["token"];
+$AJXP_FILE_URL = $_GET["file"];
+?>
+<html>
+<head>
+    <script src="webodf/webodf.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" charset="utf-8">
+        function init()
+        {
+            var odfelement = document.getElementById("odf");
+            window.odfcanvas = new odf.OdfCanvas(odfelement);
+            window.odfcanvas.load("http://localhost/index.php?secure_token=<?php echo $AJXP_SECURE_TOKEN; ?>&get_action=download&file=<?php echo $AJXP_FILE_URL; ?>");
+            //window.odfcanvas.setEditable(true);
+            /*
+            odfcanvas.odfContainer().save(function(err){
+                console.log(err);
+            });
+            */
+        }
+        window.setTimeout(init, 0);
+    </script>
+    <style type="text/css">
+        .shadow_class{
+            box-shadow: 1px 1px 6px black;
+        }
+    </style>
+</head>
+<body style="background-color: rgb(85, 85, 85);">
+    <div id="odf" class="shadow_class"></div>
+</body>
+</html>
