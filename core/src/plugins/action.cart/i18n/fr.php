dfd8d471bf67bf973d490a4c37bf688319dd46eb
cdujeu <charles@ajaxplorer.info>
2013-11-15 18:25:10 +0100

    Translate action.cart

---
+++ core/src/plugins/action.cart/i18n/fr.php
@@ -0,0 +1,20 @@
+<?php
+
+$mess = array(
+
+    "1" => "Ajouter le panier",
+    "2" => "Ajouter les éléments sélectionnés au panier",
+    "3" => "Vider",
+    "4" => "Vider le panier courant",
+    "5" => "Nouveau",
+    "6" => "Créer un nouveau panier",
+    "7" => "Télécharger",
+    "8" => "Télécharger tout le panier d'un coup",
+    "9" => "Mon Panier",
+    "10"=> "Paniers",
+    "11"=> "Compresser et partager",
+    "12"=> "Créer une archive et la partager",
+    "13"=> "Changer le libellé de ce panier de sélection",
+    "14"=> "Une archive va être créée avec tous les fichiers dedans. Entrez un nom pour cette archive :",
+    "15"=> "Déposer des éléments dans le panier pour les utiliser plus tard.",
+);
