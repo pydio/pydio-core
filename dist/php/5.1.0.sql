c0e0a81a13abda8ad675da5dab372b548caca349
cdujeu <charles@ajaxplorer.info>
2013-11-17 01:56:36 +0100

    Refix sql upgrade file

--- dist/php/5.1.0.sql
+++ dist/php/5.1.0.sql
@@ -1,12 +1,12 @@
 /* SEPARATOR */
 ALTER TABLE `ajxp_feed` ADD COLUMN `index_path` MEDIUMTEXT NULL;
 /* SEPARATOR */
-ALTER TABLE `ajxp_simple_store` ADD COLUMN `insertion_date` DATETIME DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `ajxp_simple_store` ADD COLUMN `insertion_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 /* SEPARATOR */
-CREATE TABLE IF NOT EXISTS ajxp_user_teams {
+CREATE TABLE IF NOT EXISTS ajxp_user_teams (
     team_id VARCHAR(255) NOT NULL,
     user_id varchar(255) NOT NULL,
     team_label VARCHAR(255) NOT NULL,
     owner_id varchar(255) NOT NULL,
     PRIMARY KEY(team_id, user_id)
-}
+);
