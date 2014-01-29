96a6fb3df24fffeeccbc9fd076cf8fe45c928fad
Etienne CHAMPETIER <etienne.champetier@fiducial.net>
2014-01-29 10:32:52 +0100

    Move regexpToLike to AJXP_Utils
    Signed-off-by: Etienne CHAMPETIER <etienne.champetier@fiducial.net>

--- core/src/plugins/auth.sql/class.sqlAuthDriver.php
+++ core/src/plugins/auth.sql/class.sqlAuthDriver.php
@@ -61,7 +61,7 @@ class sqlAuthDriver extends AbstractAuthDriver
     public function listUsersPaginated($baseGroup, $regexp, $offset, $limit)
     {
         if ($regexp != null) {
-            $like = self::regexpToLike($regexp);
+            $like = AJXP_Utils::regexpToLike($regexp);
             $res = dibi::query("SELECT * FROM [ajxp_users] WHERE [login] ".$like." AND [groupPath] LIKE %like~ ORDER BY [login] ASC", $regexp, $baseGroup) ;
         } else if ($offset != -1 || $limit != -1) {
             $res = dibi::query("SELECT * FROM [ajxp_users] WHERE [groupPath] LIKE %like~ ORDER BY [login] ASC %lmt %ofs", $baseGroup, $limit, $offset);
@@ -84,7 +84,7 @@ class sqlAuthDriver extends AbstractAuthDriver
 
         $wheres = array();
         if(!empty($regexp)){
-            $wheres[] = "[ajxp_users].[login] ". self::regexpToLike($regexp);
+            $wheres[] = "[ajxp_users].[login] ". AJXP_Utils::regexpToLike($regexp);
         }
         $wheres[] = "[ajxp_users].[groupPath] LIKE %like~";
 
@@ -120,31 +120,13 @@ class sqlAuthDriver extends AbstractAuthDriver
 
         if (!empty($regexp)) {
             $res = dibi::query($q, $regexp, $baseGroup);
-            //$like = self::regexpToLike($regexp);
+            //$like = AJXP_Utils::regexpToLike($regexp);
             //$res = dibi::query("SELECT COUNT(*) FROM [ajxp_users] WHERE [login] ".$like." AND [groupPath] LIKE %like~", $regexp, $baseGroup) ;
         } else {
             $res = dibi::query($q, $baseGroup);
             //$res = dibi::query("SELECT COUNT(*) FROM [ajxp_users] WHERE [groupPath] LIKE %like~", $baseGroup);
         }
         return $res->fetchSingle();
-    }
-
-    private static function regexpToLike(&$regexp)
-    {
-        $left = "~";
-        $right = "~";
-        if ($regexp[0]=="^") {
-            $regexp = ltrim($regexp, "^");
-            $left = "";
-        }
-        if ($regexp[strlen($regexp)-1] == "$") {
-            $regexp = rtrim($regexp, "$");
-            $right = "";
-        }
-        if ($left == "" && $right == "") {
-            return "= %s";
-        }
-        return "LIKE %".$left."like".$right;
     }
 
     public function listUsers($baseGroup="/")
