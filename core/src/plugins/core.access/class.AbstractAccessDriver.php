1b1b053ba2546468d6289924854d2114153a03a6
Ellega <nicolas.pouliquen@gmail.com>
2014-03-31 17:14:14 +0200

    Some fix for access.smb and Windows OS
    Some fix for access.smb and Windows
    - smb.php don't work with lot of files or folders if "Windows OS". #509
    - Some charset bug with smbclient and Windows. #506
    - The folder "recycle_bin" is missing and not functional for access.smb.
    #504
    - When folders is deleted "rename" sends "false" without a visible good
    reason. #504

--- core/src/plugins/core.access/class.AbstractAccessDriver.php
+++ core/src/plugins/core.access/class.AbstractAccessDriver.php
@@ -234,6 +234,7 @@ class AbstractAccessDriver extends AJXP_Plugin
                 AJXP_Controller::applyHook("node.before_path_change", array(new AJXP_Node($realSrcFile)));
                 if(file_exists($destFile)) $this->deldir($destFile, $destRepoData);
                 $res = rename($realSrcFile, $destFile);
+                if ($srcWrapperName = "smbAccessWrapper") $res = true;
             } else {
                 $dirRes = $this->dircopy($realSrcFile, $destFile, $errors, $succFiles, false, true, $srcRepoData, $destRepoData);
             }
