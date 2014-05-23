6439e0536e4ab69628667322ee4230329872d74e
DepaMarco <depardimarco@hotmail.it>
2014-05-23 16:30:11 +0200

    Added Italian translation for plugin 'access.ajxp_user'

---
+++ core/src/plugins/access.ajxp_user/i18n/it.php
@@ -0,0 +1,120 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+$mess=array(
+"title"=> "Bacheca",
+"desc" => "Modifica le impostazioni dell'account, della ribrica, degli elementi condivisi ecc.",
+"1" => "Rubrica",
+"2" => "Cartelle condivise",
+"3" => "File condivisi",
+"4"	=> "Percorso File",
+"5"	=> "Repository",
+"6"	=> "Password",
+"7"	=> "Scadenza",
+"8" => "Elementi",
+"9" => "Utenti associati",
+"10"=> "Workspaces associati",
+"11"=> "Se sicuro di voler cancellare gli elementi selezionati?",
+"12"=> "Non hai il permesso di cancellare questi file.",
+"13"=> "File pubblico cancellato correttamente.",
+"14"=> "Integrità",
+"15"=> "Ok",
+"16"=> "Corrotto",
+"17"=> "Download URL",
+"18"=> "Copia URL",
+"19"=> "Copia l'URL per inviarlo tramite email.",
+"20"=> "Downloads",
+"21"=> "Si",
+"22"=> "No",
+"23"=> "%s file cancellati correttamente.",
+"24"=> "Niente da cancellare.",
+"25"=> "Elimina Scaduti",
+"26"=> "Elimina i file condivisi scaduti.",
+"27"=> "Proprietario",
+"28"=> "File condivisi per il download diretto, sia protetti da password che non.",
+"29"=> "Delega workspaces creati dagli utenti.",
+"30"=> "Utenti creati per accedere ai workspaces delegati.",
+"31"=> "Descrizione",
+"32"=> "Reset",
+"33"=> "Reset contatore download",
+"34"=> "Mie Condivisioni",
+"35"=> "Tutti gli elementi condivisi (file e cartelle)",
+"36"=> "Parametri",
+"37"=> "Il mio profilo e le impostazioni dell'applicativo",
+"38"=> "Notifiche",
+"39"=> "Seleziona un workspace",
+"40"=> "Benvanuto, %s",
+"41"=> "Usa questo workspace come principale",
+"42"=> "Entra",
+"43"=> "Il Mio Profilo",
+"43t"=> "Tutti miei dati personali",
+"44"=> "Il Mio Account",
+"45"=> "Dati personali",
+"46"=> "Rubrica",
+"47"=> "Utenti e teams che hai creato",
+"48"=> "I Miei Utenti",
+"49"=> "Utenti esterni da te creati",
+"50"=> "I Miei Teams",
+"51"=> "Teams di utenti, usati come scorciatoia durante la condivisione",
+"52"=> "Sei sicuro di voler cancellare questo team? Questa operazione non cancellerà gli utenti associati..",
+"53"=> "Partenza!",
+"54"=> "Capisci come iniziare con Pydio seguendo i video 'How-to'",
+"55"=> "Hai bisogno di aiuto <a>per iniziare?</a>",
+"56"=> "Capisci come iniziare con Pydio? Ecco alcuni video per te!",
+"57"=> "Download Pydio per...",
+"58"=> "Dispositivi Android",
+"59"=> "iPhone/iPad",
+    "60"=>"Mac OS (Beta)",
+    "61"=>"Windows (Beta)",
+    "62"=>"<h2>Iniziamo!</h2>
+            Questo video è un'introduzione alla web application Pydio:
+            <ul>
+                <li>Bacheca Utente</li>
+                <li>Panoramica GUI</li>
+                <li>Gestione file e cartelle</li>
+                <li>Anteprima e modifica dei file</li>
+                <li>Accesso rapido, grazie ai segnalibri ed alle scorciatoie</li>
+                <li>Carrello selezioni</li>
+            </ul>",
+    "63"=>"<h2>Condividere file con Pydio</h2>
+                            In questo tutorial, ci approcceremo alle basi della condivisione con Pydio:
+                            <ul>
+                                <li>Creare link web</li>
+                                <li>Inviarli ad utenti esterni od interni</li>
+                                <li>Essere informati di quando un contenuto viene scaricato</li>
+                                <li>Creare link protetti da password</li>
+                                <li>Creare più link su di un singolo file</li>
+                                <li>Terminare totalmente la condivisione</li>
+            </ul>",
+    "64"=>"<h2>Condividere cartelle con Pydio</h2>
+                            Questo video mostra le varie opzioni per la condivisione delle cartelle mediante Pydio
+                            <ul>
+                                <li>Condividere come nuovo workspace</li>
+                                <li>Condividere come un Minisito pubblico</li>
+            </ul>",
+    "65"=> "Scopri di più sul canale ufficiale Pydio!",
+    "66"=>"<h2>Usare Pydio su dispositivi iOS</h2>
+                            Questo video spiega come usare l'applicazione Pydio (disponibile sull'App Store) su dispositivi muniti di iOS. Il procedimento è abbastanza simile anche per gli utenti Android..
+<ul><li>Configurare la connessione al server</li>
+<li>Navigare tra i file</li>
+<li>Mantenere un file disponibile offline e le interazioni con altre applicazioni</li>
+<li>Protezione dell'app attraverso un codice PIN</li>
+<li>Uso dei segnalibri e del motore di ricerca</li></ul>",
+);
