37b45806332cbfa9996799eb43dc88462e3415fc
DepaMarco <depardimarco@hotmail.it>
2014-06-03 15:43:02 +0200

    Italian translation for 'access.sftp_psl' plugin

---
+++ core/src/plugins/access.sftp_psl/i18n/conf/it.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+$mess=array(
+"SSH File Transfer Protocol (SFTP)" => "SSH File Transfer Protocol (SFTP)",
+"The SSH File Transfer Protocol is a network protocol that provides file access, file transfer, and file management functionalities over SSH2." => "Questo protocollo è un protocollo di rete che fornisce accesso ai file, il loro trasferimento e la loro gestione, attraverso l'SSH2.",
+"Host" => "Host",
+"SFTP Host to connect to" => "Host SFTP al quale connettersi",
+"Port" => "Porta",
+"SFTP Host port" => "Porta dell'Host SFTP",
+"Path" => "Percorso",
+"Real path to the root folder on the server" => "Percorso reale alla cartella principale sul server",
+"Fix Permissions" => "Permessi",
+"How to handle remote permissions to be used by PHP as local permissions. See manual." => "Come gestire i permessi remoti per essere utilizzati dal PHP come permessi locali. Se veda il manuale."
+);
