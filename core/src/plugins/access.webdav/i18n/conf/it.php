fc07ae7b131dbc5b4dc6d2dc45ecffcff03e6171
DepaMarco <depardimarco@hotmail.it>
2014-06-11 16:28:58 +0200

    Italian translation for plugin 'access.webdav'

---
+++ core/src/plugins/access.webdav/i18n/conf/it.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"WebDAV (PEAR)" => "WebDAV (PEAR)",
+"Access a WebDAV server. Requires PEAR/HTTP_WebDAVClient installed with the patched found in the plugin folder applied." => "Accesso ad un server WebDav. Richiede PEAR/HTTP_WebDAVClient installato, patchato usando il contenuto della cartella del plugin.",
+"Host" => "Host",
+"Host of the WebDAV server, with http/https and without trailing slash. Can contain user:password too." => "Host del server WebDav, da indicare con http/https e senza la slash finale. Può contenere anche utente:password.",
+"Uri" => "Uri",
+"Path to the root folder on the server" => "Percorso alla cartella principale sul server",
+"File Creation Mask" => "Maschera Creazione File",
+"Optionnaly apply a chmod operation. Value must be numeric, like 0777, 0644, etc." => "Opzionalmente, verrà applicata un'operazione chmod. Il valore deve essere numerico, come ad esempio 0777, 0644, ecc.",
+);
