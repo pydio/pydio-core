a1660849f264bb3c0bc5e29e5377966679c5a155
DepaMarco <depardimarco@hotmail.it>
2014-06-12 15:11:41 +0200

    Italian translation for plugin 'action.powerfs'

---
+++ core/src/plugins/action.powerfs/i18n/conf/it.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+$mess = array(
+    // will be replaced by the application title
+    "Power FS" => "Power FS",
+    "This set of extensions assume that you have an administration access to your server: ability to tweak the webserver and the php configuration, ability to access the command line, etc." => "Questo set di estensione assume che tu abbia l'accesso come amministratore al proprio server: possibilità di modificare il webserver e la configurazione PHP, possibilità di accedere alla linea di comando ecc."
+);
