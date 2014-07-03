33c9be3c14007d615ae8444c7ec1cf7806082f66
DepaMarco <depardimarco@hotmail.it>
2014-07-03 09:29:46 +0200

    Italian translation for plugin 'action.timestamp' and FIX the English one

---
+++ core/src/plugins/action.timestamp/i18n/conf/it.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"Timestamp" => "Timestamp",
+"Allow Timestamp file with Universign." => "Abilita il file Timestamp con Universing.",
+"msgBox1" => "Conferma di voler aggiungere un timestamp a questo file",
+"URL" => "URL",
+"Timestamp provider API URL" => "URL delle API del fornitore di Timestamp",
+"User Mail" => "Email Utente",
+"Password" => "Password",
+"User account" => "Account",
+"User Password" => "Password",
+"timestamp" => "timestamp",
+"Allow timestamp files with Universign." => "Permetti file timestamp con Universign.",
+);
