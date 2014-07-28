9f76c93a2316a51c7d52ef251052f2667c760a56
DepaMarco <depardimarco@hotmail.it>
2014-07-28 09:43:46 +0200

    Italian translation for 'action.updater' plugin.

---
+++ core/src/plugins/action.updater/i18n/conf/it.php
@@ -0,0 +1,38 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"Update Engine" => "Motore Aggiornamento",
+"Automatic update of Pydio. Since version 3.3.3" => "Aggiornamento automatico di Pydio. Sin dalla versione 3.3.3",
+"Update Site" => "Aggiorna Sito",
+"Where to update" => "Dove aggiornare",
+"Preserve Files" => "Preserva File",
+"Files declared here (enter pathes from the root of Ajxp installation, comma-separated) will be backed up and restored before and after upgrade." => "I file dichiarati quì (inserisci i percorsi dalla root d'installazione di Pydio, separati da virgola) verranno salvati e ripristinati dopo l'aggiornamento.",
+"Enable Import From v3.2.4" => "Abilita Importazione Dalla v3.2.4",
+"Enable the migration action, available in the Settings repository" => "Abilita l'azione di migrazione, disponibile in Impostazioni repository",
+"Update Channel" => "Canale Aggiornamento",
+"Check stable or dev channel" => "Controlla il canale stabile o di sviluppo",
+"Proxy Host" => "Host Proxy",
+"Use a predefined proxy to establish connexion" => "Usa un proxy predefinito per stabilire la connessione",
+"Proxy User" => "Utente Proxy",
+"Predefined proxy user name" => "Utente predefinito per il Proxy",
+"Proxy Pass" => "Password Proxy",
+"Predefined proxy password" => "Password perdefinita per il Proxy",
+);
