8eafe3979803999fea2665048b21e4272c7d762d
mtskar <dbosenko@karelia.mts.ru>
2014-08-05 10:28:53 +0400

    Create ru.php

---
+++ core/src/plugins/meta.user/i18n/ru.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+$mess=array(
+"1" => "Метаданные",
+"2" => "Нет метки",
+"3" => "Удалить рейтинг",
+"4" => "Неважно",
+"5" => "Сделать",
+"6" => "Персональное",
+"7" => "Работа",
+"8" => "Важно",
+"9" => "Нет значений",
+);
