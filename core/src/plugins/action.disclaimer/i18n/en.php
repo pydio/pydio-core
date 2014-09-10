8ca81737eb7739759b3574f10e2e0bc8fd7e4725
cdujeu <charles@ajaxplorer.info>
2014-09-10 22:20:34 +0200

    Basic Disclaimer plugin - still need to find a way to apply specific lock value to a given set of users

---
+++ core/src/plugins/action.disclaimer/i18n/en.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Disclaimer plugin for Pydio
+*/
+$mess=array(
+    "1" => "Disclaimer",
+    "2" => "Accept Disclaimer",
+    "3" => "You must accept the general usage conditions below. Please read and use the checkbox to accept.",
+    "4" => "I therefore accept the conditions of usage"
+);
