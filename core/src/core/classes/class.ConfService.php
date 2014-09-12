73c78cfc29364de2d1f7557dbaa8e275d428e451
cdujeu <charles@ajaxplorer.info>
2014-09-12 09:15:00 +0200

    Fix null user getLock at startup

--- core/src/core/classes/class.ConfService.php
+++ core/src/core/classes/class.ConfService.php
@@ -563,9 +563,11 @@ class ConfService
     {
         // APPEND CONF FILE REPOSITORIES
         $loggedUser = AuthService::getLoggedUser();
-        $l = $loggedUser->getLock();
         $objList = array();
-        if(!empty($l)) return $objList;
+        if($loggedUser != null){
+            $l = $loggedUser->getLock();
+            if( !empty($l)) return $objList;
+        }
         foreach ($this->configs["DEFAULT_REPOSITORIES"] as $index=>$repository) {
             $repo = self::createRepositoryFromArray($index, $repository);
             if($scope == "user" && $loggedUser != null && !self::repositoryIsAccessible($index, $repo, $loggedUser)){
