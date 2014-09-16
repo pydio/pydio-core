f1f8ead97bcb8f5eb06c5172ab9cadae1017b10b
Nicola Mustone <mustone.nicola@gmail.com>
2014-09-16 10:39:04 +0200

    Added it translation

---
+++ core/src/plugins/boot.conf/i18n/it.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+
+$mess = array(
+    "1" => "Installazione Wizard Pydio",
+    "2" => "Benvenuto nell'Installazione Wizard di Pydio",
+    "3" => "Grazei per aver installato Pydio!<br> Questo tool si assicurerà che la tua piattaforma di condivisione sia funzionante in poco tempo: naviga nelle varie sezioni per impostare i comportamenti generali dell'applicazione. Puoi posizionare il puntatore del mouse sulle etichette dei campi per maggiori informazioni. <br>Sei pronto ? ",
+    "4" => "Inizia!",
+    "5" => "Per favore attendi mentre viene configurato Pydio! Sarà disponibile in pochi secondi...",
+    "6" => "Installa Pydio Ora",
+    "7" => "Lingua",
+);
---
+++ core/src/plugins/meta.comments/i18n/it.php
@@ -0,0 +1,8 @@
+<?php
+
+$mess = array(
+"1" => "Commenti Utenti",
+"2" => "Inserisci il tuo commento qui",
+"3" => "Pubblica",
+"4" => "Vai a %s",
+);
