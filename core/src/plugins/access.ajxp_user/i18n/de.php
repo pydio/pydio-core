1acac1c142c71e7c723476dab3489bf56093cdfd
Florian Vogt <flori@vorlif.org>
2014-09-30 10:18:11 +0200

    German translation - fix fail

--- core/src/plugins/access.ajxp_user/i18n/de.php
+++ core/src/plugins/access.ajxp_user/i18n/de.php
@@ -20,7 +20,7 @@
 */
 $mess=array(
 "1" => "Gemeinsame Benutzer",
-"2" => "Gemeinsame Repository",
+"2" => "Gemeinsame Repositorys",
 "3" => "Öffentliche Dateien",
 "4"	=> "Dateipfad",
 "5"	=> "Repository",
