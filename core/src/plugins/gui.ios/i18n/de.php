60452e450845ead06f031b0d4ddbb0a350722a9d
Florian Vogt <flori@vorlif.org>
2014-09-30 17:35:16 +0200

    Create files for german translation
    Create files and start translation

---
+++ core/src/plugins/gui.ios/i18n/de.php
@@ -0,0 +1,30 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"1.ios" => "Für den Zugriff auf %s steht ein Nativer iOs Client zu Verfügung.",
+"2.ios" => "Ich habe den Client bereits. Öffne ihn und füge diesen Server zu meinen Remote Servern hinzu.",
+"3.ios" => "Ich habe den Client nicht. Jetzt im App Store öffnen!",
+"4.ios" => "Ich habe kein Interesse, lass mich die Webversion von %s benutzen.",
+"1.android" => "Für den Zugriff auf %s steht ein Nativer Android Client zu Verfügung.",
+"2.android" => "Ich habe den Client bereits. Öffne ihn und füge diesen Server zu meinen Remote Servern hinzu.",
+"3.android" => "Ich habe den Client nicht. Jetzt im Play Store öffnen!",
+"4.android" => "Ich habe kein Interesse, lass mich die Webversion von %s benutzen.",
+);
