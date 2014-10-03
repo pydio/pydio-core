2249f0df8cbbbd671e3b853e2d2424817978bff8
Martin Schaible <martin@netfusion.ch>
2014-10-03 02:03:38 +0200

    Umlauts with its HTML entities replaced String "Workplace" translated Some translation corrected

--- core/src/plugins/access.ajxp_home/i18n/de.php
+++ core/src/plugins/access.ajxp_home/i18n/de.php
@@ -18,66 +18,70 @@
 *
 * The latest code can be found at <http://pyd.io/>.
 */
+/*******************************************************************************
+* German translation:
+*   + update: Martin Schaible <martin@martinschaible.ch>
+*******************************************************************************/ 
 $mess=array(
 "title" => "Mein Dashboard",
 "desc" => "Eigene Kontoeinstellungen, eigenes Adressbuch, geteilte Elemente, etc. verwalten",
-"39"=> "Workspace auswählen",
+"39"=> "Arbeitsumgebung ausw&auml;hlen",
 "40"=> "Willkommen, %s",
-"41"=> "Diesen Workspace standardmässig verwenden",
-"42"=> "Betreten",
+"41"=> "Diese Arbeitsumgebung als Standard verwenden",
+"42"=> "Eintreten",
 "43"=> "Mein Profil",
-"43t"=> "Alle meine persönlichen Dateien",
+"43t"=> "Alle meine pers&ouml;nlichen Dateien",
 "44"=> "Mein Konto",
-"45"=> "Persönliche Daten",
+"45"=> "Pers&ouml;nliche Daten",
 "46"=> "Adressbuch",
 "47"=> "Benutzer und Teams, die Sie erstellt haben",
 "48"=> "Meine Benutzer",
 "49"=> "Externe Benutzer, die Sie erstellt haben",
 "50"=> "Meine Teams",
 "51"=> "Teams von Benutzern zum schnellen Teilen von Inhalten",
-"52"=> "Wollen Sie dieses Team wirklich löschen? Es werden dabei keine Benutzer gelöscht.",
+"52"=> "Wollen Sie dieses Team wirklich l&ouml;schen? Es werden dabei keine Benutzer gel&ouml;scht.",
     "53"=> "Erste Schritte",
     "54"=> "Machen Sie Ihre ersten Schritte mit APPLICATION_TITLE mit folgenden Video-Kurzanleitungen",
-    "55"=> "Brauchen Sie Unterstützung für die <a>ersten Schritte?</a>",
+    "55"=> "Brauchen Sie Unterst&uuml;tzung f&uuml;r die <a>ersten Schritte?</a>",
     "56"=> "Ihre ersten Schritte mit APPLICATION_TITLE? Hier finden Sie einige Videos dazu.",
-    "57"=> "APPLICATION_TITLE herunterladen für...",
-    "58"=> "Android-Geräte",
+    "57"=> "APPLICATION_TITLE herunterladen f&uuml;r...",
+    "58"=> "Android-Ger&auml;te",
     "59"=> "iPhone/iPad",
     "60"=>"Mac OS (Beta)",
     "61"=>"Windows (Beta)",
     "62"=>"<h2>Erste Schritte</h2>
-            Dieses Video gibt eine Einführung zur APPLICATION_TITLE Webapplikation:
+            Dieses Video gibt eine Einf&uuml;hrung zur APPLICATION_TITLE Webapplikation:
             <ul>
                 <li>Benutzer-Dashboard</li>
-                <li>Übersicht über die Benutzeroberfläche</li>
+                <li>&uuml;bersicht &uuml;ber die Benutzeroberfl&auml;che</li>
                 <li>Dateien und Ordner verwalten</li>
                 <li>Vorschau und Bearbeitung von Dateien</li>
-                <li>Schellzugriff über Lesezeichen oder die Suche</li>
+                <li>Schellzugriff &uuml;ber Lesezeichen oder die Suche</li>
                 <li>Auswahl-Liste</li>
             </ul>",
     "63"=>"<h2>Dateien mit APPLICATION_TITLE teilen</h2>
-                            In dieser Erklärung finden Sie die Grundlagen zum Teilen von Dateien mit APPLICATION_TITLE:
+                            In dieser Erkl&auml;rung finden Sie die Grundlagen zum Teilen von Dateien mit APPLICATION_TITLE:
                             <ul>
                                 <li>Neue Web-Links erstellen</li>
                                 <li>Links internen oder externen Benutzern senden</li>
                                 <li>Benachrichtigungen beim Download eines Links</li>
-                                <li>Erstellen eines Passwort-geschützten Links</li>
+                                <li>Erstellen eines Passwort-gesch&uuml;tzten Links</li>
                                 <li>Erstellen mehrerer Links zu einer Datei</li>
                                 <li>Teilen einer Datei beenden</li>
             </ul>",
     "64"=>"<h2>Ordner mit APPLICATION_TITLE teilen</h2>
-                            Das folgende Video erklärt das Teilen von Ordnern mit APPLICATION_TITLE
+                            Das folgende Video erkl&auml;rt das Teilen von Ordnern mit APPLICATION_TITLE
                             <ul>
-                                <li>Als neuen Workspace teilen</li>
-                                <li>Als öffentliche Miniseite teilen</li>
+                                <li>Als neue Arbeitsumgebung teilen</li>
+                                <li>Als &ouml;ffentliche Miniseite teilen</li>
             </ul>",
-    "65"=> "Mehr über APPLICATION_TITLE im YouTube-Kanal erfahren!",
-    "66"=>"<h2>APPLICATION_TITLE auf iOS-Geräten verwenden</h2>
-                            Das folgende Video erklärt die Verwendung der APPLICATION_TITLE App (verfügbar im App Store). Die Nutzung auf Android ist ähnlich.
+    "65"=> "Mehr &uuml;ber APPLICATION_TITLE im YouTube-Kanal erfahren!",
+    "66"=>"<h2>APPLICATION_TITLE auf iOS-Ger&auml;ten verwenden</h2>
+                            Das folgende Video erkl&auml;rt die Verwendung der APPLICATION_TITLE App (verf&uuml;gbar im App Store). Die Nutzung auf Android ist &auml;hnlich.
 <ul><li>Serververbindung einrichten</li>
 <li>Dateinavigation</li>
 <li>Dateien offline speichern</li>
-<li>App mit einem PIN-Code schützen</li>
+<li>App mit einem PIN-Code sch&uuml;tzen</li>
 <li>Verwenden von Lesezeichen und der Suchfunktion</li></ul>",
     "67"=> "Sie sind nicht %s? %logout",
 );
