0b6a4e509feac41fb727ac64d54d6e8146852dbc
DepaMarco <depardimarco@hotmail.it>
2014-10-17 10:29:45 +0200

    Italian translation for plugin 'auth.serial'

---
+++ core/src/plugins/auth.serial/i18n/conf/it.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"Serialized authentication storage" => "Autenticazione Serializzata",
+"Stores the Authentication data as serialized PHP values on the filesystem." => "Salva i dati d'Autenticazione come valori serializzati, in un file PHP, sul filesystem.",
+"Users" => "Utenti",
+"The users list" => "Lista utenti",
+"Users File" => "File utenti",
+"Fast Checks" => "Controlli Rapidi",
+"When using a custom stream wrapper to read the files, this can increase performances by skipping various is_file,is_dir checks" => "Quando si utilizza un wrapper personalizzato per leggere i file, questa opzione può aumentare le performance saltando diversi controlli 'is_file' e 'is_dir'",
+);
