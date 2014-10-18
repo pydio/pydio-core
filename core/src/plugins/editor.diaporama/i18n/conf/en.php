10184e58c1a79fee737e4b7655041d684de395cc
Anael Mobilia <contrib@anael.eu>
2014-10-18 17:56:11 +0200

    Typo (minor)

--- core/src/plugins/editor.diaporama/i18n/conf/en.php
+++ core/src/plugins/editor.diaporama/i18n/conf/en.php
@@ -20,7 +20,7 @@
 */
 $mess=array(
 "Image viewer" => "Image viewer",
-"Image viewer, used for previewing images and browsing them in a diaporam. Generates a thumbnail version of the images." => "Image viewer, used for previewing images and browsing them in a diaporam. Generates a thumbnail version of the images.",
+"Image viewer, used for previewing images and browsing them in a diaporam. Generates a thumbnail version of the images." => "Image viewer, used for previewing images and browsing them in a diaporama. Generates a thumbnail version of the images.",
 "Remote Wrappers" => "Remote Wrappers",
 "When connected to a remote server, metadata extraction can make the listing action very long, as it must first copy each file locally. For plugins using the defined wrappers and files whose size is greater than this threshold, the image type and dimension will be skipped." => "When connected to a remote server, metadata extraction can make the listing action very long, as it must first copy each file locally. For plugins using the defined wrappers and files whose size is greater than this threshold, the image type and dimension will be skipped.",
 "Size Threshold" => "Size Threshold",
