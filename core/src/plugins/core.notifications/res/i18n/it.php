0fd83fb58de8e59a11e6dd7e4342883816d62095
DepaMarco <depardimarco@hotmail.it>
2014-11-05 15:57:06 +0100

    Italian translation for plugin 'core.notifications'

---
+++ core/src/plugins/core.notifications/res/i18n/it.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Copyright 2007-2012 Charles du Jeu <contact (at) cdujeu.me>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"1" => "Notifiche",
+"2" => "Feed Notifiche",
+"3" => "Avvisi",
+"4" => "Notifiche",
+"5" => "Tutti gli Eventi",
+"6" => "Vai a ...",
+"7" => "Rimuovi",
+"8" => "Nessun avviso",
+"9" => "Attività Workspace",
+"10"=> "Attività Cartella",
+"11"=> "Attività File",
+);
