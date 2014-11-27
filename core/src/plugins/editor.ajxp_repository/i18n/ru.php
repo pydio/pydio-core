d6dff61fb055a644f0321450946b83b08f75789b
mtskar <dbosenko@karelia.mts.ru>
2014-11-27 10:15:01 +0300

    Create ru.php

---
+++ core/src/plugins/editor.ajxp_repository/i18n/ru.php
@@ -0,0 +1,37 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"1" => "редактор рабочих пространств",
+"2" => "Редактор рабочих пространств",
+"3" => "Опции драйвера",
+"4" => "Возможности рабочего пространства",
+"5" => "Выберите во возможность",
+"6" => "Редактирование параметров",
+"7" => "Use the selector below to activate plugins that will add more functionnalities to the workspace. Don't forget to click on the Add a Source button to apply your changes.",
+"8" => "Нажмите на плагин для редактирования параметров или просто удалите эту возможность (плагин)",
+"9" => "%s плагинов",
+"10" => "Meta Sources",
+"11" => "Add a source",
+"12" => "Meta Plugin",
+"13" => "Are you sure you want to delete this source?",
+"14" => "Please choose a driver!",
+"15" => "This workspace is defined in the config file (see conf/bootstrap_repositories.php), you cannot edit its features via the GUI.",
+);
