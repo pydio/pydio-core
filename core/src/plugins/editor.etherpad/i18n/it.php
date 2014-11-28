aa103aa726e94003e9b7eb66310bcf3aa3d7c608
DepaMarco <depardimarco@hotmail.it>
2014-11-28 09:41:43 +0100

    Italian translation for plugin 'editor.etherpad'

---
+++ core/src/plugins/editor.etherpad/i18n/it.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+
+$mess = array(
+    "1" => "Area Collaborazione",
+    "1b"=> "Crea o unisciti ad una sessione collaborativa",
+    "2" => "Editor Collaborativo",
+    "3" => "Editor collaborativo in tempo reale",
+    "4" => "Unisciti ad una sessione collaborativa esistente o creare una nuova: ",
+    "5" => "Sessioni Libere",
+    "6" => "File",
+    "7" => "Entra",
+    "8" => "Nome Sessione",
+    "9" => "Crea Nuova Sessione",
+);
