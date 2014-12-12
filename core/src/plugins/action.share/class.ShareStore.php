0ee767795461f24a479fd9c5a902f99ce0670c94
cdujeu <charles@ajaxplorer.info>
2014-12-12 09:24:22 +0100

    Update share.php template

--- core/src/plugins/action.share/class.ShareStore.php
+++ core/src/plugins/action.share/class.ShareStore.php
@@ -47,12 +47,14 @@ class ShareStore {
     private function createGenericLoader(){
         if(!is_file($this->downloadFolder."/share.php")){
             $loader_content = '<'.'?'.'php
-                    $hash = $_GET["hash"];
+                    define("AJXP_EXEC", true);
+                    require_once("'.str_replace("\\", "/", AJXP_INSTALL_PATH).'/core/classes/class.AJXP_Utils.php");
+                    $hash = AJXP_Utils::securePath(AJXP_Utils::sanitize($_GET["hash"], AJXP_SANITIZE_ALPHANUM));
                     if(file_exists($hash.".php")){
                         require_once($hash.".php");
                     }else{
                         require_once("'.str_replace("\\", "/", AJXP_INSTALL_PATH).'/publicLet.inc.php");
-                        ShareCenter::loadShareByHash($_GET["hash"]);
+                        ShareCenter::loadShareByHash($hash);
                     }
                 ';
             if (@file_put_contents($this->downloadFolder."/share.php", $loader_content) === FALSE) {
