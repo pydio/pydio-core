072c6ef3fe477f40ef1386de937b023116d28f03
DepaMarco <depardimarco@hotmail.it>
2014-12-22 14:55:45 +0100

    Italian translation for plugin 'editor.other'

---
+++ core/src/plugins/editor.other/resources/i18n/it.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+$mess=array(
+"1" => "Scegli altro...",
+"2" => "Scegli un altro editor",
+"3" => "Seleziona un altro editor per aprire questo file",
+"4" => "ATTENZIONE: Questi editor non dichiarano di supportare questo tipo di file. Il loro comportamento è imprevedibile!",
+"5" => "Elimina le associazioni esistenti",
+);
