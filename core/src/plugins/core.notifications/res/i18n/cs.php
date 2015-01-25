7dc955cb684e3ee4e673fce192663d027ed78ad7
Jaroslav Lichtblau <svetlemodry@lichtblau.cz>
2015-01-25 17:25:52 +0100

    core.notifications Czech translation

---
+++ core/src/plugins/core.notifications/res/i18n/cs.php
@@ -0,0 +1,33 @@
+<?php
+/*
+* Copyright 2007-2012 Charles du Jeu <contact (at) cdujeu.me>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"1" => "Upozornění",
+"2" => "Kanál upozornění",
+"3" => "Výstrahy",
+"4" => "Upozornění",
+"5" => "Všechny události",
+"6" => "Jít na ...",
+"7" => "Zamítnout",
+"8" => "Žádné výstrahy",
+"9" => "Změny úložiště",
+"10"=> "Změny adresáře",
+"11"=> "Změny souboru",
+);
