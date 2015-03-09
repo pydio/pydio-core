af05e6d3719da13fe43e8cc4e2c7c0c0fe084f9c
DepaMarco <depardimarco@hotmail.it>
2015-03-09 10:01:00 +0100

    Italian translation for plugin 'index.lucene'

---
+++ core/src/plugins/index.lucene/resources/i18n/conf/it.php
@@ -0,0 +1,44 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"Lucene Search Engine" => "Motore Rierca Lucene",
+"Zend_Search_Lucene implementation to index all files and search a whole repository quickly." => "Implementazione di Zend_Search_Lucene per indicizzare tutti i file e cercare all'interno di un repository velocemente.",
+"Index Content" => "Indicizza Contenuto",
+"Parses the file when possible and index its content (see plugin global options)" => "Analizza il file quando possibile ed indicizzane il contenuto (si vedano le impostazioni globali dei plugin)",
+"Index Meta Fields" => "Indicizza Campi Meta",
+"Which additionnal fields to index and search" => "Quali campi aggiuntivi indicizzare e cercare",
+"Repository keywords" => "Parole chiave Repository",
+"If your repository path is defined dynamically by specific keywords like AJXP_USER, or your own, mention them here." => "Se il percorso del tuo workspace è definito dinamicamente con specifiche parole chiave, come AJXP_USER o personalizzate, menzionale quì.",
+"Parse Content Until" => "Analizza Contenuto Fino a",
+"Skip content parsing and indexation for files bigger than this size (must be in Bytes)" => "Salta l'analisi e l'indicizzazione di file più grandi di questa dimensione (DEVE essere in BYTES)",
+"HTML files" => "File HTML",
+"List of extensions to consider as HTML file and parse content" => "Lista delle estensioni da considerare come file HTML per analizzarne il contenuto",
+"Text files" => "File di Testo",
+"List of extensions to consider as Text file and parse content" => "Lista delle estensioni da considerare come file di testo per analizzarne il contenuto",
+"Unoconv Path" => "Percorso Unoconv",
+"Full path on the server to the 'unoconv' binary" => "Percorso completo sul server per l'eseguibile 'unoconv'",
+"PdftoText Path" => "Percorso PdftoText",
+"Full path on the server to the 'pdftotext' binary" => "Percorso completo sul server per l'eseguibile 'pdftotext'",
+"Query Analyzer" => "Analizzatore Query",
+"Analyzer used by Zend to parse the queries. Warning, the UTF8 analyzers require the php mbstring extension." => "Analizzatore usato da Zend per scansionare le query. ATTENZIONE: l'analizzatore UTF8 richiede l'estensione 'mbstring' di PHP.",
+"Wildcard limitation" => "Limitazione Wildcard",
+"For the sake of performances, it is not recommanded to use wildcard as a very first character of a query string. Lucene recommends asking the user minimum 3 characters before wildcard. Still, you can set it to 0 if necessary for your usecases." => "Per amor delle performance, non è raccomandato l'uso di wildcard come primo e solo carattere di una query. Lucene raccomanda di chiedere all'utente un minimo di 3 caratteri prima del wildcard. Ad ogni modo, si può impostare a 0 se necessario.",
+);
