e6c148f738b2c7b28b5526c6171dba473c6ce62d
DepaMarco <depardimarco@hotmail.it>
2015-03-12 16:17:24 +0100

    Italian translation for plugin 'log.sql'

---
+++ core/src/plugins/log.sql/i18n/conf/it.php
@@ -0,0 +1,28 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"DB Storage" => "Memoria DB",
+"Stores the logs in an SQL database." => "Salva i log in un database SQL.",
+"Connexion" => "Connessione",
+"The Database Connexion" => "Connessione al Database",
+"SQL Tables" => "Tabelle SQL",
+"Install SQL Tables" => "Installa Tabelle SQL",
+);
