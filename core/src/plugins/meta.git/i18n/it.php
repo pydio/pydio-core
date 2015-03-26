865f6bc681df2e3893706f682e08980c7aceb2d7
DepaMarco <depardimarco@hotmail.it>
2015-03-26 14:37:38 +0100

    Italian translation for plugin 'meta.git'

---
+++ core/src/plugins/meta.git/i18n/it.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+
+defined('AJXP_EXEC') or die('Accesso non consentito');
+
+$mess = array(
+    "1" => "Versioni",
+    "2" => "Documento versioni precedenti",
+    "3" => "Download",
+    "4" => "Download delle versioni selezionate",
+    "5" => "Anteprima (browser)",
+    "6" => "Apre le versioni selezionate in una finestra del browser",
+    "7" => "Riporta a...",
+    "8" => "Riporta il file alla revisione selezionata sul server",
+    "9" => "#",
+    "10" => "Data",
+    "11" => "Autore",
+    "12" => "Modifica",
+    "13" => "Sicuro di voler procedere? Ripristinare ad una vecchia versione creerà una nuova versione di questo documento con la revisione selezionata.",
+    "RENAME"        => "File rinominato",
+    "MOVE"          => "File spostato",
+    "MODIFICATION"  => "Contenuto modificato",
+    "DELETION"      => "File cancellato",
+    "CREATION"      => "File creato",
+);
