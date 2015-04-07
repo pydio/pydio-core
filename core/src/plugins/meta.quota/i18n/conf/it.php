d26fbdddfc6e9e050e6e720e5010f99513a4d338
DepaMarco <depardimarco@hotmail.it>
2015-04-07 09:39:09 +0200

    Italian translation for plugin 'meta.quota'

---
+++ core/src/plugins/meta.quota/i18n/conf/it.php
@@ -0,0 +1,37 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"FS Mount" => "Monta FS",
+"Mount a remote location on the file system before accessing the repository" => "Monta un percorso remoto nel filesystem prima di accedere al workspace",
+"User quota" => "Quota utilizzo",
+"Maintain the size of a given directory for each user" => "Mantieni la dimensione di una cartella selezionata per ciascun utente",
+"Usage scope" => "Ambito utilizzo",
+"Define if usage must be computed for this repository only, or cumulated on all the repositories of the user" => "Definisce se l'utilizzo deve essere calcolato solo per questa cartella o sommato a tutti i repository dell'utente.",
+"Repository" => "Repository",
+"Cumulate repositories" => "Somma repositorie",
+"User Quota" => "Quota Utente",
+"Authorized quota. Use php.ini like values (20M, 2G), etc." => "Quota autorizzata. Utilizza valori conformi al 'php.ini', come (20M, 2G), etc.",
+"Cache value" => "Valore cache",
+"Store computed quota value in the user preferences, to avoid triggering computation each time it is requested. This may introduce some lag if the repository is shared by many users." => "Salva il valore calcolato nelle preferenze dell'utente, per prevenire il ricalcolo ogni volta che viene richiesto. Questo può introdurre qualche ritardo se il repository è condiviso tra più utenti.",
+"Soft Limit (%)" => "Limite leggero (%)",
+"Custom Field (Deprecated)" => "Campo Personalizzato (Deprecato)",
+"If you want to define quotas for each user, define a custom field in the CUSTOM_DATA parameter of the conf plugin, and declare this field name here." => "Se si vuole definire una quota per ciascun utente, si definisca un campo personalizzato nel parametro CUSTOM_DATA del plugin 'conf' e se ne dichiari il nome quì.",
+);
