65319a29623a27580e118413f2717a62a4ee1b75
DepaMarco <depardimarco@hotmail.it>
2015-05-05 14:43:13 +0200

    Italian translation for plugin 'meta.user' and FIX English one

---
+++ core/src/plugins/meta.user/i18n/conf/it.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* Copyright 2007-2014 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"Text Metadata" => "Testo Metadata",
+"Simple metadata implementation stored in a hidden file." => "Semplice implementazione di metadata salvata in un file nascosto.",
+"Meta File" => "File Meta", // This is not used (code is commented out in manifest.xml)
+"Hidden file used inside folders to save meta data" => "File nascosto usato nella cartella per salvare dati meta", // This is not used (code is commented out in manifest.xml)
+"Field ID" => "Campo ID",
+"Id of fiels, use standard characters here." => "ID del campo. Utilizzare caratteri standard.",
+"Label" => "Etichetta",
+"Label of the field, human friendly" => "Etichetta del campo, human-friendly",
+"Field Type" => "Tipo Campo",
+"Type of field" => "Tipo del campo",
+"Short text" => "Testo Breve",
+"Long text" => "Testo Lungo",
+"Created by..." => "Creato da...",
+"Last updated by..." => "Ultima modifica di...",
+"Stars Rating" => "Votazione Stelle",
+"Color labels" => "Colore etichette",
+"Selection (set choices in Additional Info)" => "Selezione (imposta le voci in 'Info Addizionali')",
+"Column visibility" => "Visibilità Colonna",
+"Visible" => "Visibile",
+"Hidden" => "Nascosto",
+"Set default visibility." => "Imposta visibilità principale.",
+"Additional info" => "Info Addizionali",
+"Depending on the field type. Currently used for selection only" => "In base al tipo di campo. Attualmente utilizzato solo per la selezione.",
+);
