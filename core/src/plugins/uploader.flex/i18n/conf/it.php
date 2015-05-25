038f6b08bab7f21d2da7879f3cee761b208a6058
DepaMarco <depardimarco@hotmail.it>
2015-05-25 09:23:17 +0200

    Italian translation for plugin 'uploader.flex'

---
+++ core/src/plugins/uploader.flex/i18n/conf/it.php
@@ -0,0 +1,27 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"Flash uploader" => "Flash",
+"Legacy flash uploader, used when the browser does not support HTML5. If you want to force the usage of this uploader, deactivate the uploader.html, and remove the activeCondition tag from the uploader.flex manifest file." => "Vecchio uploader in Flash, usato quando i browser non supportavano l'HTML5. Se si vuole forzare l'uso di questo uploader, deattivare l'uploader.html e rimuovere il tag 'activeCondition' dal file 'uploader.flex'.",
+"Miscalleneous" => "Altro",
+"HTTPS Policy" => "Politica HTTPS",
+"An Https Policy file maybe used to solve Flash upload problems when in Https. Use path relative to the web root." => "Un file di politica HTTPS potrebbe essere usato per risolvere i problemi di caricamento in HTTPS con Flash. Utilizzare un percorso relativo dalle root del sito.",
+);
