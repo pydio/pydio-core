e2be0a97360e3234de288ca9cd29f28165a21431
cdujeu <charles@ajaxplorer.info>
2015-06-16 11:35:36 +0200

    All necessary exports

---
+++ core/src/plugins/gui.ajax/res/js/vendor/react/modules/export.js
@@ -0,0 +1,9 @@
+window.React = require('react');
+window.PureRenderMixin = require('react/addons').addons.PureRenderMixin;
+window.ReactCSSTransitionGroup = require('react/addons').addons.CSSTransitionGroup;
+window.ReactMUI = require('material-ui');
+window.Infinite = require('react-infinite');
+window.injectTapEventPlugin = require("react-tap-event-plugin");
+window.ReactGridLayout = require('react-grid-layout');
+window.ReactChart = require('react-chartjs');
+window.injectTapEventPlugin();
