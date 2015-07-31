973d4ee2cd0aa116d37beb58ec11ae209ace5830
DepaMarco <depardimarco@hotmail.it>
2015-07-31 11:11:08 +0200

    Update Italian translation for plugin 'access.ajxp_home'

---
+++ core/src/plugins/access.ajxp_home/i18n/conf/it.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <http://pyd.io/>.
+ */
+$mess=array(
+"Welcome Page"=> "Pagina di Benvenuto",
+"Welcome Page displaying a list of workspaces" => "Pagina di benvenuto che visualizza la lista dei workspace",
+"Open workspace by simple click" => "Apri un workspace con un semplice click",
+"Open workspace by simple click instead of double click." => "Apri un workspace con un solo click invece di due.",
+"Enable Getting Started" => "Abilita 'Getting Started'",
+"Enable addition tabulation to access getting started videos" => "Abilita tabulazione addizionale per accedere ai video 'getting started'",
+"iOS App URL" => "URL per App iOS",
+"URL of the iOS application" => "URL per App iOS",
+"Android App URL" => "URL per App Android",
+"URL of the Android application" => "URL per App Android",
+"Windows PydioSync URL" => "URL PydioSync Windows",
+"URL of the windows sync application" => "URL all'applicazione Windows per sincronizzazzione",
+"Mac PydioSync URL" => "URL PydioSync Mac",
+"URL of the mac sync application" => "URL all'applicazione Mac per sincronizzazzione",
+);
