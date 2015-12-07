aea0f07ed6e842a5653b53490618012f8b10ec32
cdujeu <charles@ajaxplorer.info>
2015-12-07 16:24:00 +0100

    Fix actions disabling merging

--- core/src/core/classes/class.AJXP_Role.php
+++ core/src/core/classes/class.AJXP_Role.php
@@ -354,10 +354,10 @@ class AJXP_Role implements AjxpGroupPathProvider
             $actions = $this->actions[AJXP_REPO_SCOPE_ALL];
         }
         if ($repository != null && isSet($this->actions[AJXP_REPO_SCOPE_SHARED]) && $repository->hasParent()) {
-            $actions = array_merge($actions, $this->actions[AJXP_REPO_SCOPE_SHARED]);
+            $actions = $this->array_merge_recursive2($actions, $this->actions[AJXP_REPO_SCOPE_SHARED]);
         }
         if ($repository != null && isSet($this->actions[$repository->getId()])) {
-            $actions = array_merge($actions, $this->actions[$repository->getId()]);
+            $actions = $this->array_merge_recursive2($actions, $this->actions[$repository->getId()]);
         }
         return $actions;
     }
