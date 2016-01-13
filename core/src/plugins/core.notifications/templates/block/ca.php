f17d0a81cf84190f0ff76006764a614381636d5f
Salva-G <sgomezm@imi.palma.es>
2016-01-13 10:11:02 +0100

    Catalan & spanish tranlations

---
+++ core/src/plugins/core.notifications/templates/block/ca.php
@@ -0,0 +1,47 @@
+<?php
+    /*
+    * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+    * This file is part of Pydio.
+    *
+    * Pydio is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Affero General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * Pydio is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+    *
+    * You should have received a copy of the GNU Affero General Public License
+    * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+    *
+    * The latest code can be found at <http://pyd.io/>.
+    */
+//      catalan translation: Salva Gómez <salva.gomez at gmail.com>, 2016
+$mess=array(
+    "user_link"  => "per",
+    "file.add" => "creat",
+    "file.delete" => "eliminat",
+    "file.change" => "modificat",
+    "file.rename" => "renombrat a AJXP_NODE_LABEL",
+    "file.view" => "consultat",
+    "file.copy" => "copiat a AJXP_PARENT_PATH",
+    "file.copy_to" => "copiat a AJXP_TARGET_FOLDER",
+    "file.copy_from" => "copiat des de AJXP_SOURCE_FOLDER",
+    "file.move" => "mogut a AJXP_PARENT_PATH",
+    "file.move_to" => "mogut a AJXP_TARGET_FOLDER",
+    "file.move_from" => "mogut des de AJXP_SOURCE_FOLDER",
+    "folder.add" => "creada",
+    "folder.delete" => "eliminada",
+    "folder.rename" => "renombrada a AJXP_NODE_LABEL",
+    "folder.change" => "contingut modificat modificado",
+    "folder.view" => "oberta",
+    "folder.copy" => "copiada des de AJXP_SOURCE_FOLDER",
+    "folder.copy_to" => "copiada a AJXP_TARGET_FOLDER",
+    "folder.copy_from" => "copiada des de AJXP_SOURCE_FOLDER",
+    "folder.move" => "moguda des de AJXP_SOURCE_FOLDER",
+    "folder.move_to" => "moguda a AJXP_TARGET_FOLDER",
+    "folder.move_from" => "moguda des de AJXP_SOURCE_FOLDER",
+    "ajxp_link" => "Faci click aquí per anar directament a <a href='AJXP_LINK'>AJXP_NODE_LABEL</a>",
+);
