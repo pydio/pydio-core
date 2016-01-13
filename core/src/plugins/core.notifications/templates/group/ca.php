e5c27e270e407b561c305d4b0a22b5e7aab51c5f
Salva-G <sgomezm@imi.palma.es>
2016-01-13 13:46:15 +0100

    core.notifications templates ca&es translations

---
+++ core/src/plugins/core.notifications/templates/group/ca.php
@@ -0,0 +1,39 @@
+<?php
+    /*
+    * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+    * This file is part of Pydio.
+    *
+    * Pydio is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU Affero General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * Pydio is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU Affero General Public License for more details.
+    *
+    * You should have received a copy of the GNU Affero General Public License
+    * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+    *
+    * The latest code can be found at <http://pyd.io/>.
+    */
+//      catalan translation: Salva Gómez <salva.gomez at gmail.com>, 2016
+$mess=array(
+    "file.add" => "Arxiu AJXP_NODE_LABEL afegit a AJXP_PARENT_PATH",
+    "file.delete" => "Arxiu AJXP_NODE_LABEL eliminat de AJXP_PARENT_PATH",
+    "file.change" => "Arxiu AJXP_NODE_LABEL modificat per AJXP_USER",
+    "file.view" => "Arxiu AJXP_NODE_LABEL va ser consultat per AJXP_USER",
+    "file.copy_to" => "AJXP_NODE_LABEL copiat a AJXP_TARGET_FOLDER",
+    "file.copy_from" => "AJXP_NODE_LABEL copiat des de AJXP_SOURCE_FOLDER",
+    "file.move_to" => "AJXP_NODE_LABEL mogut a AJXP_TARGET_FOLDER",
+    "file.move_from" => "AJXP_NODE_LABEL mogut des de AJXP_SOURCE_FOLDER",
+    "folder.add" => "Carpeta AJXP_NODE_LABEL creada",
+    "folder.delete" => "Carpeta AJXP_NODE_LABEL eliminada",
+    "folder.change" => "El contingut de AJXP_NODE_LABEL ha canviat",
+    "folder.view" => "La carpeta AJXP_NODE_LABEL va ser oberta",
+    "folder.copy_to" => "Carpeta AJXP_NODE_LABEL copiada a AJXP_TARGET_FOLDER",
+    "folder.copy_from" => "Carpeta AJXP_NODE_LABEL copiada des de AJXP_SOURCE_FOLDER",
+    "folder.move_to" => "Carpeta AJXP_NODE_LABEL moguda a AJXP_TARGET_FOLDER",
+    "folder.move_from" => "Carpeta AJXP_NODE_LABEL moguda des de AJXP_SOURCE_FOLDER",
+);
