1943e9b9f9059479794bdb2ed48337b5624be0cc
Jaroslav Lichtblau <svetlemodry@lichtblau.cz>
2016-01-17 21:25:24 +0100

    Czech translation - meta.svn plugin

---
+++ core/src/plugins/meta.svn/i18n/cs.php
@@ -0,0 +1,19 @@
+<?php
+
+$mess = array(
+"1" => "Revize adresářů",
+"11" => "SVN log aktuálního adresáře",
+"2" => "Verze",
+"21" => "Předchozí verze vybraných položek",
+"3" => "Přepnout na revizi",
+"31" => "Porovnat",
+"32" => "Vrátit",
+"33" => "Toto vytvoří novou verzi souboru s obsahem z předchozí verze. Jste si jisti?",
+"4" => "Verzování",
+"41" => "Poslední revize",
+"42" => "Datum revize",
+"43" => "Autor revize",
+"5" => "Vybrané soubory/adresáře byly zkopírovány/přesunuty (SVN)",
+"51" => "Vybrané soubory/adresáře byly smazány (SVN)",
+"61" => "Akce",
+);
