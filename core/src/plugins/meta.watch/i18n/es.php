cdd8d7ceb4b000de9e0eeb6493d8d428168d717f
Salva-G <sgomezm@imi.palma.es>
2016-02-03 10:38:06 +0100

    meta.watch ca & es translations

---
+++ core/src/plugins/meta.watch/i18n/es.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: admin
+ * Date: 04/10/12
+ * Time: 17:16
+ * To change this template use File | Settings | File Templates.
+ */
+ //	Spanish translation: Salva Gómez <salva.gomez at gmail.com> 2015
+$mess = array(
+    "1" => "Monitorizar...",
+    "2" => "Notificar cuando se accede a esta carpeta",
+    "3" => "Detener monitorización",
+    "4" => "Detener monitorización de accesos a esta carpeta",
+    "5" => "Monitorizar...",
+    "6" => "Monitorizar cambios en este fichero", // This string seems to be unused
+    "7" => "Detener monitorización",
+    "8" => "Detener monitorización de cambios en este fichero",
+    "9" => "Consulta",
+    "10"=> "Alguien abra el fichero",
+    "10b"=> "Alguien explore la carpeta",
+    "11"=> "Modificación",
+    "12"=> "Alguien modifique el fichero",
+    "12b"=> "Alguien modifique un fichero dentro de esta carpeta",
+    "13"=> "Ambos",
+    "14"=> "Alguien abra o escriba en el fichero",
+    "14b"=> "Alguien explore o modifique el contenido de la carpeta",
+);
