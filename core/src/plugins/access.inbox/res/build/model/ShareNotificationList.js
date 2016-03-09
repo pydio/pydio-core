7097ff8518151758126835647db856942257915d
Greg Hecquet <gregory@pydio.com>
2016-03-09 11:17:27 +0100

    Adding hook to Repository loaded change

--- core/src/plugins/access.inbox/res/build/model/ShareNotificationList.js
+++ core/src/plugins/access.inbox/res/build/model/ShareNotificationList.js
@@ -28,7 +28,11 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
             this._data = { shares: [] };
             this._pydio = pydio;
             this.options = options || {};
+
             this.load();
+            this._pydio.observe("registry_part_loaded", (function () {
+                this.load();
+            }).bind(this));
         }
 
         // Globals
