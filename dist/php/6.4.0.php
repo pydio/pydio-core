28ce1c3d53134cad39d6c9da3d92e32f3de8f8dc
cdujeu <charles@ajaxplorer.info>
2016-03-28 16:39:55 +0200

    Create 6.4.0 script

---
+++ dist/php/6.4.0.php
@@ -0,0 +1,12 @@
+<?php
+
+// FORCE bootstrap_repositories copy
+if (is_file(AJXP_INSTALL_PATH."/conf/bootstrap_repositories.php".".new-".date("Ymd"))) {
+    rename(AJXP_INSTALL_PATH."/conf/bootstrap_repositories.php", AJXP_INSTALL_PATH."/conf/bootstrap_repositories.php.pre-update");
+    rename(AJXP_INSTALL_PATH."/conf/bootstrap_repositories.php".".new-".date("Ymd"), AJXP_INSTALL_PATH."/conf/bootstrap_repositories.php");
+}
+
+// Add new repo to root role
+$rootRole = AuthService::getRole("AJXP_GRP_/");
+$rootRole->setAcl("inbox", "rw");
+AuthService::updateRole($rootRole);
