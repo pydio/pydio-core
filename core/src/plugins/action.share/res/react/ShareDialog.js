7ed8c22fd8b6f9b7295dda9eb581367349c7e5f0
cdujeu <charles@ajaxplorer.info>
2016-04-07 14:19:50 +0200

    Use original_path for header if present in metadata.

--- core/src/plugins/action.share/res/react/ShareDialog.js
+++ core/src/plugins/action.share/res/react/ShareDialog.js
@@ -187,11 +187,16 @@
     var HeaderPanel = React.createClass({
         mixins:[ContextConsumerMixin],
         render: function(){
+
+            let nodePath = this.props.shareModel.getNode().getPath();
+            if(this.props.shareModel.getNode().getMetadata().get("original_path")){
+                nodePath = this.props.shareModel.getNode().getMetadata().get("original_path");
+            }
             return (
                 <div className="headerPanel">
                     <div
                         style={{fontSize: 24, color:'white', padding:'20px 16px 14px', wordBreak:'break-all'}}
-                    >{this.context.getMessage('44').replace('%s', PathUtils.getBasename(this.props.shareModel.getNode().getPath()))}</div>
+                    >{this.context.getMessage('44').replace('%s', PathUtils.getBasename(nodePath))}</div>
                 </div>
             );
         }
