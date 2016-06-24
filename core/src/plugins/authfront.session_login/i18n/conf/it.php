7a602363f269d565e6b39d7f1c4fe956e6fd5edf
DepaMarco <depardimarco@hotmail.it>
2016-06-24 10:03:14 +0200

    Adding Italian translation for plugin 'authfront.session_login'

---
+++ core/src/plugins/authfront.session_login/i18n/conf/it.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"Standard login screen" => "Schermata di login standard",
+"Display a standard user/password login screen" => "Visualizza una schermata standard con username/password",
+"Order" => "Ordine",
+"Order this plugin with other auth frontends" => "Ordina questo plugin rispetto agli altri frontend di autenticazione",
+"Protocol Type" => "Tipo Protocollo",
+"Enable/disable automatically based on the protocol used" => "Abilita/Disabilita automaticamente in base al protocollo in uso",
+"Sessions Only" => "Solo Sessioni",
+"REST Only" => "Solo REST",
+"Session-based or Rest" => "Basato su sessione o Rest",
+);
