14180560beb8c2f7c254e59c3c80acbd1115647f
cdujeu <charles@ajaxplorer.info>
2016-07-06 16:10:22 +0200

    fix FR typo (cherry picked from commit e3b4ab2)

--- core/src/plugins/access.ajxp_home/res/i18n/fr.php
+++ core/src/plugins/access.ajxp_home/res/i18n/fr.php
@@ -81,7 +81,7 @@ $mess=array(
 "67"=> "Vous n'êtes pas %s, %logout.",
 "68"=> "Télécharger l'appli de synchro (Windows)",
 "69"=> "Télécharger l'appli de synchro (Mac)",
-"70"=> "Application pour smarthpones iOS",
-"71"=> "Application pour smarthpones Android",
+"70"=> "Application pour smartphones iOS",
+"71"=> "Application pour smartphones Android",
 "72"=> "Chercher dans tous les dépôts...",
 );
