422b9446dc199478f727d92399534397bf57d1f9
Frederico Passos <fred.passos@gmail.com>
2016-09-08 17:28:54 -0300

    core.mailer translate

---
+++ core/src/plugins/core.mailer/i18n/pt-br.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* Copyright 2007-2012 Charles du Jeu <charles (at) cdujeu.me>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess = array(
+"1" => "E-mail enviado com sucesso para %s usuário(s)",
+"2" => "Nenhum endereço encontrado para enviar o e-mail!",
+"3" => "Nenhum serviço de e-mail configurado para enviar e-mails, favor contatar o administrador",
+"4" => "De",
+"5" => "Para",
+"6" => "Assunto",
+"7" => "Mensagem",
+"8" => "Escolha um usuário ou digite um endereço",
+"9" => "Ingestão de Notificações",
+);
