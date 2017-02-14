77f0aa4249666d7d3612be654b24ac466f52a247
leobouilloux <leo,bouilloux@gmail.com>
2017-02-14 16:56:23 +0100

    Added Italian translations for core.notification

---
+++ core/src/plugins/core.notifications/templates/short/it.php
@@ -0,0 +1,42 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <https://pydio.com>.
+*/
+$mess=array(
+    "file.add" => "File AJXP_NODE_LABEL creato in AJXP_PARENT_PATH",
+    "file.delete" => "File AJXP_NODE_LABEL cancellato da AJXP_PARENT_PATH",
+    "file.change" => "File AJXP_NODE_LABEL modificato da AJXP_USER",
+    "file.view" => "File AJXP_NODE_LABEL consultato da AJXP_USER",
+    "file.copy_to" => "AJXP_NODE_LABEL copiato in AJXP_TARGET_FOLDER",
+    "file.copy_from" => "AJXP_NODE_LABEL copiato da AJXP_SOURCE_FOLDER",
+    "file.move_to" => "AJXP_NODE_LABEL spostato in AJXP_TARGET_FOLDER",
+    "file.move_from" => "AJXP_NODE_LABEL spostato da AJXP_SOURCE_FOLDER",
+    "folder.add" => "Cartella AJXP_NODE_LABEL creata in AJXP_PARENT_PATH",
+    "folder.delete" => "Cartella AJXP_NODE_LABEL cancellata da AJXP_PARENT_PATH",
+    "folder.change" => "Contenuto della cartella AJXP_NODE_LABEL aggiornato da AJXP_USER",
+    "folder.view" => "Cartella AJXP_NODE_LABEL aperta da AJXP_USER",
+    "folder.copy_to" => "Cartella AJXP_NODE_LABEL copiata in AJXP_TARGET_FOLDER",
+    "folder.copy_from" => "Cartella AJXP_NODE_LABEL copiata da AJXP_SOURCE_FOLDER",
+    "folder.move_to" => "Cartella AJXP_NODE_LABEL spostata in AJXP_TARGET_FOLDER",
+    "folder.move_from" => "Cartella AJXP_NODE_LABEL spostata da AJXP_SOURCE_FOLDER",
+    "file.rename" => "AJXP_SOURCE_LABEL rinominata in AJXP_NODE_LABEL",
+    "folder.rename" => "AJXP_SOURCE_LABEL &egrave; stato rinominato in AJXP_NODE_LABEL",
+    "share.file" => "L'utente AJXP_USER ha condiviso il file AJXP_NODE_LABEL con te",
+    "share.folder" => "L'utente AJXP_USER ha condiviso la cartella AJXP_NODE_LABEL con te",
+);
