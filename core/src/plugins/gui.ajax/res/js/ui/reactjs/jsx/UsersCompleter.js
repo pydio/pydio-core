279ec881a891fb23f7021b4b7073069e8293a854
cdujeu <charles@ajaxplorer.info>
2017-03-06 19:57:31 +0100

    New UsersCompleter based on MaterialUI.AutoCompleter

--- core/src/plugins/gui.ajax/res/js/ui/reactjs/jsx/UsersCompleter.js
+++ core/src/plugins/gui.ajax/res/js/ui/reactjs/jsx/UsersCompleter.js
@@ -19,164 +19,12 @@
  */
 (function(global) {
 
-    var UsersLoader = React.createClass({
-
-        propTypes:{
-            renderSuggestion:React.PropTypes.func.isRequired,
-            onValueSelected:React.PropTypes.func.isRequired,
-            fieldLabel:React.PropTypes.string.isRequired,
-            excludes:React.PropTypes.array.isRequired,
-            usersOnly:React.PropTypes.bool,
-            existingOnly:React.PropTypes.bool,
-            freeValueAllowed:React.PropTypes.bool,
-            className:React.PropTypes.string
-        },
-
-        getInitialState:function(){
-            return {
-                createUser: null,
-                showComplete: true,
-                loading:0,
-                minChars:parseInt(global.pydio.getPluginConfigs("core.conf").get("USERS_LIST_COMPLETE_MIN_CHARS"))
-            };
-        },
-
-        suggestionLoader:function(input, callback){
-            var excludes = this.props.excludes;
-            var disallowTemporary = this.props.existingOnly && !this.props.freeValueAllowed;
-            this.setState({loading:this.state.loading + 1});
-            PydioUsers.Client.authorizedUsersStartingWith(input, function(users){
-                this.setState({loading:this.state.loading - 1});
-                if(disallowTemporary){
-                    users = users.filter(function(user){
-                        return !user.getTemporary();
-                    });
-                }
-                if(excludes && excludes.length){
-                    users = users.filter(function(user){
-                        return excludes.indexOf(user.getId()) == -1;
-                    });
-                }
-                // Hack : force suggestions display
-                if(this.refs.autosuggest.lastSuggestionsInputValue && this.refs.autosuggest.lastSuggestionsInputValue.indexOf(input) === 0){
-                    this.refs.autosuggest.lastSuggestionsInputValue = input;
-                }
-                callback(null, users);
-            }.bind(this), this.props.usersOnly, this.props.existingOnly);
-        },
-
-        getSuggestions(input, callback){
-            var minChars = this.state.minChars;
-            if(!this.state.showComplete || input.length < minChars){
-                callback(null, []);
-                return;
-            }
-            FuncUtils.bufferCallback('suggestion-loader-users', 400, function(){
-                this.suggestionLoader(input, callback);
-            }.bind(this));
-        },
-
-        suggestionValue: function(suggestion){
-            return '';
-        },
-
-        onSuggestionSelected: function(userObject, event){
-            if(!userObject) return;
-            var suggestion = userObject.asObject();
-            var blur = true;
-            if(suggestion.group){
-                this.props.onValueSelected(suggestion.group, suggestion.label, 'group', userObject);
-            }else if(suggestion.id) {
-                this.props.onValueSelected(suggestion.id, suggestion.label, suggestion.external?'tmp_user':'user', userObject);
-            }else if(suggestion.temporary){
-                this.setState({createUser:suggestion.label});
-                blur = false;
-            }
-            if(blur){
-                this.setState({showComplete: false}, function(){
-                    global.setTimeout(function(){
-                        this.refs['autosuggest'].refs['input'].blur();
-                    }.bind(this), 10);
-                });
-            }
-        },
-
-        submitCreationForm: function(){
-
-            var prefix = PydioUsers.Client.getCreateUserPostPrefix();
-            var values = this.refs['creationForm'].getValuesForPost(prefix);
-            PydioUsers.Client.createUserFromPost(values, function(values, jsonReponse){
-                let id;
-                if(jsonReponse['createdUserId']){
-                    id = jsonReponse['createdUserId'];
-                }else{
-                    id = values[prefix + 'new_user_id'];
-                }
-                var display = values[prefix + 'USER_DISPLAY_NAME'] || id;
-                var fakeUser = new PydioUsers.User(id, display, 'user');
-                this.props.onValueSelected(id, display, 'user', fakeUser);
-                this.setState({createUser:null});
-            }.bind(this));
-
-        },
-
-        cancelCreationForm:function(){
-            this.setState({createUser:null});
-        },
-
-        render: function(){
-
-            if(this.state.createUser){
-
-                return (
-                    <div className={this.props.className}>
-                    <div className={"react-autosuggest"}>
-                        <input type="text" id="users-autosuggest" className="react-autosuggest__input" value={'Create User ' + this.state.createUser}/>
-                        <div className="react-autosuggest__suggestions">
-                            <UserCreationForm ref="creationForm" newUserName={this.state.createUser} />
-                            <div style={{padding:16, textAlign:'right', paddingTop:0}}>
-                                <ReactMUI.FlatButton label="Save & add" secondary={true} onClick={this.submitCreationForm} />
-                                <ReactMUI.FlatButton label="Cancel" onClick={this.cancelCreationForm} />
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                );
-
-            }else{
-                const inputAttributes = {
-                    id: 'users-autosuggest',
-                    name: 'users-autosuggest',
-                    className: 'react-autosuggest__input',
-                    placeholder: this.props.fieldLabel,
-                    onBlur: event => this.setState({showComplete:false}),
-                    onFocus: event => this.setState({showComplete:true}),
-                    value: ''   // Initial value
-                };
-                return (
-                    <div style={{position:'relative'}} className={this.props.className + (this.state.showComplete ? '':' list-force-hide')}>
-                        <span className={"suggest-search icon-" + (this.state.loading && this.state.showComplete ? 'refresh rotating' : 'search')}/>
-                        <ReactAutoSuggest
-                            ref="autosuggest"
-                            cache={false}
-                            showWhen = {input => true }
-                            inputProps={inputAttributes}
-                            suggestions={this.getSuggestions}
-                            getSuggestionValue={this.suggestionValue}
-                            renderSuggestion={this.props.renderSuggestion}
-                            onSuggestionSelected={this.onSuggestionSelected}
-                        />
-                    </div>
-                );
-            }
-        }
-
-    });
-
     var UserCreationForm = React.createClass({
 
         propTypes:{
-            newUserName:React.PropTypes.string.isRequired
+            newUserName:React.PropTypes.string.isRequired,
+            submitCreationForm: React.PropTypes.func.isRequired,
+            cancelCreationForm: React.PropTypes.func.isRequired
         },
 
         getParameters: function(){
@@ -208,16 +56,201 @@
         },
 
         render:function(){
-            return <PydioForm.FormPanel
-                className="reset-pydio-forms"
-                depth={-1}
-                parameters={this.getParameters()}
-                values={this.state.values}
-                onChange={this.onValuesChange}
-            />
+            return (
+                <MaterialUI.Paper zDepth={2} style={{height: 250, overflowY: 'auto'}}>
+                    <PydioForm.FormPanel
+                        className="reset-pydio-forms"
+                        depth={-1}
+                        parameters={this.getParameters()}
+                        values={this.state.values}
+                        onChange={this.onValuesChange}
+                    />
+                    <div style={{padding:16, textAlign:'right', paddingTop:0}}>
+                        <MaterialUI.FlatButton label={global.pydio.MessageHash[484]} secondary={true} onClick={this.props.submitCreationForm} />
+                        <MaterialUI.FlatButton label={global.pydio.MessageHash[49]} onClick={this.props.cancelCreationForm} />
+                    </div>
+                </MaterialUI.Paper>
+            )
         }
     });
 
+    var UsersLoader = React.createClass({
+
+        propTypes:{
+
+            renderSuggestion:React.PropTypes.func.isRequired,
+            onValueSelected :React.PropTypes.func.isRequired,
+            fieldLabel      :React.PropTypes.string.isRequired,
+            excludes        :React.PropTypes.array.isRequired,
+            usersOnly       :React.PropTypes.bool,
+            existingOnly    :React.PropTypes.bool,
+            freeValueAllowed:React.PropTypes.bool,
+            className       :React.PropTypes.string
+
+        },
+
+        getInitialState:function(){
+            return {
+                dataSource  : [],
+                loading     : false,
+                searchText  : '',
+                minChars    : parseInt(global.pydio.getPluginConfigs("core.conf").get("USERS_LIST_COMPLETE_MIN_CHARS"))
+            };
+        },
+
+        suggestionLoader:function(input, callback){
+
+            var excludes = this.props.excludes;
+            var disallowTemporary = this.props.existingOnly && !this.props.freeValueAllowed;
+            this.setState({loading:this.state.loading + 1});
+            PydioUsers.Client.authorizedUsersStartingWith(input, function(users){
+                this.setState({loading:this.state.loading - 1});
+                console.log(users);
+                if(disallowTemporary){
+                    users = users.filter(function(user){
+                        return !user.getTemporary();
+                    });
+                }
+                if(excludes && excludes.length){
+                    users = users.filter(function(user){
+                        return excludes.indexOf(user.getId()) == -1;
+                    });
+                }
+                callback(users);
+            }.bind(this), this.props.usersOnly, this.props.existingOnly);
+
+        },
+
+
+        textFieldUpdate: function(value){
+
+            this.setState({searchText: value});
+            if(this.state.minChars && value && value.length < this.state.minChars ){
+                return;
+            }
+            this.setState({loading: true});
+            FuncUtils.bufferCallback('remote_users_search', 300, function(){
+                this.suggestionLoader(value, function(users){
+                    let crtValueFound = false;
+                    const values = users.map(function(userObject){
+                        let component = (<MaterialUI.MenuItem>{this.props.renderSuggestion(userObject)}</MaterialUI.MenuItem>);
+                        return {
+                            userObject  : userObject,
+                            text        : userObject.getExtendedLabel(),
+                            value       : component
+                        };
+                    }.bind(this));
+                    this.setState({dataSource: values, loading: false});
+                }.bind(this));
+            }.bind(this));
+        },
+
+        onCompleterRequest: function(value, index){
+
+            console.log(value, index);
+            if(index === -1){
+                this.state.dataSource.map(function(entry){
+                    if(entry.text === value){
+                        value = entry;
+                    }
+                });
+                if(value && !value.userObject && this.props.freeValueAllowed){
+                    this.props.onValueSelected(value, value, '');
+                    return;
+                }
+            }
+            if(value && value.userObject){
+                const object = value.userObject;
+                if(object.getTemporary()){
+                    this.setState({createUser: object.getLabel()});
+                }else{
+                    this.props.onValueSelected(object.getId(), object.getLabel(), object.getType());
+                }
+                this.setState({searchText: '', dataSource:[]});
+            }
+
+        },
+
+        submitCreationForm: function(){
+
+            var prefix = PydioUsers.Client.getCreateUserPostPrefix();
+            var values = this.refs['creationForm'].getValuesForPost(prefix);
+            PydioUsers.Client.createUserFromPost(values, function(values, jsonReponse){
+                let id;
+                if(jsonReponse['createdUserId']){
+                    id = jsonReponse['createdUserId'];
+                }else{
+                    id = values[prefix + 'new_user_id'];
+                }
+                var display = values[prefix + 'USER_DISPLAY_NAME'] || id;
+                var fakeUser = new PydioUsers.User(id, display, 'user');
+                this.props.onValueSelected(id, display, 'user', fakeUser);
+                this.setState({createUser:null});
+            }.bind(this));
+
+        },
+
+        cancelCreationForm:function(){
+            this.setState({createUser:null});
+        },
+
+
+        render: function(){
+
+            const {dataSource} = this.state;
+            const containerStyle = {position:'relative', overflow: 'visible'};
+
+            if(this.state.createUser){
+
+                return (
+                    <div style={containerStyle}>
+                        <MaterialUI.TextField
+                            floatingLabelText={this.props.fieldLabel}
+                            value={global.pydio.MessageHash[485] + ' (' + this.state.createUser + ')'}
+                            disabled={true}
+                            fullWidth={true}
+                        />
+                        <div style={{position: 'absolute', top: 73, left: 0, right: 0, zIndex: 10}}>
+                            <UserCreationForm
+                                ref="creationForm"
+                                newUserName={this.state.createUser}
+                                submitCreationForm={this.submitCreationForm}
+                                cancelCreationForm={this.cancelCreationForm}
+                            />
+                        </div>
+                    </div>
+                );
+
+            }
+
+            return (
+                <div style={containerStyle}>
+                    <MaterialUI.AutoComplete
+                        filter={MaterialUI.AutoComplete.noFilter}
+                        dataSource={dataSource}
+                        searchText={this.state.searchText}
+                        onUpdateInput={this.textFieldUpdate}
+                        className={this.props.className}
+                        openOnFocus={true}
+                        floatingLabelText={this.props.fieldLabel}
+                        fullWidth={true}
+                        onNewRequest={this.onCompleterRequest}
+                        listStyle={{maxHeight: 350, overflowY: 'auto'}}
+                    />
+                    <div style={{position:'absolute', right:4, bottom: 14, height: 20, width: 20}}>
+                        <MaterialUI.RefreshIndicator
+                            size={20}
+                            left={0}
+                            top={0}
+                            status={this.state.loading ? 'loading' : 'hide' }
+                        />
+                    </div>
+                </div>
+            );
+
+        }
+
+    });
 
     var ns = global.UsersCompleter || {};
     ns.Input = UsersLoader;
