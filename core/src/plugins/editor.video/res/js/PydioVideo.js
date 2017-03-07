edead954fde92871b386581b7daf1bf8c9e8fa5d
Greg Hecquet <gregory@pydio.com>
2017-03-07 11:22:34 +0100

    Video editor in react

---
+++ core/src/plugins/editor.video/res/js/PydioVideo.js
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com>.
+ */
+
+import Player from './Video';
+
+class PydioVideo extends React.Component {
+
+    static getPreviewComponent(node, rich = false) {
+        if (rich) {
+            return <Preview node={node} pydio={window.pydio} rich={rich} />
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+        return (
+            <PydioComponents.AbstractEditor {...this.props}>
+                <Preview node={this.props.node} pydio={window.pydio} rich={true} />
+            </PydioComponents.AbstractEditor>
+        );
+    }
+}
+
+class Preview extends React.Component {
+
+    constructor(props) {
+
+        super(props)
+
+        const {pydio, node, rich} = this.props
+
+        let url = null
+
+        this.state = {}
+
+        this.onReady = this._handleReady.bind(this)
+
+        this.getSessionId().then((sessionId) => this.setState({
+            url: pydio.Parameters.get('ajxpServerAccess') + '&ajxp_sessid=' + sessionId + '&get_action=read_video_data&file=' + encodeURIComponent(node.getPath())
+        }));
+    }
+
+    getSessionId() {
+        const {pydio, node} = this.props
+
+        return new Promise((resolve, reject) => {
+            pydio.ApiClient.request({
+                get_action: 'get_sess_id'
+            }, function(transport) {
+                resolve(transport.responseText)
+            })
+        });
+    }
+
+    _handleReady() {
+        this.setState({
+            ready: true
+        })
+    }
+
+    render() {
+        if (!this.state.url) {
+            return null
+        }
+
+        let options = {
+            preload: 'auto',
+            autoplay: false,
+            controls: true,
+            flash: {
+                swf: "plugins/editor.video/node_modules/video.js/dist/video-js.swf"
+            },
+            techOrder: ['flash', 'html5']
+        }
+
+        let loader = null;
+        let invisibleStyle = null;
+        if (!this.state.ready) {
+            loader = <PydioReactUI.Loader style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, zIndex: 0}}/>
+            invisibleStyle = {
+                position: "relative",
+                zIndex: "-1"
+            }
+        }
+
+        return (
+            <div style={{position: "relative", padding: 0, margin: 0}}>
+                {loader}
+                <div style={invisibleStyle}>
+                    <Player options={options} src={this.state.url} resize={true} onReady={this.onReady}></Player>
+                </div>
+            </div>
+        )
+    }
+}
+
+function guid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}
+
+// We need to attach the element to window else it won't be found
+window.PydioVideo = PydioVideo
+
+export default PydioVideo
