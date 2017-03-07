c9a5401f2b6518a289f56b961a851fa9d3a2be23
cdujeu <charles@ajaxplorer.info>
2017-03-07 12:14:12 +0100

    GenericDir theme

--- core/src/plugins/gui.ajax/res/js/ui/reactjs/jsx/Workspaces/detailpanes/GenericDir.js
+++ core/src/plugins/gui.ajax/res/js/ui/reactjs/jsx/Workspaces/detailpanes/GenericDir.js
@@ -1,14 +1,24 @@
 import InfoPanelCard from './InfoPanelCard'
 
-export default React.createClass({
+let GenericDir = React.createClass({
 
     render: function(){
+
+        const themePalette = this.props.muiTheme.palette;
+        const tBarStyle = {
+            backgroundColor: themePalette.primary1Color
+        };
+
         return (
             <InfoPanelCard>
                 <div className="mimefont-container"><div className={"mimefont mdi mdi-" + this.props.node.getMetadata().get('fonticon')}></div></div>
-                <PydioMenus.Toolbar className="primaryToolbar" renderingType="button-icon" toolbars={["info_panel", "info_panel_share"]} controller={this.props.pydio.getController()}/>
+                <PydioMenus.Toolbar toolbarStyle={tBarStyle} className="primaryToolbar" renderingType="button-icon" toolbars={["info_panel", "info_panel_share"]} controller={this.props.pydio.getController()}/>
             </InfoPanelCard>
         );
     }
 
 });
+
+GenericDir = MaterialUI.Style.muiThemeable()(GenericDir);
+
+export {GenericDir as default}
