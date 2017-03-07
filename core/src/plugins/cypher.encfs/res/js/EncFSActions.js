285ee09b8e8ebcf0b66d6721f4a66116e9027697
cdujeu <charles@ajaxplorer.info>
2017-03-07 09:36:26 +0100

    Rewrite EncFS

--- core/src/plugins/cypher.encfs/res/js/EncFSActions.js
+++ core/src/plugins/cypher.encfs/res/js/EncFSActions.js
@@ -1,75 +1,129 @@
 (function(global){
 
-    let pydio = global.pydio;
+    const pydio = global.pydio;
+    const MessageHash = pydio.MessageHash;
 
     class Callbacks{
 
         static createCypheredFolder(){
 
-            modal.showDialogForm('', 'encfs_cypher', null, function(oForm){
 
-                var passValue = $(oForm).down('input[name="password"]').getValue();
-                var passValue2 = $(oForm).down('input[name="password_confirm"]').getValue();
-                if(passValue != passValue2){
-                    pydio.displayMessage('ERROR', 'Warning, both passwords differ');
-                    return;
-                }
-                var conn = new Connexion();
-                conn.setParameters($H({
+            const submitCallback = function(passValue){
+
+                PydioApi.getClient().request({
                     get_action:'encfs.cypher_folder',
                     file:pydio.getUserSelection().getUniqueNode().getPath(),
                     pass: passValue
-                }));
-                conn.onComplete = function(transport){
+                }, function(transport){
                     PydioApi.getClient().parseXmlMessage(transport.responseXML);
-                };
-                conn.sendAsync();
-                hideLightBox();
+                });
 
-            })
+            };
+
+            pydio.UI.openComponentInModal('EncFSActions', 'CypherDialog', {
+                submitValue: submitCallback
+            });
 
         }
 
         static cypherFolder(){
 
-            var conn = new Connexion();
-            conn.setParameters($H({
+            PydioApi.getClient().request({
                 get_action:'encfs.cypher_folder',
                 file:pydio.getUserSelection().getUniqueNode().getPath()
-            }));
-            conn.onComplete = function(transport){
+            }, function(transport){
                 PydioApi.getClient().parseXmlMessage(transport.responseXML);
-            };
-            conn.sendAsync();
-            hideLightBox();
+            });
 
         }
 
         static uncypherFolder(){
 
-            modal.showDialogForm('', 'encfs_mount', null, function(oForm){
+            const submitCallback = function(passValue){
 
-                var passValue = $(oForm).down('input[name="password"]').getValue();
-                var conn = new Connexion();
-                conn.setParameters($H({
+                PydioApi.getClient().request({
                     get_action:'encfs.uncypher_folder',
                     file:pydio.getUserSelection().getUniqueNode().getPath(),
                     pass: passValue
-                }));
-                conn.onComplete = function(transport){
+                }, function(transport){
                     PydioApi.getClient().parseXmlMessage(transport.responseXML);
-                };
-                conn.sendAsync();
-                hideLightBox();
+                });
 
+            };
 
+            pydio.UI.openComponentInModal('PydioReactUI', 'PromptDialog', {
+                submitValue: submitCallback,
+                dialogTitleId: 'encfs.3',
+                legendId: 'encfs.7',
+                fieldLabelId: 'encfs.8',
+                fieldType: 'password'
             });
         }
 
     }
 
+    const CypherDialog = React.createClass({
+
+        propTypes: {
+            dialogTitleId:React.PropTypes.integer,
+            legendId:React.PropTypes.integer,
+            fieldLabelId:React.PropTypes.integer,
+            fieldType: React.PropTypes.oneOf(['text', 'password']),
+            submitValue:React.PropTypes.func.isRequired,
+            defaultValue:React.PropTypes.string,
+            defaultInputSelection:React.PropTypes.string
+        },
+
+        mixins:[
+            PydioReactUI.ActionDialogMixin,
+            PydioReactUI.CancelButtonProviderMixin,
+            PydioReactUI.SubmitButtonProviderMixin
+        ],
+
+        getDefaultProps: function(){
+            return {
+                dialogTitle: MessageHash['encfs.2'],
+                dialogIsModal: true
+            };
+        },
+        submit(){
+            const in1 = this.refs.input1.getValue();
+            const in2 = this.refs.input2.getValue();
+            if(in1 === in2){
+                this.props.submitValue(in1);
+                this.dismiss();
+            }else{
+                this.setState({error: MessageHash[238]});
+            }
+        },
+        render: function(){
+            const error = this.state && this.state.error ? (<div>{this.state.error}</div>) : null;
+            return (
+                <div>
+                    <div className="dialogLegend">{MessageHash['encfs.10']}</div>
+                    <MaterialUI.TextField
+                        floatingLabelText={MessageHash['encfs.8']}
+                        ref="input1"
+                        onKeyDown={this.submitOnEnterKey}
+                        type="password"
+                    />
+                    <MaterialUI.TextField
+                        floatingLabelText={MessageHash['encfs.9']}
+                        ref="input2"
+                        onKeyDown={this.submitOnEnterKey}
+                        type="password"
+                    />
+                    {error}
+                </div>
+            );
+        }
+
+    });
+
+
     global.EncFSActions = {
-        Callbacks: Callbacks
+        Callbacks: Callbacks,
+        CypherDialog: CypherDialog
     };
 
 })(window)
