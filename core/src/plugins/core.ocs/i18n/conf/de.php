8f28ea31e1b488e8ce53c356fccc8ead4f1bdc12
cdujeu <charles@ajaxplorer.info>
2017-03-11 13:43:47 +0100

    Trusted Servers implementation by simply forwarding user_list_authorized_users to the remote server with valid credentials.

--- core/src/plugins/core.ocs/i18n/conf/de.php
+++ core/src/plugins/core.ocs/i18n/conf/de.php
@@ -4,4 +4,13 @@ $mess = array(
     "Open Cloud API Implementation" => "Implementierung freier Cloud-Plattformen",
     "Enable Federated Sharing" => "Zugriff per Federated Cloud Sharing aktivieren",
     "Allow users to send / receive shares from other Pydio instances or from other Federated-Sharing-compatible systems" => "Dateien mit anderen Systemen austauschen, die Federated Cloud Sharing unterstützen.",
+    "Trusted Servers" => "Trusted Servers",
+    "Server ID" => "Server ID",
+    "Label" => "Label",
+    "Unique ID (use alphanumeric characters) identifying server" => "Unique ID (use alphanumeric characters) identifying server",
+    "User-friendly label to display the servers list" => "User-friendly label to display the servers list",
+    "Server URL" => "Server URL",
+    "Server URL including http/https protocol" => "Server URL including http/https protocol",
+    "User name used to connect to remote server. Must be created on the remote server first" => "User name used to connect to remote server. Must be created on the remote server first",
+    "Password used to connect to remote server." => "Password used to connect to remote server."
 );
--- core/src/plugins/core.ocs/i18n/conf/en.php
+++ core/src/plugins/core.ocs/i18n/conf/en.php
@@ -3,5 +3,14 @@ $mess = array(
     "Open Cloud API" => "Open Cloud Specification",
     "Open Cloud API Implementation" => "Open Cloud API Implementation",
     "Enable Federated Sharing" => "Enable Federated Sharing",
-    "Allow users to send / receive shares from other Pydio instances or from other Federated-Sharing-compatible systems" => "Allow users to send / receive shares from other Pydio instances or from other Federated-Sharing-compatible systems"
+    "Allow users to send / receive shares from other Pydio instances or from other Federated-Sharing-compatible systems" => "Allow users to send / receive shares from other Pydio instances or from other Federated-Sharing-compatible systems",
+    "Trusted Servers" => "Trusted Servers",
+    "Server ID" => "Server ID",
+    "Label" => "Label",
+    "Unique ID (use alphanumeric characters) identifying server" => "Unique ID (use alphanumeric characters) identifying server",
+    "User-friendly label to display the servers list" => "User-friendly label to display the servers list",
+    "Server URL" => "Server URL",
+    "Server URL including http/https protocol" => "Server URL including http/https protocol",
+    "User name used to connect to remote server. Must be created on the remote server first" => "User name used to connect to remote server. Must be created on the remote server first",
+    "Password used to connect to remote server." => "Password used to connect to remote server."
 );
