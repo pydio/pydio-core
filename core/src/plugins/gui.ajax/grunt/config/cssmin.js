185fbc5347f009dfd6678f86aa0f192b5b81e42e
cdujeu <charles@ajaxplorer.info>
2017-04-05 10:55:38 +0200

    Unused css resources

--- core/src/plugins/gui.ajax/grunt/config/cssmin.js
+++ core/src/plugins/gui.ajax/grunt/config/cssmin.js
@@ -5,12 +5,7 @@ module.exports = {
     },
     target: {
         files: {
-            'res/themes/material/css/allz.css': [
-                'res/themes/material/css/pydio.css',
-                'res/themes/common/css/animate-custom.css',
-                'res/themes/common/css/chosen.css',
-                'res/themes/common/css/media.css'
-            ]
+            'res/themes/material/css/allz.css': ['res/themes/material/css/pydio.css']
         }
     }
 };
