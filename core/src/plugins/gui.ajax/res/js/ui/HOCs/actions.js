34df10f779824788542aafe9320b2f6f7d57debe
Greg Hecquet <gregory@pydio.com>
2017-04-12 17:54:17 +0200

    Fixing exif

--- core/src/plugins/gui.ajax/res/js/ui/HOCs/actions.js
+++ core/src/plugins/gui.ajax/res/js/ui/HOCs/actions.js
@@ -37,7 +37,7 @@ const withActions = (Component) => {
                 <div style={{display: "flex", flexDirection: "column", flex: 1}}>
                     <Toolbar elements={actions} />
 
-                    <Component {...remainingProps} style={{flex: 1}} />
+                    <Component {...remainingProps} />
                 </div>
             )
         }
--- core/src/plugins/meta.exif/res/js/PydioExif.js
+++ core/src/plugins/meta.exif/res/js/PydioExif.js
@@ -1,221 +1,203 @@
-(function(global){
+import React, {Component} from 'react'
 
-    let DetailPanel = React.createClass({
+import {ToolbarGroup, FlatButton, Card, CardTitle, CardText, Table, TableBody, TableRow, TableRowColumn} from 'material-ui'
 
-        parseValues: function(node){
+class DetailPanel extends Component {
 
-            let configs = this.props.pydio.getPluginConfigs('meta.exif');
-            if(!configs.has('meta_definitions')){
-                return;
-            }
-            let nodeMeta = node.getMetadata();
-            let definitions = configs.get('meta_definitions');
-            let items = [];
-            let gpsData = {'COMPUTED_GPS-GPS_Latitude':null, 'COMPUTED_GPS-GPS_Longitude':null};
-            for(let key in definitions){
-                if(!definitions.hasOwnProperty(key)) continue;
-                if(nodeMeta.has(key)){
-                    let value = nodeMeta.get(key);
-                    if(gpsData[key] !== undefined){
-                        gpsData[key] = nodeMeta.get(key);
-                        value = value.split('--').shift();
-                    }
-                    items.push({
-                        key: key,
-                        label:definitions[key],
-                        value:value
-                    });
-                }
-            }
-            if(gpsData['COMPUTED_GPS-GPS_Longitude'] && gpsData['COMPUTED_GPS-GPS_Latitude']){
-                // Special Case
-                ResourcesManager.loadClassesAndApply(['OpenLayers', 'PydioMaps'], function(){
-                    this.setState({gpsData:gpsData});
-                }.bind(this));
-            }
-            this.setState({items:items});
-        },
-
-        componentDidMount: function(){
-            this.parseValues(this.props.node);
-        },
-
-        componentWillReceiveProps: function(nextProps){
-            if(nextProps.node !== this.props.node){
-                this.setState({gpsData:null});
-                this.parseValues(nextProps.node);
-            }
-        },
-
-        mapLoaded: function(map, error){
-            if(error && console) console.log(error);
-        },
-
-        openInExifEditor: function(){
-            const editor = global.pydio.Registry.findEditorById("editor.exif");
-            if(editor){
-                global.pydio.UI.openCurrentSelectionInEditor(editor, this.props.node);
-            }
-        },
-
-        openInMapEditor: function(){
-            const editors = global.pydio.Registry.findEditorsForMime("ol_layer");
-            if(editors.length){
-                global.pydio.UI.openCurrentSelectionInEditor(editors[0], this.props.node);
-            }
-        },
-
-        render: function(){
-
-            let items = [];
-            let actions = [];
-            if(this.state && this.state.items){
-
-                const fields = this.state.items.map(function(object){
-                    return (
-                        <div key={object.key} className="infoPanelRow" style={{float:'left', width: '50%', padding: '0 4px 12px', whiteSpace:'nowrap'}}>
-                            <div className="infoPanelLabel">{object.label}</div>
-                            <div className="infoPanelValue">{object.value}</div>
-                        </div>
-                    )
-                });
-                items.push(<div style={{padding: '0 12px'}}>{fields}</div>)
-                items.push(<div style={{clear:'left'}}></div>)
-
-                actions.push(
-                    <MaterialUI.FlatButton onClick={this.openInExifEditor} label="More Exif" />
-                );
-            }
-            if(this.state && this.state.gpsData){
-                items.push(
-                    <PydioReactUI.AsyncComponent
-                        namespace="PydioMaps"
-                        componentName="OLMap"
-                        key="map"
-                        style={{height: 170,marginBottom:0, padding:0}}
-                        centerNode={this.props.node}
-                        mapLoaded={this.mapLoaded}
-                    />
-                );
-                actions.push(
-                    <MaterialUI.FlatButton onClick={this.openInMapEditor} label="Open Map" />
-                )
-            }
-
-            if(!items.length){
-                return null;
-            }
-            return (
-                <PydioWorkspaces.InfoPanelCard style={this.props.style} title="Exif Data" actions={actions} icon="camera" iconColor="#607d8b">
-                    {items}
-                </PydioWorkspaces.InfoPanelCard>
-            );
-
+    parseValues(node){
+        const configs = this.props.pydio.getPluginConfigs('meta.exif');
+        if(!configs.has('meta_definitions')){
+            return;
         }
 
-    });
+        const nodeMeta = node.getMetadata();
+        const definitions = configs.get('meta_definitions');
 
-    let Editor = React.createClass({
+        let items = Object.keys(definitions)
+            .filter(key => nodeMeta.has(key))
+            .map(key => ({key, label: definitions[key], value: nodeMeta.get(key).split('--').shift()}))
 
-        componentDidMount: function(){
-            this.loadData();
-        },
+        let gpsData = ["COMPUTED_GPS-GPS_Latitude", "COMPUTED_GPS-GPS_Longitude"]
+            .filter(key => nodeMeta.has(key))
+            .map((key) => ({key, value: nodeMeta.get(key)}))
+            .reduce((obj, cur) => ({...obj, [cur.key]: cur.value }), {});
 
-        getInitialState:function(){
-            return {loaded: false};
-        },
+        if(gpsData['COMPUTED_GPS-GPS_Longitude'] && gpsData['COMPUTED_GPS-GPS_Latitude']){
+            // Special Case
+            ResourcesManager.loadClassesAndApply(['OpenLayers', 'PydioMaps'], () => this.setState({gpsData}));
+        }
 
-        loadData:function(){
-            PydioApi.getClient().request({
-                get_action:'extract_exif',
-                file:this.props.node.getPath(),
-                format:'json'
-            }, function(transp){
-                if(!transp.responseJSON){
-                    this.setState({loaded: true, error: 'Could not load JSON'});
-                }else{
-                    let gpsData;
-                    if(transp.responseJSON['COMPUTED_GPS']){
-                        let {GPS_Latitude, GPS_Longitude} = transp.responseJSON['COMPUTED_GPS'];
-                        gpsData = {
-                            latitude:GPS_Latitude.split('--').pop(),
-                            longitude:GPS_Longitude.split('--').pop()
-                        };
-                    }
-                    this.setState({loaded: true, data: transp.responseJSON, gpsData:gpsData});
-                }
-            }.bind(this), function(){
-                this.setState({loaded: true, error: 'Could not load data'});
-            });
-        },
+        this.setState({items});
+    }
 
-        openGpsLocator: function(){
-            const editors = global.pydio.Registry.findEditorsForMime("ol_layer");
-            if(editors.length){
-                global.pydio.UI.openCurrentSelectionInEditor(editors[0], this.props.node);
-            }
-        },
+    componentDidMount() {
+        this.parseValues(this.props.node);
+    }
 
-        render:function(){
+    componentWillReceiveProps(nextProps){
+        if(nextProps.node !== this.props.node){
+            this.setState({gpsData:null});
+            this.parseValues(nextProps.node);
+        }
+    }
 
-            let content, actions=null, error;
-            if(this.state.loaded && !this.state.error) {
+    mapLoaded(map, error){
+        if (error && console) console.log(error);
+    }
 
-                const data = this.state.data;
-                let sectionItems = [];
-                for(let section in data){
-                    if(!data.hasOwnProperty(section)) continue;
-                    let items = [];
-                    items.push(<MaterialUI.Subheader key={section+'-head'}>{section}</MaterialUI.Subheader>);
-                    for(let keyName in data[section]){
-                        if(!data[section].hasOwnProperty(keyName)) continue;
-                        let label = <span><span style={{fontWeight:500}}>{keyName}</span> : {data[section][keyName]}</span>;
-                        items.push(<MaterialUI.ListItem key={section + '-' +keyName} primaryText={label}/>);
-                    }
-                    sectionItems.push(<MaterialUI.List key={section}>{items}</MaterialUI.List>);
-                    sectionItems.push(<MaterialUI.Divider/>);
-                }
-                content = (
-                    <div className="vertical_fit" style={{overflowY:'auto'}}>
-                        {sectionItems}
+    openInExifEditor() {
+        const {pydio, node} = this.props
+
+        const editor = pydio.Registry.findEditorById("editor.exif");
+        if (editor) {
+            pydio.UI.openCurrentSelectionInEditor(editor, node);
+        }
+    }
+
+    openInMapEditor() {
+        const {pydio, node} = this.props
+
+        const editors = pydio.Registry.findEditorsForMime("ol_layer");
+        if (editors.length) {
+            pydio.UI.openCurrentSelectionInEditor(editors[0], node);
+        }
+    }
+
+    render(){
+
+        let items = [];
+        let actions = [];
+        if (this.state && this.state.items) {
+
+            const fields = this.state.items.map(function(object){
+                return (
+                    <div key={object.key} className="infoPanelRow" style={{float:'left', width: '50%', padding: '0 4px 12px', whiteSpace:'nowrap'}}>
+                        <div className="infoPanelLabel">{object.label}</div>
+                        <div className="infoPanelValue">{object.value}</div>
                     </div>
-                );
+                )
+            });
+            items.push(<div style={{padding: '0 12px'}}>{fields}</div>)
+            items.push(<div style={{clear:'left'}}></div>)
 
-            }else if(this.state.loaded && this.state.error){
-
-                error = <div>{this.state.error}</div>
-
-            }else{
-
-                content = <PydioReactUI.Loader/>;
-
-            }
-
-            if(this.state.gpsData){
-                actions = [
-                    <MaterialUI.ToolbarGroup firstChild={true}>
-                        <MaterialUI.FlatButton label={"Locate on a map"} onClick={this.openGpsLocator}/>
-                    </MaterialUI.ToolbarGroup>
-                ];
-            }
-
-            return (
-                <PydioComponents.AbstractEditor
-                    {...this.props}
-                    actions={actions}
-                    errorString={error}>
-                    {content}
-                </PydioComponents.AbstractEditor>
+            actions.push(
+                <MaterialUI.FlatButton onClick={() => this.openInExifEditor()} label="More Exif" />
             );
-
+        }
+        if (this.state && this.state.gpsData) {
+            items.push(
+                <PydioReactUI.AsyncComponent
+                    namespace="PydioMaps"
+                    componentName="OLMap"
+                    key="map"
+                    style={{height: 170,marginBottom:0, padding:0}}
+                    centerNode={this.props.node}
+                    mapLoaded={this.mapLoaded}
+                />
+            );
+            actions.push(
+                <MaterialUI.FlatButton onClick={() => this.openInMapEditor()} label="Open Map" />
+            )
         }
 
-    });
+        if (!items.length) {
+            return null;
+        }
+        return (
+            <PydioWorkspaces.InfoPanelCard style={this.props.style} title="Exif Data" actions={actions} icon="camera" iconColor="#607d8b">
+                {items}
+            </PydioWorkspaces.InfoPanelCard>
+        );
 
+    }
+}
 
-    window.PydioExif = {
-        Editor: Editor,
-        DetailPanel:DetailPanel
-    };
+class Editor extends Component {
 
-})(window);
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: [],
+            error: ""
+        }
+    }
+
+    componentDidMount() {
+        const callback = (object) => {
+            this.setState(object)
+            typeof this.props.onLoad === 'function' && this.props.onLoad()
+        }
+
+        PydioApi.getClient().request({
+            get_action:'extract_exif',
+            file:this.props.node.getPath(),
+            format:'json'
+        },
+        ({responseJSON}) => responseJSON ? callback({data: responseJSON}) : callback({error: 'Could not load JSON'}),
+        () => callback({error: 'Could not load data'}));
+    }
+
+    openGpsLocator() {
+        const {pydio, node} = this.props
+
+        const editors = pydio.Registry.findEditorsForMime("ol_layer");
+        if (editors.length) {
+            pydio.UI.openCurrentSelectionInEditor(editors[0], this.props.node);
+        }
+    }
+
+    render() {
+        let content;
+
+        const {data, error} = this.state;
+
+        return (
+            <Viewer
+                {...this.props}
+                actions={
+                    <ToolbarGroup firstChild={true}>
+                        <FlatButton label={"Locate on a map"} onClick={() => this.openGpsLocator()}/>
+                    </ToolbarGroup>
+                }
+                error={error}
+                style={{display: "flex", justifyContent: "space-around", flexFlow: "row wrap"}}
+            >
+                {Object.keys(data).map(key =>
+                    <Card style={{width: "calc(50% - 20px)", margin: 10, overflow: "auto"}}>
+                        <CardTitle key={key+'-head'}>{key}</CardTitle>
+
+                        <CardText>
+                            <Table selectable={false}>
+                                <TableBody displayRowCheckbox={false}>
+                                {Object.keys(data[key]).map(itemKey =>
+                                    <TableRow key={`${key}-${itemKey}`}>
+                                        <TableRowColumn>{itemKey}</TableRowColumn>
+                                        <TableRowColumn>{data[key][itemKey]}</TableRowColumn>
+                                    </TableRow>
+                                )}
+                                </TableBody>
+                            </Table>
+                        </CardText>
+                    </Card>
+                )}
+            </Viewer>
+        );
+    }
+}
+
+let Viewer = (props) => {
+    return (
+        <div {...props} />
+    )
+}
+
+// Define HOCs
+Viewer = PydioHOCs.withActions(Viewer);
+Viewer = PydioHOCs.withLoader(Viewer)
+Viewer = PydioHOCs.withErrors(Viewer)
+
+window.PydioExif = {
+    Editor: Editor,
+    DetailPanel:DetailPanel
+};
