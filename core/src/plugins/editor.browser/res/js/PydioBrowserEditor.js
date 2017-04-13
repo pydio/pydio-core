79377167c7d6135cbe244ea144a5d778d6c7f2df
Greg Hecquet <gregory@pydio.com>
2017-04-13 13:50:38 +0200

    Reactifying editor.browser

--- core/src/plugins/editor.browser/res/js/PydioBrowserEditor.js
+++ core/src/plugins/editor.browser/res/js/PydioBrowserEditor.js
@@ -18,95 +18,101 @@
  * The latest code can be found at <https://pydio.com>.
  */
 
-(function(global){
+import React, {Component} from 'react';
 
-    let BrowserOpener = React.createClass({
+class PydioBrowserEditor extends Component {
 
-        componentDidMount: function(){
-            let node = this.props.node;
-            const configs = this.props.pydio.getPluginConfigs("editor.browser");
-
-            if(node.getAjxpMime() == "url" || node.getAjxpMime() == "website"){
-                this.openBookmark(node, configs);
-            }else{
-                this.openNode(node, configs);
+    static get styles() {
+        return {
+            iframe: {
+                border: 0,
+                flex: 1
             }
-
-        },
-
-        getInitialState: function(){
-            return {loading: true, frameSrc:null};
-        },
-
-        openBookmark(node, configs){
-
-            let alwaysOpenLinksInBrowser = (configs.get('OPEN_LINK_IN_TAB') === 'browser');
-
-            PydioApi.getClient().request({get_action:'get_content', file:node.getPath()}, function(transp){
-                var url = transp.responseText;
-                if(url.indexOf('URL=') !== -1){
-                    url = url.split('URL=')[1];
-                    if(url.indexOf('\n') !== -1){
-                        url = url.split('\n')[0];
-                    }
-                }
-                this._openURL(url, alwaysOpenLinksInBrowser, true);
-            }.bind(this));
-        },
-
-        openNode(node, configs){
-
-            var repo = pydio.user.getActiveRepository();
-            var loc = document.location.href.split('?').shift().split('#').shift();
-            var url = loc.substring(0, loc.lastIndexOf('/'));
-            if(document.getElementsByTagName('base').length){
-                url = document.getElementsByTagName('base')[0].href;
-                if(url.substr(-1) == '/') url = url.substr(0, url.length - 1);
-            }
-            var open_file_url = LangUtils.trimRight(url, "\/") + "/" + pydio.Parameters.get('ajxpServerAccess') + "&get_action=open_file&repository_id=" + repo + "&file=" + encodeURIComponent(node.getPath());
-            var configs = this.props.pydio.getPluginConfigs("editor.browser");
-            let alwaysOpenDocsInBrowser = configs.get('OPEN_DOCS_IN_TAB') === "browser";
-
-            this._openURL(open_file_url, alwaysOpenDocsInBrowser, false);
-
-        },
-
-        _openURL(url, modal=false, updateTitle = false){
-            if(modal){
-                global.open(url, '', "location=yes,menubar=yes,resizable=yes,scrollbars=yes,toolbar=yes,status=yes");
-                if(this.props.onRequestTabClose){
-                    this.props.onRequestTabClose();
-                }
-            }else{
-                if(updateTitle && this.props.onRequestTabTitleUpdate){
-                    this.props.onRequestTabTitleUpdate(url);
-                }
-                this.setState({loading:false, frameSrc:url});
-            }
-        },
-
-        render: function(){
-
-            let content;
-            if(this.state.loading){
-                content = <div>Loading</div>;
-            }else{
-                content = (
-                    <iframe
-                        style={{border:0}}
-                        className="vertical_fit"
-                        src={this.state.frameSrc}
-                    ></iframe>
-                );
-            }
-            return (
-                {content}
-            );
-
         }
+    }
+    
+    constructor(props) {
+        super(props)
 
-    });
+        this.state = {
+            frameSrc: null
+        }
+    }
 
-    window.PydioBrowserEditor = BrowserOpener;
+    componentDidMount(){
+        const {pydio, node} = this.props;
+        const configs = pydio.getPluginConfigs("editor.browser");
 
-})(window);
+        if (node.getAjxpMime() == "url" || node.getAjxpMime() == "website") {
+            this.openBookmark(node, configs);
+        } else {
+            this.openNode(node, configs);
+        }
+    }
+
+    openBookmark(node, configs) {
+
+        let alwaysOpenLinksInBrowser = (configs.get('OPEN_LINK_IN_TAB') === 'browser');
+
+        PydioApi.getClient().request({get_action:'get_content', file:node.getPath()}, function(transp){
+            var url = transp.responseText;
+            if(url.indexOf('URL=') !== -1){
+                url = url.split('URL=')[1];
+                if(url.indexOf('\n') !== -1){
+                    url = url.split('\n')[0];
+                }
+            }
+            this._openURL(url, alwaysOpenLinksInBrowser, true);
+        }.bind(this));
+    }
+
+    openNode(node, configs) {
+
+        var repo = pydio.user.getActiveRepository();
+        var loc = document.location.href.split('?').shift().split('#').shift();
+        var url = loc.substring(0, loc.lastIndexOf('/'));
+        if(document.getElementsByTagName('base').length){
+            url = document.getElementsByTagName('base')[0].href;
+            if(url.substr(-1) == '/') url = url.substr(0, url.length - 1);
+        }
+        var open_file_url = LangUtils.trimRight(url, "\/") + "/" + pydio.Parameters.get('ajxpServerAccess') + "&get_action=open_file&repository_id=" + repo + "&file=" + encodeURIComponent(node.getPath());
+        var configs = this.props.pydio.getPluginConfigs("editor.browser");
+        let alwaysOpenDocsInBrowser = configs.get('OPEN_DOCS_IN_TAB') === "browser";
+
+        this._openURL(open_file_url, alwaysOpenDocsInBrowser, false);
+
+    }
+
+    _openURL(url, modal=false, updateTitle = false) {
+        if(modal){
+            global.open(url, '', "location=yes,menubar=yes,resizable=yes,scrollbars=yes,toolbar=yes,status=yes");
+            if (this.props.onRequestTabClose) {
+                this.props.onRequestTabClose();
+            }
+        }else{
+            if (updateTitle && this.props.onRequestTabTitleUpdate) {
+                this.props.onRequestTabTitleUpdate(url);
+            }
+            this.setState({frameSrc:url});
+        }
+    }
+
+    render() {
+        return (
+            <CompositeEditor style={PydioBrowserEditor.styles.iframe} src={this.state.frameSrc} />
+        )
+    }
+}
+
+let CompositeEditor = (props) => {
+    return <iframe {...props} />
+}
+
+// Define HOCs
+if (typeof PydioHOCs !== "undefined") {
+    CompositeEditor = PydioHOCs.withActions(CompositeEditor);
+    CompositeEditor = PydioHOCs.withLoader(CompositeEditor);
+    CompositeEditor = PydioHOCs.withErrors(CompositeEditor);
+}
+
+window.PydioBrowserEditor = PydioBrowserEditor;
