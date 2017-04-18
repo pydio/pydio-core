57400f71e4f26dc396c060f76efa63b3b667ce24
cdujeu <charles@ajaxplorer.info>
2017-04-18 11:41:06 +0200

    Rewrite DuoSecurity Modal

--- core/src/plugins/authfront.duosecurity/res/js/DuoSecurityActions.js
+++ core/src/plugins/authfront.duosecurity/res/js/DuoSecurityActions.js
@@ -5,44 +5,66 @@
     class Callbacks{
 
         static duoPrompt() {
-            
-            modal.showDialogForm('', 'duosecurity_box', function(oForm){
-
-                oForm.down("#duosecurity_box").insert('<iframe id="duo_iframe" width="420" height="330" frameborder="0"></iframe>');
-                oForm.insert('<form method="POST" id="duo_form"></form>');
-                modal.refreshDialogPosition();
-                new PeriodicalExecuter(function(pe){
-                    var sigResponse = oForm.down('input[name="sig_response"]');
-                    if(! sigResponse ) return;
-                    pe.stop();
-                    var conn = new Connexion();
-                    conn.setParameters({
-                        sig_response: sigResponse.getValue(),
-                        get_action  : 'duo_post_verification_code'
-                    });
-                    conn.onComplete = function(){
-                        window.setTimeout(function(){
-                            pydio.loadXmlRegistry();
-                        }, 400);
-                        hideLightBox();
-                    };
-                    conn.sendAsync();
-                }, 1);
-                Duo.init({
-                    host:pydio.getPluginConfigs('authfront.duosecurity').get('DUO_AUTH_HOST'),
-                    sig_request:pydio.getPluginConfigs('authfront.duosecurity').get('DUO_AUTH_LAST_SIGNATURE'),
-                    post_action:''
-                });
-                Duo.ready();
-
-            }, null, false, true);
-            
+            pydio.UI.openComponentInModal('DuoSecurityActions', 'Dialog');
         }
 
     }
 
+    const DuoSecDialog = React.createClass({
+
+        mixins:[
+            PydioReactUI.ActionDialogMixin
+        ],
+
+        getDefaultProps: function(){
+            return {
+                dialogTitle: "Duo Security Configuration",
+                dialogIsModal: true
+            };
+        },
+
+        componentDidMount:function(){
+
+            const {pydio, onDismiss} = this.props;
+            const {XMLUtils, PydioApi} = global.PydioCore;
+            const oForm = this.refs.form;
+
+            Duo.init({
+                host        : pydio.getPluginConfigs('authfront.duosecurity').get('DUO_AUTH_HOST'),
+                sig_request : pydio.getPluginConfigs('authfront.duosecurity').get('DUO_AUTH_LAST_SIGNATURE'),
+                post_action : '',
+                submit_callback : function(oForm){
+                    const value = oForm.elements['sig_response'].value;
+                    PydioApi.getClient().request({
+                        sig_response: value,
+                        get_action  : 'duo_post_verification_code'
+                    }, () => {
+                        global.setTimeout(function(){
+                            pydio.loadXmlRegistry();
+                        }, 400);
+                    });
+                    onDismiss();
+                }
+            });
+        },
+
+        shouldComponentUpdate:function(){
+            return false;
+        },
+
+        render: function(){
+            return (
+                <div>
+                    <iframe id="duo_iframe" width={370} height={330} frameBorder={0}></iframe>
+                </div>
+            );
+        }
+
+    });
+
     global.DuoSecurityActions = {
-        Callbacks: Callbacks
+        Callbacks: Callbacks,
+        Dialog   : DuoSecDialog
     };
 
-})(window)
+})(window);
