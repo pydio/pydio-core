a944a929a8a6059ef3470a4c69699fc1857b1cfe
cdujeu <charles@ajaxplorer.info>
2017-04-19 09:44:23 +0200

    Fix Toggle Bookmark action

--- core/src/plugins/gui.ajax/res/js/ui/CoreActions/callbacks/toggleBookmark.js
+++ core/src/plugins/gui.ajax/res/js/ui/CoreActions/callbacks/toggleBookmark.js
@@ -1,5 +1,18 @@
 import {pydio} from '../globals'
+const PydioApi = require('pydio/http/api')
 
 export default function(){
-    pydio.notify("add_bookmark");
+    const selection = pydio.getContextHolder();
+    if(selection.isEmpty() || !selection.isUnique()){
+        return;
+    }
+    const node = selection.getUniqueNode();
+    const isBookmarked = node.getMetadata().get('ajxp_bookmarked') === 'true';
+    PydioApi.getClient().request({
+        get_action:'get_bookmarks',
+        bm_action: isBookmarked ? 'delete_bookmark' : 'add_bookmark',
+        bm_path:node.getPath()
+    }, (t) => {
+        selection.requireNodeReload(node);
+    });
 }
