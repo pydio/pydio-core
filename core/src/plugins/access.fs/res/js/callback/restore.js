d83cb47b9cdeb992ae305e1a90cc3e973a7c9b0b
cdujeu <charles@ajaxplorer.info>
2017-04-24 10:33:04 +0200

    Enable multiple node restore (js-side)

--- core/src/plugins/access.fs/res/js/callback/restore.js
+++ core/src/plugins/access.fs/res/js/callback/restore.js
@@ -4,7 +4,17 @@ export default function (pydio) {
 
     return function(){
 
-        PydioApi.getClient().postSelectionWithAction('restore');
+        if(pydio.getContextHolder().isMultiple()){
+            const ctxNode = pydio.getContextHolder().getContextNode();
+            pydio.getContextHolder().getSelectedNodes().forEach((n) => {
+                let tmpModel = new PydioDataModel();
+                tmpModel.setContextNode(ctxNode);
+                tmpModel.setSelectedNodes([n]);
+                PydioApi.getClient().postSelectionWithAction('restore', null, tmpModel);
+            });
+        }else{
+            PydioApi.getClient().postSelectionWithAction('restore');
+        }
 
     }
 
