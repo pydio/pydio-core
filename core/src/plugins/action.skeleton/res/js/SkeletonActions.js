9380a07946732c9d230ee0b2784cedb72e4d6345
cdujeu <charles@ajaxplorer.info>
2017-05-02 12:22:22 +0200

    Rewrite action.skeleton

--- core/src/plugins/action.skeleton/res/js/SkeletonActions.js
+++ core/src/plugins/action.skeleton/res/js/SkeletonActions.js
@@ -1,5 +1,7 @@
 (function(global){
 
+    const {ActionDialogMixin,CancelButtonProviderMixin,SubmitButtonProviderMixin, Loader} = require('pydio').requireLib('boot');
+
     let pydio = global.pydio;
 
     class Callbacks{
@@ -13,21 +15,61 @@
         }
 
         static frameButton(){
-            var dialogLoadFunction = function(){
-                var conn = new Connexion();
-                conn.addParameter("get_action", "my_skeleton_button_frame");
-                conn.onComplete = function(transport){
-                    $('loaded_content').update(transport.responseText);
-                }
-                conn.sendAsync();
-            };
-            modal.showDialogForm("My Link", "my_skeleton_form", dialogLoadFunction);
+            pydio.UI.openComponentInModal('SkeletonActions', 'Dialog');
         }
 
     }
 
+    /**
+     * Sample Dialog class used for reference only, ready to be
+     * copy/pasted :-)
+     */
+    const SkeletonDialog = React.createClass({
+
+        mixins:[
+            ActionDialogMixin,
+            CancelButtonProviderMixin,
+            SubmitButtonProviderMixin
+        ],
+
+        getDefaultProps: function(){
+            return {
+                dialogTitle: "Demonstration Dialog",
+                dialogIsModal: true
+            };
+        },
+        submit(){
+            this.dismiss();
+        },
+        componentDidMount: function(){
+            PydioApi.getClient().request({get_action:'my_skeleton_button_frame'}, (transport)=>{
+                this.setState({content: transport.responseText});
+            });
+        },
+        render: function(){
+
+            if(!this.state || !this.state.content){
+                return <Loader/>;
+            }
+            return (
+                <div dangerouslySetInnerHTML={{__html:this.state.content}}/>
+            );
+        }
+
+    });
+
+    const Footer = React.createClass({
+
+        render: function(){
+            return <div style={{zIndex: 1500, position:'absolute', bottom:0, left: 0, right: 0, padding:10, backgroundColor:'white'}}>Test Content</div>
+        }
+
+    });
+
     global.SkeletonActions = {
-        Callbacks: Callbacks
+        Callbacks: Callbacks,
+        Dialog   : SkeletonDialog,
+        Template : Footer
     };
 
 })(window)
