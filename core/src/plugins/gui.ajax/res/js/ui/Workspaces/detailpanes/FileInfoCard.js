b6adeb8095384c34f12d3b1d74732828c5a59e28
cdujeu <charles@ajaxplorer.info>
2017-05-05 12:35:00 +0200

    Fix FileInfoCard rendering

--- core/src/plugins/gui.ajax/res/js/ui/Workspaces/detailpanes/FileInfoCard.js
+++ core/src/plugins/gui.ajax/res/js/ui/Workspaces/detailpanes/FileInfoCard.js
@@ -1,23 +1,25 @@
 import React from 'react'
+import Pydio from 'pydio'
 import InfoPanelCard from './InfoPanelCard'
+const {PydioContextConsumer} = Pydio.requireLib('boot')
 
 class FileInfoCard extends React.Component {
-    constructor(props) {
-        super(props)
 
-        const meta = props.node.getMetadata();
+    render() {
+
+        const {node, getMessage} = this.props;
+        const meta = node.getMetadata();
 
         let size = meta.get('bytesize');
         let hSize = PathUtils.roundFileSize(parseInt(size));
         let time = meta.get('ajxp_modiftime');
         var date = new Date();
         date.setTime(parseInt(meta.get('ajxp_modiftime')) * 1000);
-
         let formattedDate = PathUtils.formatModifDate(date);
 
         let data = [
-            {key:'size',label:'File Size',value:hSize},
-            {key:'date',label:'Modified on',value:formattedDate}
+            {key:'size',label:getMessage('2'),value:hSize},
+            {key:'date',label:getMessage('4'),value:formattedDate}
         ];
 
         let w = meta.get('image_width');
@@ -25,20 +27,22 @@ class FileInfoCard extends React.Component {
         if(w && h){
             data = [
                 ...data,
-                {key:'image', label:'Image Dimension', value:w + 'px X ' + h + 'px'}
+                {key:'image', label:getMessage('135'), value:w + 'px X ' + h + 'px'}
             ]
         }
 
-        this.state = {
-            data: data
-        }
-    }
-
-    render() {
         return (
-            <InfoPanelCard {...this.props} title="File Information" standardData={this.state.data} contentStyle={{paddingBottom: 10}} icon="information-outline" iconColor="#2196f3"/>
+            <InfoPanelCard
+                {...this.props}
+                title={getMessage('341')}
+                standardData={data}
+                contentStyle={{paddingBottom: 10}}
+                icon="information-outline"
+                iconColor="#2196f3"
+            />
         );
     }
 }
 
+FileInfoCard = PydioContextConsumer(FileInfoCard);
 export {FileInfoCard as default}
