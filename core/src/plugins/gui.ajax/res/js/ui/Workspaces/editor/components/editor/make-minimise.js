fc74e8a9811c40324105f538f0d0c23c7ad0ef52
Greg Hecquet <gregory@pydio.com>
2017-05-12 14:52:36 +0200

    Minimise renders twice

--- core/src/plugins/gui.ajax/res/js/ui/Workspaces/editor/components/editor/make-minimise.js
+++ core/src/plugins/gui.ajax/res/js/ui/Workspaces/editor/components/editor/make-minimise.js
@@ -21,10 +21,6 @@ const makeEditorMinimise = (Target) => {
         render() {
             const {minimised} = this.state
 
-            if (typeof minimised === "undefined") {
-                return <Target {...this.props} />
-            }
-
             const motionStyle = {
                 scale: minimised ? spring(ORIGIN, ANIMATION) : TARGET
             };
