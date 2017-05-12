9a625f5c2612e9647aa55d21c34ec956e8bcc47c
cdujeu <charles@ajaxplorer.info>
2017-05-12 08:53:31 +0200

    Add a light shadow to detach image from background

--- core/src/plugins/editor.diaporama/res/js/editor.js
+++ core/src/plugins/editor.diaporama/res/js/editor.js
@@ -98,6 +98,7 @@ class Editor extends PureComponent {
                         containerHeight={containerHeight}
                         imgClassName="diaporama-image-main-block"
                         style={{backgroundColor:'#424242'}}
+                        imgStyle={{boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px'}}
                     />
                 }
                 </ImageSizeProvider>
