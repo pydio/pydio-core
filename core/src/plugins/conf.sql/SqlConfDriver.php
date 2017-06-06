2b9f0e818e134222e8f78828c3140b7454a87e73
cdujeu <charles@ajaxplorer.info>
2017-06-06 11:58:51 +0200

    Do not fetchAll() in simpleStoreList() and ability to pass a callbackFunction.

--- core/src/plugins/conf.sql/SqlConfDriver.php
+++ core/src/plugins/conf.sql/SqlConfDriver.php
@@ -1096,7 +1096,7 @@ class SqlConfDriver extends AbstractConfDriver implements SqlTableProvider
      * @param string $relatedObjectId
      * @return array
      */
-    public function simpleStoreList($storeId, &$cursor=null, $dataIdLike="", $dataType="serial", $serialDataLike="", $relatedObjectId=""){
+    public function simpleStoreList($storeId, &$cursor=null, $dataIdLike="", $dataType="serial", $serialDataLike="", $relatedObjectId="", $callbackFunction = null){
         $wheres = array();
         $wheres[] = array('[store_id]=%s', $storeId);
         if(!empty($dataIdLike)){
@@ -1125,15 +1125,22 @@ class SqlConfDriver extends AbstractConfDriver implements SqlTableProvider
             $children_results = dibi::query("SELECT * FROM [ajxp_simple_store] WHERE %and", $wheres);
 
         }
-        $values = $children_results->fetchAll();
+
         $result = array();
-        foreach($values as $value){
+        foreach($children_results as $value){
             if ($dataType == "serial") {
                 $data = unserialize($value["serialized_data"]);
             } else {
                 $data = $value["binary_data"];
             }
-            $result[$value['object_id']] = $data;
+            if($callbackFunction !== null){
+                $stop = $callbackFunction($value['object_id'], $data);
+                if($stop){
+                    break;
+                }
+            }else{
+                $result[$value['object_id']] = $data;
+            }
         }
         return $result;
     }
