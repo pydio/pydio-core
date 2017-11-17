3a9921dd0c754d1377bddbab574d5ee391ddd363
DepaMarco <depardimarco@hotmail.it>
2017-11-17 11:32:57 +0100

    Add Italian translation for plugin 'gui.ajax'

---
+++ core/src/plugins/gui.ajax/res/i18n/it.php
@@ -0,0 +1,42 @@
+<?php
+$mess = [
+
+    "tour.welcomemodal.title" => "Benvenuto su APPLICATION_TITLE",
+    "tour.welcomemodal.subtitle" => "Fai il Drag'n'drop della foto migliore per il tuo profilo! Questo veloce tour ti guiderà nell'interfaccia web.",
+    "tour.welcomemodal.skip" => "Salta",
+    "tour.welcomemodal.start" => "Inizia il tour",
+
+    "tour.create-menu.title" => "Aggiungi file",
+    "tour.infopanel.title" => "Pannello Informazioni",
+    "tour.display-bar.title" => "Opzioni Visuale",
+    "tour.uwidget.title" => "Cartouche Utente",
+    "tour.workspaces.title" => "I Miei Workspace",
+    "tour.globsearch.title" => "Ricerca Globale",
+    "tour.widget-cards.title" => "Widgets",
+    "tour.openworkspace.title" => "Apri un workspace",
+
+    "tour.create-menu"=>"Inizia aggiungendo nuovi file o cartelle al workspace corrente.",
+    "tour.infopanel.1"=> "Quì troverai alcune informazioni riguardo la selezione corrente: informazioni sul file, stato della condivisione, meta-dati definiti dall'utente ecc",
+    "tour.infopanel.2"=> "Puoi chiudere questo pannello usando il pulsante info nella toolbar.",
+    "tour.infopanel.search"=> "Cerca",
+    "tour.infopanel.folder"=> "Cartella",
+    "tour.infopanel.file"=> "File",
+    "tour.uwidget.addressbook" => "Elenco di tutti gli utenti che accedono alla piattaforma. Crea i tuoi utenti e forma i team da usare per la condivisione di risorse",
+    "tour.uwidget.alerts" => "Il pannello degli avvisi ti informerà ogniqualvolta un utente con cui condividi delle risorse vi accede. Queste notifiche possono esserti inviate direttamente per e-mail.",
+    "tour.uwidget.menu" => "Accesso ad altre opzioni: gestire il tuo profilo e le funzionalità di sicurezza, sfogliare tutte le risorse che hai condiviso, uscire dalla piattaforma.",
+    "tour.uwidget.home" => "Torna al pannello di benvenuto utilizzando questo pulsante",
+    "tour.display-bar" => "Questa toolbar ti permette di cambiare la visualizzazione: scambiare la modalità tra anteprima e dettagli a seconda del tuo utilizzo, ordinare i file per nome, data ecc.",
+
+    "tour.workspaces.1" => "I workspace sono cartelle di livello più alto che aiuato ad organizzare i dati. Possono essere accedute solo da te o condivise con più persone.",
+    "tour.workspaces.2" => "Altri utenti possono condividere alcune delle loro cartelle con te: appariranno anch'esse quì come workspace.",
+    "tour.globsearch.1" => "Utilizza questo campo di ricerca per trovare file e cartelle in ogni workspace. Verranno mostrati solo i primi 5 risultati; entra in un workspace per avere più risultati e più opzioni di ricerca.",
+    "tour.globsearch.2" => "Quando nessuna voce di ricerca viene inserita, viene mostrata la cronologia dei file e delle cartelle cui si è acceduto di recente.",
+    "tour.widget-cards" => "Quì puoi aggiungere o rimuovere i widget. Puoi anche riordinarli semplicemente trascinandoli.",
+    "tour.openworkspace" => "Alla prima connessione, la tua cronologia sarà probabilmente vuota. Inserisci un workspace quando sei pronto per aggiungere i file.",
+
+    "tour.locale.back" => "Indietro",
+    "tour.locale.close" => "Chiudi",
+    "tour.locale.last" => "Fine",
+    "tour.locale.next" => "Avanti",
+    "tour.locale.skip" => "Salta",
+];
