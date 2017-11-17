e8cdf06190683bf45e76984cdd2d461266567a98
DepaMarco <depardimarco@hotmail.it>
2017-11-17 14:43:52 +0100

    Add Italian translation for plugin 'access.ajxp_conf' (admin) 1/2

---
+++ core/src/plugins/access.ajxp_conf/res/i18n/admin/it.php
@@ -0,0 +1,346 @@
+<?php
+/*
+ * Copyright 2007-2017 Abstrium <contact (at) pydio.com>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com/>.
+ */
+$mess=array(
+    "menu.0" => "Bacheca",
+    "menu.1" => "Attività",
+    "menu.2" => "Log Server",
+    "menu.3" => "Aspetto",
+    "menu.4" => "Tema, loghi, opzioni dell'aspetto",
+    "menu.5" => "Funzionalità Condivisione",
+    "menu.6" => "Opzioni per tutte le condivisioni di file e cartelle",
+    "menu.7" => "Editor",
+    "menu.8" => "Abilita/disabilita gli editor e gestiscine i parametri",
+    "menu.9" => "Uploader",
+    "menu.10" => "Estensioni per inviare file attraverso la UI web.",
+    "menu.11" => "--- Autenticazione",
+    "menu.11b" => "Autenticazione",
+    "menu.12" => "--- Configurazioni Backend",
+    "menu.13" => "--- Logger",
+    "menu.14" => "Notifiche",
+    "menu.15" => "Come gli utenti ricevono notifiche dalle varie parti dell'applicazione.",
+    "menu.16" => "Pydio Booster",
+    "menu.17" => "Messaggistica istantanea e tool di accelerazione esterno",
+    "menu.18" => "Tutti i Plugin",
+    "menu.19" => "Plugin Disponibili",
+    "menu.20" => "Funzionalità Utente",
+    "menu.21" => "Parametri dell'utente finale",
+    "menu.22" => "Pagina Benvenuto",
+    "menu.23" => "Pagina di arrivo per l'utente dopo l'autenticazione. Configura elementi visualizzati e link.",
+
+    "home.1" => "Accesso Rapido",
+    "home.2" => "Selettori Date",
+    "home.3" => "Cursori Pagine",
+    "home.4" => "Data Inizio",
+    "home.5" => "Data Fine",
+    "home.6" => "Caricamento...",
+
+    "home.7" => "Widget più attivi",
+    "home.8" => "Tipo...",
+    "home.9" => "Utenti",
+    "home.10" => "Indirizzi IP",
+    "home.11" => "Azioni File",
+    "home.12" => "Legenda",
+    "home.13" => "Intervallo Date",
+    "home.14" => "Ultimo Giorno",
+    "home.15" => "Ultima Settimana",
+    "home.16" => "Ultimo Mese",
+    "home.17" => "Azione File (se il tipo è azione)",
+    "home.18" => "Intervallo Aggiornamento (secondi)",
+
+    "home.19" => "Small Graph Widget",
+    "home.20" => "Big Graph Card",
+    "home.21" => "Grafico",
+    "home.22" => "Upload",
+    "home.23" => "Download",
+    "home.24" => "File Condivisi",
+    "home.25" => "Connessioni",
+    "home.26" => "Giornalmente",
+    "home.27" => "Settimanalmente",
+    "home.28" => "Mensilmente",
+    "home.29" => "Frequenza Campionamento",
+    "home.30" => "Titolo",
+    // Rich Log List
+    "home.31" => "Nessun evento trovato.",
+    "home.32" => "Eventi Recenti",
+    "home.33" => "Tutti",
+    "home.34" => "Errori",
+    // Server Status
+    "home.35" => "Status Server",
+    "home.36" => "Occupato",
+    "home.37" => "Libero",
+    "home.38" => "Spazio Usato",
+    "home.39" => "Spazio Libero",
+    "home.40" => "Uso CPU",
+    "home.41" => "Uso Disco Sistema",
+    "home.42" => "Carico Server",
+
+    "home.43" => "Aggiungi scorciatoia ...",
+    "home.44" => "Inizia",
+    "home.45" => "Ricevuto, non mostrare di nuovo!",
+    "home.getting_started" => "Benvenuto nella Bacheca di Amministrazione. Questa è la tua sala di controllo per tutti gli aspetti dell'applicazione. Sfoglia il menu di sinistra per gestire utenti, workspace e parametri dell'applicazione. Puoi personalizzare questa bacheca con dati in formato di grafico per una gestione più efficiente. Apri il Dashboard Builder cliccando sul pulsante arancione in basso a destra per aggiungere o togliere i tuoi widget. Potrai poi riordinare i badge semplicemente cliccando e trascinandoli.<br/>Se hai bisogno di aiuto quando compili i campi dei parametri, quando vedi il simbolo <span class='icon-question-sign'></span>, cliccaci sopra per aprire il Pydio Companion.",
+
+    // Widgets Builder
+    "home.48" => "Rimuovi",
+    "home.49" => "Aggiungi/Rimuovi Widget",
+    "home.50" => "Aggiungi un nuovo widget...",
+    "home.51" => "Sei sicuro? Il layout corrente andrà perso.",
+    "home.52" => "Crea Widget",
+    "home.53" => "Dashboard Builder",
+    "home.54" => "Per personalizzare la tua bacheca, nascondi i widget non necessari cliccando sui pulsanti RIMUOVI.",
+    "home.55" => "Per aggiungere il tuo widget, seleziona il tipo, imposta i parametri e clicca Crea.",
+    "home.56" => "Reimposta Layout",
+
+    // Default Widgets
+    "home.57" => "connessioni oggi",
+    "home.58" => "download oggi",
+    "home.59" => "upload questa settimana",
+    "home.60" => "file condivisi oggi",
+    "home.61" => "utente più attivo oggi",
+    "home.62" => "IP più attivo nell'ultima settimana",
+    "home.63" => "più scaricato nell'ultima settimana",
+    "home.64" => "anteprima più visualizzata nell'ultima settimana",
+    "home.65" => "Attività File",
+    "home.66" => "Connessioni Web",
+    "home.67" => "Benvenuto nella tua bacheca amministrativa. Come amministratore, sei autorizzato a gestire solo gli utenti e i workspace del tuo gruppo. Se vuoi modificare le impostazioni globali dell'applicazione, esegui l'accesso come super amministratore (i super amministratori devono essere esterni a qualunque gruppo).",
+    "home.68" => "Torna ai Workspace",
+    "home.69" => "Filtra per Workspace",
+    "home.70" => "Lascia vuoto per utilizzare i dati di tutti i workspace, oppure seleziona un workspace indicato.",
+    "home.71" => "Filtro nome file",
+    "home.72" => "Usa *.ext per filtrare per estensione, per esempio, oppure prefisso* per selezionare solo i file che iniziano con quel prefisso.",
+    "home.73" => "Se il tipo è Azione File, seleziona l'azione che desideri monitorare.",
+    "home.74" => "Vedi tutte le attività",
+    "home.75" => "Lista Todo",
+    "home.76" => "Aggiungi il tuo task",
+    "home.77" => "Esempio Task",
+    "home.78" => "fatto",
+    "home.79" => "Il widget è stato aggiunto alla tua bacheca",
+
+    // Logs Panel
+    "logs.1" => "Server Logs",
+    "logs.2" => "Pick a day",
+    "logs.3" => "Filter Logs",
+    "logs.4" => "Click on a row for details",
+    "logs.5" => "Event Detail",
+    "logs.6" => "Export logs",
+    "logs.7" => "Export from",
+    "logs.8" => "To",
+    "logs.9" => "File will be export as CSV format with semi-colmuns (;) separators and fields enclosed in double-quotes (\").",
+    "logs.10"=> "Export and download",
+    "logs.11"=> "Export...",
+
+    // People Panel
+    "user.1" => "+ User",
+    "user.2" => "+ Group",
+    "user.3" => "Groups",
+    "user.4" => "Roles",
+    "user.5" => "Groups & Users",
+    "user.6" => "+ Role",
+    "user.7" => "Search users & groups",
+    "user.8" => "Set parameters on this group to apply them all groups and users.",
+    "user.9" => "Last connected",
+    "user.10" => "Access to %i workspaces",
+    "user.11" => "%i roles",
+    "user.12" => "Group path",
+    "user.13" => "shared",
+    "user.14" => "Create a new role",
+    "user.15" => "Create a new group",
+    "user.16" => "Group Identifier",
+    "user.17" => "Group Label",
+    "user.18" => "Role Identifier",
+    "user.19" => "Create a new user",
+    "user.20" => "User will be created in group %s",
+    "user.21" => "User login",
+    "user.22" => "User password",
+    "user.23" => "Confirm password",
+    "user.24" => "Root Group",
+    "user.25" => "Root Group Role",
+    "user.26" => "%s Group Role",
+    "user.27" => "User Personal Role",
+
+
+    "react.1" => "parent folder",
+    "react.2" => "bulk actions",
+    "react.3" => "Search Results for '%s'",
+    "react.4" => "Close Results",
+    "react.5a" => "read",
+    "react.5b" => "write",
+    "react.5" => "deny",
+    "react.6" => "children",
+    "react.7" => "Create a rule",
+    "react.8" => "You are about to manage permissions at the lower level. This folder will be read
+                        only by default, and you will manage permissions selectively on its children. Choose
+                        whether to copy the current permissions to its children, or to clear their permissions, which would make
+                        them inaccessible by default.",
+    "react.9" => "You are about to disable the permissions management at the sub-level: all
+                        children will automatically inherit this folder permissions, and you current settings
+                        for these children will be lost. Are you sure to confirm ?",
+    "react.10" => "Inherit Permissions",
+    "react.11" => "Empty Permissions",
+    "react.12" => "Clear Permissions",
+    "react.13" => "Show full permissions",
+    "react.14" => "Show resulting tree",
+
+
+    "plugins.1" => "General Options",
+    "plugins.2" => "Master Driver",
+    "plugins.3" => "Secondary Driver",
+    "plugins.4" => "Editor plugins are the previewers/editors associated to any file types in the application.
+                    These plugins are handle specifically in the GUI (extensions) and can declare the mimetypes they support.
+                    The order parameter also allows to define a priority for the 'open with...' action.",
+    "plugins.5" => "Save Changes",
+    "plugins.6" => "Revert",
+
+    "ws.1" => "Manage Templates",
+    "ws.2" => "Close Templates",
+    "ws.3" => "+ workspace",
+    "ws.4" => "+ Template",
+    "ws.5" => "Slug",
+    "ws.6" => "New Workspace",
+    "ws.7" => "New Template",
+    "ws.8" => "Existing Template",
+    "ws.9" => "Driver",
+    "ws.10" => "Select a template",
+    "ws.11" => "Workspace based on ...",
+    "ws.12" => "Template %s",
+    "ws.13" => "Template Options",
+    "ws.14" => "Set Generic Options",
+    "ws.15" => "Workspace Options",
+    "ws.16" => "Choose Storage Driver",
+    "ws.17" => "Select Storage Type",
+    "ws.18" => "Loading drivers",
+    "ws.19" => "Save to add features",
+    "ws.20" => "Create",
+    "ws.21" => "Template General Options",
+    "ws.22" => "Workspace General Options",
+    "ws.23" => "Delete workspace",
+    "ws.24" => "Children Workspaces",
+    "ws.25" => "Shares",
+    "ws.26" => "Folders Permissions",
+    "ws.27" => "Additional Features",
+    "ws.28" => "Summary",
+    "ws.29" => "Parameters",
+    "ws.30" => "General Options",
+    "ws.31" => "Remove Feature",
+    "ws.32" => "Add a feature...",
+    "ws.33" => "Recent Activity",
+    "ws.34" => "Last events on this workspace",
+    "ws.35" => "Accessible to %i users",
+    "ws.36" => "+ %i files or folders shared",
+    "ws.37" => "Workspaces using this template",
+    "ws.38" => "Shared Items in this workspace",
+    "ws.39" => "Owner",
+    "ws.40" => "Share Type",
+    "ws.41" => "Path",
+    "ws.42" => "Folder Level Permissions",
+    "ws.43" => "Specify here the default access permissions to this workspace folders.
+                                To enable the permissions management at a sub-level, select the 'CHILDREN' on a given folder.
+                                This will set the current folder as readonly by default, and you will be able to open accesses
+                                selectively to any of its children folders.",
+    "ws.44" => "Please be aware that this is only a mask: if you modify the real folders directly, you may have
+                                to edit this mask accordingly. Also, these permissions may be globally limited by the 'Default Rights'
+                                paremeter set for this workspace (see the General tab).",
+    "ws.45" => "As its parameters are using 'AJXP_USER' or 'AJXP_GROUP_*' keywords, the contents of
+                                this workspace will vary depending on the currently authenticated user. For this reason, you cannot browse the
+                                folders directly and assign permissions to them, but you can freely define some generic pathes that will be used as masks.",
+    "ws.46" => "Add a feature",
+    "ws.47" => "Select a feature",
+    "ws.48" => "Beware, this workspace/template is not editable, as it is defined directly in the conf/bootstrap_repositories.php file.",
+    "ws.49" => "",
+    "ws.50" => "Indexation",
+    "ws.51" => "This workspace has an indexer (%s). Normally, indexation is done \"on-the-fly\" when files are added/removed
+                    through the application interfaces. However if you are doing some modifications directly at the storage level, you may have
+                    to manually re-trigger an indexation from time to time. Use buttons below to either index now, or program a task in the scheduler
+                    to run indexation on a regular basis.",
+    "ws.52" => "Please note: as this worksace has user-specific contents, indexation will be launched for each user.",
+    "ws.53" => "Launch indexation now",
+    "ws.54" => "Schedule every day at 3AM",
+    "ws.55" => "Automatic Purge",
+    "ws.56" => "This workspace has a \"purge\" setup, to automatically remove documents that were left untouched since %s days. Make sure to add a task in the
+                    scheduler to regularly process this workspace and look for expired document. You can use the buttons below to create this task automatically,
+                    ok run the purge directly.",
+    "ws.57" => "Please note: as this worksace has user-specific contents, purge will be running for each user.",
+    "ws.58" => "Purge now",
+    "ws.59" => "Schedule every day at 4AM",
+    "ws.60" => "Workspace Activity",
+    "ws.61" => "Activity of the workspace is displayed to the users in the right-hand panel. You can clear all activity
+                            for this workspace, which can be handy e.g. if you are going \"live\" in production after a staging period where you have
+                            made some tests in this workspace.",
+    "ws.62" => "Clear Activity",
+    "ws.63" => "Warning, this will clear all activity of this workspace displayed to users. Are you sure you want to do that?",
+    "ws.64" => "Cleared %s events",
+    "ws.65" => "Workspace Maintenance",
+    "ws.66" => "Running Tasks",
+    "ws.67" => "No tasks running",
+    "ws.68" => "No Parameters",
+    "ws.69" => "Template-based workspace",
+    "ws.70" => "Driver-based workspace",
+    "ws.71" => "Complete the steps below",
+    "ws.72" => "Fill in the form on the right",
+    "ws.73" => "Pick value before continuing",
+
+    "scheduler.1" => "Scheduler is used to trigger tasks automatically on a regular basis. It requires the launch of a master command regularly: if you are using Pydio Booster, it should trigger this for you. Otherwise, use a CRON to launch master command every minute.",
+    "scheduler.2" => "Command-line is not enabled in the %1, the scheduler will not be able to run the jobs.",
+    "scheduler.3" => "global configurations", // replaced dynamically as %1 with a link in the previous line
+
+    "perfs.title" => "System Performances",
+    "perfs.score"  => "Performance Score",
+    "perfs.desc"   => "Checking system configurations for possible improvements.",
+    "perfs.widget"  => "Add widget to dashboard",
+    "perfs.tests"   => "Performances Tests",
+    "perfs.more"    => "More info",
+    
+    "perfs.debug.title" => "Debug Modes",
+    "perfs.debug.description" => "Checks if debug modes are disabled on the server",
+    "perfs.debug.help" => "Make sure to disable debug modes on the server. This is done manually by editing the conf/booststrap_context.php (or /etc/pydio/bootstrap_context.php) file.",
+    "perfs.debug.error" => "Warning, one of AJXP_SERVER_DEBUG, AJXP_CLIENT_DEBUG or AJXP_SKIP_CACHE is set to True!",
+    
+    "perfs.cli.title" => "Command-line PHP",
+    "perfs.cli.description" => "Defer time-consuming tasks to command line.",
+    "perfs.cli.help" => "Enable the command line in core parameters. This will greatly improve end-user performances, as time-consuming tasks are delegated to other processes.",
+    "perfs.cli.error" => "Warning, command line is not enabled!",
+
+    "perfs.opcache.title" => "Opcode Cache Extension",
+    "perfs.opcache.description" => "Checks that a PHP Opcode cache is active",
+    "perfs.opcache.help" => "Pydio is a rich PHP framework that requires loading a lot of files in memory. Make sure to enable a PHP Opcache like APC/u, Opcache or XCache.",
+    "perfs.opcache.error" => "Could not detect any php extension from APC, APCu, Opcache or XCache.",
+
+    "perfs.cache.title" => "Pydio Cache",
+    "perfs.cache.description" => "Checks that a Pydio is configured to use a cache layer",
+    "perfs.cache.help" => "Pydio can connect to an external cache server like Redis or Memcache, or use in-memory cache to greatly improve file browsing performances.",
+    "perfs.cache.error" => "It does not seem that you have activated the cache service.",
+
+    "perfs.booster.title" => "Pydio Booster",
+    "perfs.booster.description" => "Checks that Pydio Booster is started and enables WebSocket communication",
+    "perfs.booster.help" => "Pydio Booster provides an easy-to-deploy additional tool to add WebSocket support and Upload/Download accelators in Pydio.",
+    "perfs.booster.error" => "Pydio Booster Error, see dedicated panel",
+
+    "perfs.load.title" => "Server Load",
+    "perfs.load.description" => "Detect high server load (Linux servers only)",
+    "perfs.load.help" => "A high server load may indicate that your server is missing CPU or RAM given your usage of Pydio.",
+    "perfs.load.error" => "Maybe adding some CPU or RAM to your server could be neccessary.",
+
+    "perfs.mailer.title" => "Mailer Queue",
+    "perfs.mailer.description" => "Checks that mailer is using the Queue feature",
+    "perfs.mailer.help" => "Using the mail queue avoids sending every emails directly during every actions and requests, but store them in a separate database and process the queue separately.",
+    "perfs.mailer.error" => "Mailer is not configured to defer emails sending.",
+
+
+);
